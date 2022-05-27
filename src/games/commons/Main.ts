541b43ee9e9f9afe46ff8c5d04d7cebad1a2ced7
Dong Zhou <dozhou@rubiconproject.com>
2022-05-27 15:28:50 -0700

    Update Main.ts

--- src/games/commons/Main.ts
+++ src/games/commons/Main.ts
@@ -1,24 +1,12 @@
 import { styled } from '@mui/material/styles';
 
-const Main = styled('main')<{ open: boolean, drawer_width: number }>(({ theme, open, drawer_width }) => ({
+const Main = styled('main')(({ theme }) => ({
     border: 0,
     width: "100%",
     height: "100%",
     overflow: "hidden",
     flexGrow: 1,
     padding: theme.spacing(0),
-    transition: theme.transitions.create('margin', {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-    }),
-    marginLeft: `-${drawer_width}px`,
-    ...(open && {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginLeft: 0,
-    }),
 }));
 
 export default Main;
