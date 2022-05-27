a8b4fa527a13607240f6e212bac5fa736a79262a
Dong Zhou <dozhou@rubiconproject.com>
2022-05-27 14:57:14 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,34 +3,24 @@ import Home from './home';
 
 import { ProblemsProvider } from './problems/commons/ProblemsContext';
 
-const SubApps = () => (
-    <Routes>
-        <Route index element={<Home />} />
-        <>
-            <Route path="sorting/bubble-sort" element={<div>Bubble Sort</div>} />
-            <Route path="sorting/selection-sort" element={<div>Selection Sort</div>} />
-            <Route path="sorting/merge-sort" element={<div>Merge Sort</div>} />
-            <Route path="sorting/insertion-sort" element={<div>Insertion Sort</div>} />
-            <Route path="sorting/quick-sort" element={<div>Quick Sort</div>} />
-            <Route path="sorting/heap-sort" element={<div>Heap Sort</div>} />
-            <Route path="sorting/counting-sort" element={<div>Counting Sort</div>} />
-            <Route path="sorting/bucket-sort" element={<div>Bucket Sort</div>} />
-            <Route path="sorting/radix-sort" element={<div>Redix Sort</div>} />
-        </>
-    </Routes>
+const App = () => (
+    <ProblemsProvider>
+        <Routes>
+            <Route index element={<Home />} />
+            <>
+                <Route path="sorting/bubble-sort" element={<div>Bubble Sort</div>} />
+                <Route path="sorting/selection-sort" element={<div>Selection Sort</div>} />
+                <Route path="sorting/merge-sort" element={<div>Merge Sort</div>} />
+                <Route path="sorting/insertion-sort" element={<div>Insertion Sort</div>} />
+                <Route path="sorting/quick-sort" element={<div>Quick Sort</div>} />
+                <Route path="sorting/heap-sort" element={<div>Heap Sort</div>} />
+                <Route path="sorting/counting-sort" element={<div>Counting Sort</div>} />
+                <Route path="sorting/bucket-sort" element={<div>Bucket Sort</div>} />
+                <Route path="sorting/radix-sort" element={<div>Redix Sort</div>} />
+            </>
+        </Routes>
+    </ProblemsProvider>
+
 );
 
-function Apps() {
-
-    return (
-        <>
-            <ProblemsProvider>
-                <SubApps />
-            </ProblemsProvider>
-        </>
-
-    );
-}
-
-export default Apps;
-
+export default App;
