fbf4043194a5a6326a4e8128edcfc64390c8bf77
Dong Zhou <dozhou@rubiconproject.com>
2022-05-27 15:23:48 -0700

    rename problems to games

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ProblemsProvider } from './problems/commons/ProblemsContext';
+import { ProblemsProvider } from './games/commons/ProblemsContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
