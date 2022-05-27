9e0f81bdb8db6381e9a95173171a3e659478f2e7
Dong Zhou <dozhou@rubiconproject.com>
2022-05-27 13:19:35 -0700

    create app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import { ThemeProvider } from '@mui/material/styles';
 import { Route, Routes } from 'react-router-dom';
-import Home from './list/Home';
+import Home from './pages/list/Home';
 
-import theme from "./theme";
-import Main from "./playground/Main";
+import theme from "./pages/theme";
+import Main from "./pages/playground/Main";
 
-import Logo from './components/Logo';
-import { ProblemsProvider } from '../problems/commons/ProblemsContext';
+import Logo from './pages/components/Logo';
+import { ProblemsProvider } from './problems/commons/ProblemsContext';
 
 const SubApps = () => (
     <Routes>
