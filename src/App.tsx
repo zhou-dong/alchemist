ccdddec1f7e1a3083cc5a39dc956acaae1ae718f
Dong Zhou <dozhou@rubiconproject.com>
2022-05-27 14:43:23 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Lists';
+import Home from './pages';
 
 import { ProblemsProvider } from './problems/commons/ProblemsContext';
 
