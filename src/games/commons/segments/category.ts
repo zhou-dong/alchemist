13f675cdc0a23e30d440d95cbf7123380bff9a8b
Dong Zhou <dozhou@rubiconproject.com>
2022-05-28 00:20:18 -0700

    Create category.ts

---
+++ src/games/commons/segments/category.ts
@@ -0,0 +1,14 @@
+enum Category {
+    Tree = 0,
+    Stack = 1,
+    Queue = 2,
+    Sorting = 3,
+    DynamicProgramming = 4,
+    TwoPointers = 5,
+    HashTable = 6,
+    Math = 7
+}
+
+
+export default Category;
+
