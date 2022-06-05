eaa0d5329408d824643766202b002c3d6af855fd
Dong Zhou <dozhou@rubiconproject.com>
2022-06-04 23:49:42 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import BubbleSortAnimation from "./games/sorting/bubble-sort/Animation";
 import SelectionSortAnimation from "./games/sorting/selection-sort/Animation";
 
 import { createRenderer, createCamera, createScene, onWindowResize } from "./games/sorting/commons/three";
+import Logo from './commons/Logo';
 
 const renderer = createRenderer();
 const camera = createCamera();
@@ -13,25 +14,28 @@ const camera = createCamera();
 window.addEventListener('resize', () => onWindowResize(renderer, camera), false);
 
 const App = () => (
-    <Routes>
-        <Route index element={<Home />} />
+    <>
+        <Logo />
+        <Routes>
+            <Route index element={<Home />} />
 
-        <>
-            <Route path="sorting/bubble-sort" element={<BubbleSortAnimation renderer={renderer} camera={camera} scene={createScene()} values={[6, 5, 4, 3, 2, 1]} />} />
-            <Route path="sorting/selection-sort" element={<SelectionSortAnimation renderer={renderer} camera={camera} scene={createScene()} values={[6, 5, 4, 3, 2, 1]} />} />
-            <Route path="sorting/merge-sort" element={<div>Merge Sort</div>} />
-            <Route path="sorting/insertion-sort" element={<div>Insertion Sort</div>} />
-            <Route path="sorting/quick-sort" element={<div>Quick Sort</div>} />
-            <Route path="sorting/heap-sort" element={<div>Heap Sort</div>} />
-            <Route path="sorting/counting-sort" element={<div>Counting Sort</div>} />
-            <Route path="sorting/bucket-sort" element={<div>Bucket Sort</div>} />
-            <Route path="sorting/radix-sort" element={<div>Redix Sort</div>} />
-        </>
+            <>
+                <Route path="sorting/bubble-sort" element={<BubbleSortAnimation renderer={renderer} camera={camera} scene={createScene()} values={[6, 5, 4, 3, 2, 1]} />} />
+                <Route path="sorting/selection-sort" element={<SelectionSortAnimation renderer={renderer} camera={camera} scene={createScene()} values={[6, 5, 4, 3, 2, 1]} />} />
+                <Route path="sorting/merge-sort" element={<div>Merge Sort</div>} />
+                <Route path="sorting/insertion-sort" element={<div>Insertion Sort</div>} />
+                <Route path="sorting/quick-sort" element={<div>Quick Sort</div>} />
+                <Route path="sorting/heap-sort" element={<div>Heap Sort</div>} />
+                <Route path="sorting/counting-sort" element={<div>Counting Sort</div>} />
+                <Route path="sorting/bucket-sort" element={<div>Bucket Sort</div>} />
+                <Route path="sorting/radix-sort" element={<div>Redix Sort</div>} />
+            </>
 
-        <>
-            <Route path="dp/edit-distance" element={<EditDistance />} />
-        </>
-    </Routes>
+            <>
+                <Route path="dp/edit-distance" element={<EditDistance />} />
+            </>
+        </Routes>
+    </>
 );
 
 export default App;
