ae5b4892c50dd3da728c58badc3a42f4cef11566
Dong Zhou <dozhou@rubiconproject.com>
2022-06-05 18:32:48 -0700

    Create info.ts

---
+++ src/games/sorting/bubble-sort/info.ts
@@ -0,0 +1,11 @@
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+export const bubbleSort: Game = {
+    name: "Bobble Sort",
+    path: "/sorting/bubble-sort",
+    categories: [Category.Sorting],
+    companies: [],
+    difficulties: [Difficulty.Easy],
+}
