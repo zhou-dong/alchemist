ff9a18f1e28624d2bda119176403536de0d6be2e
Dong Zhou <dozhou@rubiconproject.com>
2022-06-22 10:33:37 -0700

    Update index.tsx

--- src/games/dp/edit-distance/index.tsx
+++ src/games/dp/edit-distance/index.tsx
@@ -2,58 +2,12 @@ import * as React from 'react';
 import { AssignmentOutlined, CheckCircleOutline, Code } from '@mui/icons-material';
 import GameWrapper from "../../commons/GameWrapper";
 import { Centered } from "../_components/Centered";
-import DescriptionComponent from "../_components/Description";
-import FormularComponent from "../_components/Formular";
+import Description from "../_components/Description";
+import Formular from "../_components/Formular";
 import { description, example, title, formular, usecases } from "./contents";
 import { IconButton, Tooltip } from '@mui/material';
 import Steps from '../_components/Steps';
 import Errors from '../_components/Errors';
-
-const Formular = () => {
-    const [openFormular, setOpenFormular] = React.useState(false);
-    const handleOpenFormular = () => setOpenFormular(true);
-    const handleCloseFormular = () => setOpenFormular(false);
-
-    return (
-        <>
-            <Tooltip title="CODE" placement='top'>
-                <IconButton onClick={handleOpenFormular}>
-                    <Code />
-                </IconButton>
-            </Tooltip>
-            <FormularComponent
-                title={title}
-                openFormular={openFormular}
-                formular={formular}
-                handleCloseFormular={handleCloseFormular}
-            />
-        </>
-    );
-}
-
-const Description: React.FC<{ success: boolean }> = ({ success }) => {
-    const [openDescription, setOpenDescription] = React.useState(false);
-    const handleOpenDescription = () => setOpenDescription(true);
-    const handleCloseDescription = () => setOpenDescription(false);
-
-    return (
-        <>
-            <Tooltip title="DESCRIPTION" placement="top">
-                <IconButton onClick={handleOpenDescription}>
-                    {success ? <CheckCircleOutline sx={{ color: 'green' }} /> : <AssignmentOutlined />}
-                </IconButton>
-            </Tooltip>
-            <DescriptionComponent
-                title={title}
-                openDialog={openDescription}
-                example={example}
-                usecases={usecases}
-                description={description}
-                handleCloseDialog={handleCloseDescription}
-            />
-        </>
-    );
-}
 
 const EditDistance = () => {
 
@@ -66,8 +20,8 @@ const EditDistance = () => {
             <Centered>
                 <Steps steps={steps} />
                 <Errors errors={errors} />
-                <Description success={success} />
-                <Formular />
+                <Description success={success} title={title} example={example} usecases={usecases} description={description} />
+                <Formular title={title} formular={formular} />
             </Centered>
         </GameWrapper>
     );
