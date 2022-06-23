da5b67a8a2b6afc63dd526c10468c8877d35f368
Dong Zhou <dozhou@rubiconproject.com>
2022-06-23 16:16:18 -0700

    Update styles.ts

--- src/games/dp/_commons/styles.ts
+++ src/games/dp/_commons/styles.ts
@@ -1,7 +1,9 @@
 import helperColor from '@mui/material/colors/green';
 import helperColorSecondary from '@mui/material/colors/blue';
 import helperColorThird from '@mui/material/colors/yellow';
+import React from 'react';
 
 export const helperStyle: React.CSSProperties = { backgroundColor: helperColor[100] };
 export const helperStyleSecondary: React.CSSProperties = { backgroundColor: helperColorSecondary[100] };
 export const helperStyleThird: React.CSSProperties = { backgroundColor: helperColorThird[200] };
+export const errorStyle: React.CSSProperties = { background: "red" }
