ef016a60047aa94a4f1b02a9d239d53909ea45fa
Dong Zhou <dozhou@rubiconproject.com>
2022-06-23 16:47:21 -0700

    Create info.ts

---
+++ src/games/dp/edit-distance/info.ts
@@ -0,0 +1,15 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+import Company from "../../commons/segments/company";
+
+const info: Game = {
+    name: title,
+    path: "/dp/edit-distance",
+    categories: [Category.DynamicProgramming],
+    companies: [Company.Google],
+    difficulties: [Difficulty.Hard],
+}
+
+export default info;
