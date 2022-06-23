eb6679cd09c114d1f1aa8e02af8a78a076316c23
Dong Zhou <dozhou@rubiconproject.com>
2022-06-23 14:22:56 -0700

    Create theme.ts

---
+++ src/games/dp/_commons/theme.ts
@@ -0,0 +1,71 @@
+import { green } from '@mui/material/colors';
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: green[500],
+        }
+    },
+    typography: {
+        fontFamily: [
+            "Circular",
+            "-apple-system",
+            "BlinkMacSystemFont",
+            "Roboto",
+            "Helvetica Neue",
+            "sans-serif",
+            "monospace"
+        ].join(",")
+    },
+    components: {
+        MuiTable: {
+            styleOverrides: {
+                root: {
+                    width: '0',
+                    borderRadius: 12,
+                    display: "inline-table"
+                }
+            }
+        },
+        MuiTableCell: {
+            styleOverrides: {
+                root: {
+                    textAlign: 'center',
+                    borderBottom: 'none',
+                    '&:last-child': {
+                        paddingRight: 0,
+                    },
+                },
+                body: {
+                    borderWidth: 1,
+                    borderStyle: 'solid',
+                    borderColor: "gray",
+                    color: "black",
+                    borderRadius: 0,
+                    fontSize: 14,
+                    fontWeight: 400,
+                    height: 35,
+                    width: 70,
+                    minWidth: 70,
+                    maxWidth: 70
+                },
+                head: {
+                    borderWidth: 1,
+                    borderStyle: 'solid',
+                    borderColor: "gray",
+                    color: "black",
+                    borderRadius: 0,
+                    fontSize: 15,
+                    fontWeight: 600,
+                    height: 35,
+                    width: 70,
+                    minWidth: 70,
+                    maxWidth: 70
+                }
+            }
+        }
+    }
+});
+
+export default theme;
