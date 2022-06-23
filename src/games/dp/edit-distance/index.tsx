f68b0aecdee64f98d3aef5aa4154b39ed60aa0a8
Dong Zhou <dozhou@rubiconproject.com>
2022-06-23 13:01:24 -0700

    Update index.tsx

--- src/games/dp/edit-distance/index.tsx
+++ src/games/dp/edit-distance/index.tsx
@@ -1,27 +1,63 @@
 import * as React from 'react';
-import { AssignmentOutlined, CheckCircleOutline, Code } from '@mui/icons-material';
 import GameWrapper from "../../commons/GameWrapper";
 import { Centered } from "../_components/Centered";
 import Description from "../_components/Description";
-import Formular from "../_components/Formular";
-import { description, example, title, formular, usecases } from "./contents";
-import { IconButton, Tooltip } from '@mui/material';
+import Formula from "../_components/Formula";
+import { description, example, title, formula, usecases } from "./contents";
+import { IconButton, Tooltip, Typography } from '@mui/material';
 import Steps from '../_components/Steps';
 import Errors from '../_components/Errors';
+import Refresh from "../_components/Refresh";
+import { createTableMatrix, createButtonsStyles, createTableStyles } from "./init";
+import Table from '../_components/Table';
+
+const bases = 'ACGT';
+const random = (max: number) => Math.floor(Math.random() * max);
+
+const createRandom = (): string => {
+    return Array(5).fill(bases.length).map(random).map(i => bases[i]).join('');
+}
+
+const buildTableAndStyles = () => {
+    const stringOne = createRandom();
+    const stringTwo = createRandom();
+    const table = createTableMatrix(stringOne, stringTwo);
+    const tableStyles = createTableStyles(stringOne, stringTwo);
+    return { table, tableStyles };
+}
 
 const EditDistance = () => {
+
+    const tableAndStyles = buildTableAndStyles();
 
     const [steps, setSteps] = React.useState(0);
     const [errors, setErrors] = React.useState(0);
     const [success, setSuccess] = React.useState(false);
 
+    const [table, setTable] = React.useState(tableAndStyles.table);
+    const [tableStyles, setTableStyles] = React.useState(tableAndStyles.tableStyles);
+
+    const handleRefresh = () => {
+        const newTableAndStyles = buildTableAndStyles();
+        setTable(newTableAndStyles.table);
+        setTableStyles(newTableAndStyles.tableStyles);
+    }
+
     return (
         <GameWrapper path="/dp/edit-distance">
             <Centered>
-                <Steps steps={steps} />
-                <Errors errors={errors} />
-                <Description success={success} title={title} example={example} usecases={usecases} description={description} />
-                <Formular title={title} formular={formular} />
+                <Typography display="inline" variant='body1'>
+                    {title.toUpperCase()}
+                </Typography>
+
+                <div>
+                    <Steps steps={steps} />
+                    <Errors errors={errors} />
+                    <Description success={success} title={title} example={example} usecases={usecases} description={description} />
+                    <Formula title={title} formula={formula} />
+                    <Refresh handleRefresh={handleRefresh} />
+                </div>
+                <Table table={table} tableStyles={tableStyles} />
             </Centered>
         </GameWrapper>
     );
