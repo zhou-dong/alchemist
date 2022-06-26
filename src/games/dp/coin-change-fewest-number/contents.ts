f8e7ce7ee357e66bebad1f957732c187c0c3544e
Dong Zhou <dozhou@rubiconproject.com>
2022-06-25 17:47:59 -0700

    Create contents.ts

---
+++ src/games/dp/coin-change-fewest-number/contents.ts
@@ -0,0 +1,32 @@
+export const title = 'Coin Change';
+
+export const formula = `
+  if (coin > col) {
+    table[row][col] = table[row - 1][col];
+  } else {
+    table[row][col] = Math.min(
+      table[row][col - coin] + 1, 
+      table[row - 1][col]
+    );
+  }
+`;
+
+export const description = `
+You are given coins of different denominations and a total amount 
+of money amount. Find the fewest number of coins that you need to 
+make up that amount.
+`;
+
+export const usecases = '';
+
+export const example = `
+Greedy Algorithm Does not Always Give Optimal Solution. For instance:
+1. Non optimal solution using Greedy Algorithm:
+    - Given: Coins = {1, 8, 13}, 16 cents = ?
+    - Greedy solution: **4 coins: 13 + 1 + 1 + 1**
+    - Optimal solution: **2 coins: 8 + 8**
+2. Can not result a solution using Greedy Algorithm:
+    - Given: Coins = {2, 8, 15}, 24 cents = ?
+    - Greedy solution: **no solution: coins: 15 + 8 + ? **
+    - Optimal solution: **3 coins: 8 + 8 + 8**
+`;
