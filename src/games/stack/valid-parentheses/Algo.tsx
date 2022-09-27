ccee5547735289c1d7aa214223b1b2fbd4f62a8e
Dong Zhou <dozhou@rubiconproject.com>
2022-09-27 01:38:28 -0700

    Update Algo.tsx

--- src/games/stack/valid-parentheses/Algo.tsx
+++ src/games/stack/valid-parentheses/Algo.tsx
@@ -2,6 +2,22 @@ import * as React from 'react';
 
 
 
+// private buildStackShell() {
+//     const shell = new StackAlgo<Cube>();
+//     const { material, position, size } = this.shellParams;
+//     const { width, height, depth } = this.nodeSize;
+//     const { x, y, z } = position;
+//     for (let i = 0; i < size; i++) {
+//         const geometry = new THREE.BoxGeometry(width, height, depth)
+//         const cube = new Cube(geometry, material, this.scene);
+//         cube.x = x - width * i;
+//         cube.y = y;
+//         cube.z = z;
+//         cube.show();
+//         shell.push(cube);
+//     }
+// }
+
 export default function Algo() {
 
 
