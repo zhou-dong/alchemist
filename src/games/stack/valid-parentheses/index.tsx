ccf5da7d910c0ffe11fe4b8517ee1eafacc614b1
Dong Zhou <dozhou@rubiconproject.com>
2022-10-01 03:03:46 -0700

    Update index.tsx

--- src/games/stack/valid-parentheses/index.tsx
+++ src/games/stack/valid-parentheses/index.tsx
@@ -1,15 +1,10 @@
-import React from 'react';
 import * as THREE from 'three';
 import Algo from "./Algo";
-import Stack from '../../../data-structures/stack';
-import Queue from '../../../data-structures/queue';
-import { clearScene, registerOrbitControls } from '../../../commons/three';
-import { stackShellParams, queueShellParams, nodeSize } from './styles';
-
 import AlgoMenu from "./AlgoMenu";
 import Title from './Title';
 import GameWrapper from '../../commons/GameWrapper';
 import info from "./info";
+import { ContainerProvider } from "./ContainerContext";
 
 interface Props {
     renderer: THREE.Renderer;
@@ -17,53 +12,14 @@ interface Props {
     scene: THREE.Scene;
 }
 
-const duration = 0.5;
-let animationFrameId = -1;
-
 const Main = ({ renderer, camera, scene }: Props) => {
-
-    const [queue, setQueue] = React.useState<Queue<string>>();
-    const [stack, setStack] = React.useState<Stack<string>>();
-
-    function animate() {
-        animationFrameId = requestAnimationFrame(animate);
-        renderer.render(scene, camera);
-    }
-
-    function cancelAnimate() {
-        cancelAnimationFrame(animationFrameId);
-    }
-
-    const ref = React.useRef<HTMLDivElement>(null);
-
-    React.useEffect(() => {
-
-        const init = () => {
-            clearScene(scene);
-            setQueue(new Queue<string>(queueShellParams.position, duration));
-            setStack(new Stack<string>(stackShellParams.position, duration));
-            registerOrbitControls(camera, renderer, scene);
-            renderer.render(scene, camera);
-        }
-
-        if (ref && ref.current) {
-            init();
-            ref.current.appendChild(renderer.domElement);
-        }
-
-        return () => cancelAnimationFrame(animationFrameId);
-    }, [ref, renderer, scene, camera]);
-
     return (
-
         <GameWrapper path={info.path}>
-            <>
+            <ContainerProvider renderer={renderer} camera={camera} scene={scene}>
                 <Title />
                 <AlgoMenu />
-                <div ref={ref}></div>
-                {/* <Algo queue={queue} stack={stack} scene={scene} animate={animate} cancelAnimate={cancelAnimate} /> */}
                 <Algo />
-            </>
+            </ContainerProvider>
         </GameWrapper>
     );
 }
