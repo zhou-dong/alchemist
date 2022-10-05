77b5bc9d4782f2994d2225c97dd95e316ff1b769
Dong Zhou <dozhou@rubiconproject.com>
2022-10-05 06:40:07 -0700

    Update AlgoMap.tsx

--- src/games/stack/valid-parentheses/AlgoMap.tsx
+++ src/games/stack/valid-parentheses/AlgoMap.tsx
@@ -21,34 +21,45 @@ interface Props {
     parenthesisMap: Map<string, string>;
 }
 
-const activedFontStyle = { color: "blue", fontWeight: "bold", fontSize: 30 };
-const inactivedFontStyle = {}
-
-const getFontStyle = (key: string, activeKey: string | null) => {
-    return (activeKey && activeKey === key) ? activedFontStyle : inactivedFontStyle;
+const getHeadCellStyle = (activedKey: string | null, parenthesisMap: Map<string, string>) => {
+    const containsKeyStyle = { width: 50 };
+    const notContainersKeyStyle = { width: 50, color: "red", fontWeight: "bold" };
+    return (activedKey && parenthesisMap.has(activedKey)) ? containsKeyStyle : notContainersKeyStyle;
 }
+
+const getBodyCellStyle = (key: string, activeKey: string | null) => {
+    const activedKeyStyle = { color: "blue", fontSize: 30 };
+    const inactivedKeyStyle = {};
+    return (activeKey && activeKey === key) ? activedKeyStyle : inactivedKeyStyle;
+};
+
+const getIconStyle = (activedKey: string | null, parenthesisMap: Map<string, string>) => {
+    const containsKeyIconStyle = { color: "blue" };
+    const notContainersKeyIconStyle = { color: "red" };
+    return (activedKey && parenthesisMap.has(activedKey)) ? containsKeyIconStyle : notContainersKeyIconStyle;
+};
 
 export default function AlgoMap({ activedKey, parenthesisMap }: Props) {
     return (
         <AlgoMapContainer>
             <Stack direction="row" spacing={1} sx={{ alignItems: "center", marginBottom: 1 }}>
-                <TableViewOutlinedIcon sx={{ color: "rgba(0, 0, 0, 0.54)" }} fontSize="medium" />
+                <TableViewOutlinedIcon sx={{ ...getIconStyle(activedKey, parenthesisMap) }} fontSize="medium" />
                 <Typography variant='body1'>Parentheses Map</Typography>
             </Stack>
             <TableContainer component={Paper} variant="outlined">
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell align="center" sx={{ width: 50 }}>Key</TableCell>
-                            <TableCell align="center" sx={{ borderLeft: "1px solid lightgrey", width: 50 }}>Value</TableCell>
+                            <TableCell align="center" sx={{ ...getHeadCellStyle(activedKey, parenthesisMap), width: 50 }}>Key</TableCell>
+                            <TableCell align="center" sx={{ ...getHeadCellStyle(activedKey, parenthesisMap), borderLeft: "1px solid lightgrey", width: 50 }}>Value</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {
                             Array.from(parenthesisMap.entries()).map(([key, value]: [string, string]) => (
                                 <TableRow key={key} hover>
-                                    <TableCell align="center" sx={{ ...getFontStyle(key, activedKey) }}>{key}</TableCell>
-                                    <TableCell align="center" sx={{ ...getFontStyle(key, activedKey), borderLeft: "1px solid lightgrey" }}>{value}</TableCell>
+                                    <TableCell align="center" sx={{ ...getBodyCellStyle(key, activedKey) }}>{key}</TableCell>
+                                    <TableCell align="center" sx={{ ...getBodyCellStyle(key, activedKey), borderLeft: "1px solid lightgrey" }}>{value}</TableCell>
                                 </TableRow>
                             ))
                         }
