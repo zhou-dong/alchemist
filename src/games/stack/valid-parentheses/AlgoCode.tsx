b4f41cdc2105b8fd7974a5ef230768dc00ef823a
Dong Zhou <dozhou@rubiconproject.com>
2022-10-09 23:06:48 -0700

    Update AlgoCode.tsx

--- src/games/stack/valid-parentheses/AlgoCode.tsx
+++ src/games/stack/valid-parentheses/AlgoCode.tsx
@@ -3,21 +3,40 @@ import * as React from 'react';
 import CodeIcon from '@mui/icons-material/Code';
 import CodeBlock, { languages } from '../../dp/_components/CodeBlock';
 import { formula } from "./contents";
+import { useAlgoContext } from "./AlgoContext";
+import { State } from "./AlgoState";
 
-interface Props {
-    activedKey: string | null;
-    parenthesisMap: Map<string, string>;
+const getHighLightLineNumber = (activedKey: string | null, parenthesisMap: Map<string, string>, state: State): number[] => {
+    if (state === State.Typing || state === State.Finished) {
+        return [];
+    }
+
+    if (!activedKey) {
+        return [19];
+    }
+
+    return parenthesisMap.has(activedKey) ? [14] : [12];
 }
 
-const getHighLightLineNumber = (activedKey: string | null, parenthesisMap: Map<string, string>): number => {
-    return (activedKey && parenthesisMap.has(activedKey)) ? 14 : 12;
-}
+const AlgoCode = () => {
 
-const AlgoCode = ({ activedKey, parenthesisMap }: Props) => {
+    const { activedKey, parenthesisMap, state } = useAlgoContext();
 
-    const highLightLine = getHighLightLineNumber(activedKey, parenthesisMap);
+    const [highLightLine, setHighLightLine] = React.useState<number[]>([]);
+    React.useEffect(() => {
+        setHighLightLine(() => getHighLightLineNumber(activedKey, parenthesisMap, state));
+    }, [activedKey, parenthesisMap, state]);
+
     const [expanded, setExpanded] = React.useState(true);
     const handleChange = () => { setExpanded(!expanded) };
+    React.useEffect(() => {
+        if (state === State.Typing || state === State.Finished) {
+            setExpanded(false);
+        }
+        if (state === State.Playing) {
+            setExpanded(true);
+        }
+    }, [state]);
 
     return (
         <Accordion expanded={expanded} onChange={handleChange}>
@@ -34,7 +53,7 @@ const AlgoCode = ({ activedKey, parenthesisMap }: Props) => {
                     code={formula}
                     language={languages.Typescript}
                     showLineNumbers={true}
-                    linesToHighlight={[highLightLine]}
+                    linesToHighlight={highLightLine}
                     wrapLines={true}
                 />
             </AccordionDetails>
