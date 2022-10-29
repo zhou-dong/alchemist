be05466f36b7166a14e29e1b249f114a88744935
Dong Zhou <dozhou@rubiconproject.com>
2022-10-29 03:29:09 -0700

    Update stackItemBuilder.ts

--- src/games/queue/implement-queue-using-stack/stackItemBuilder.ts
+++ src/games/queue/implement-queue-using-stack/stackItemBuilder.ts
@@ -2,16 +2,17 @@ import * as THREE from 'three';
 import { TextGeometryParameters } from "three/examples/jsm/geometries/TextGeometry";
 import { font } from '../../../commons/three';
 import { TextCube } from '../../../data-structures/_commons/three/text-cube';
+import { node, text } from "./styles";
 
 class StackItemBuilder<T> {
 
     private _value: T;
     private _scene: THREE.Scene;
 
-    private _textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "blue" });
-    private _textGeometryParameters: TextGeometryParameters = { font, size: 0.6, height: 0.1 };
+    private _textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: text.color });
+    private _textGeometryParameters: TextGeometryParameters = { font, size: text.size, height: text.height };
     private _cubeMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "white", opacity: 0, transparent: true });
-    private _cubeGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    private _cubeGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(node.size.width, node.size.height, node.size.depth);
     private _position: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
 
     constructor(value: T, scene: THREE.Scene) {
