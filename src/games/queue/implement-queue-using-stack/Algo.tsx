9c01aa16de15f1604fc7e7e699647fd284824bd9
Dong Zhou <dozhou@rubiconproject.com>
2022-10-29 02:57:53 -0700

    Update Algo.tsx

--- src/games/queue/implement-queue-using-stack/Algo.tsx
+++ src/games/queue/implement-queue-using-stack/Algo.tsx
@@ -1,38 +1,49 @@
-import { Button } from "@mui/material";
+import { Button, ButtonGroup } from "@mui/material";
+import { styled } from '@mui/material/styles';
 import { useAlgoContext } from "./AlgoContext";
 import StackItemBuilder from "./stackItemBuilder";
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
+import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOutlineOutlined';
+import ModeStandbyOutlinedIcon from '@mui/icons-material/ModeStandbyOutlined';
 
 const Enqueue = () => {
 
-    const { stackIn, scene, animate, cancelAnimate } = useAlgoContext();
+    const { stackIn, scene, animate, cancelAnimate, setActionsDisabled } = useAlgoContext();
 
     const handleEnqueue = async () => {
         if (!stackIn) {
             return;
         }
 
+        setActionsDisabled(true);
+
         const item = new StackItemBuilder<string>(1 + "", scene).build();
         item.show();
         animate();
         await stackIn.push(item);
         cancelAnimate();
+
+        setActionsDisabled(false);
     }
 
     return (
         <>
-            <Button onClick={handleEnqueue} variant="contained">enqueue</Button>
+            <Button onClick={handleEnqueue} startIcon={<AddCircleOutlineOutlinedIcon />}>enqueue</Button>
         </>
     );
 }
 
 const Dequeue = () => {
 
-    const { stackIn, stackOut, animate, cancelAnimate } = useAlgoContext();
+    const { stackIn, stackOut, animate, cancelAnimate, setActionsDisabled } = useAlgoContext();
 
     const handleDequeue = async () => {
         if (!stackIn || !stackOut) {
             return;
         }
+
+        setActionsDisabled(true);
 
         animate()
         const stackOutSize = await stackOut.size();
@@ -51,20 +62,51 @@ const Dequeue = () => {
 
         await stackIn.pop();
         cancelAnimate();
+
+        setActionsDisabled(false);
     }
 
     return (
-        <Button onClick={handleDequeue} variant="contained">dequeue</Button>
+        <Button onClick={handleDequeue} startIcon={<RemoveCircleOutlineOutlinedIcon />}>dequeue</Button>
+    )
+}
+
+const Empty = () => {
+
+
+    return (
+        <Button startIcon={<HelpOutlineOutlinedIcon />}>empty</Button>
+    )
+}
+
+const Actions = styled("div")(() => ({
+    width: "100%",
+    textAlign: "center",
+    position: "fixed",
+    bottom: "200px"
+}));
+
+const Peek = () => {
+
+
+    return (
+        <Button startIcon={<ModeStandbyOutlinedIcon />}>peek</Button>
     )
 }
 
 const Main = () => {
 
+    const { actionsDisabled } = useAlgoContext();
+
     return (
-        <>
-            <Enqueue />
-            <Dequeue />
-        </>
+        <Actions>
+            <ButtonGroup variant="contained" size="large" disabled={actionsDisabled}>
+                <Enqueue />
+                <Dequeue />
+                <Peek />
+                <Empty />
+            </ButtonGroup>
+        </Actions>
     );
 }
 
