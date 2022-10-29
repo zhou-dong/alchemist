b07bc60f7f7e6f5a1b9381276a751040c9453cfe
Dong Zhou <dozhou@rubiconproject.com>
2022-10-29 03:37:18 -0700

    Create stackShellBuilder.ts

---
+++ src/games/queue/implement-queue-using-stack/stackShellBuilder.ts
@@ -0,0 +1,45 @@
+import * as THREE from 'three';
+import { Cube } from '../../../data-structures/_commons/three/cube';
+import { node, shell } from "./styles";
+
+class StackShellBuilder {
+
+    private _scene: THREE.Scene;
+
+    private _material: THREE.Material = shell.material;
+    private _geometry: THREE.BoxGeometry = new THREE.BoxGeometry(node.size.width, node.size.height, node.size.depth);
+    private _position: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+
+    constructor(scene: THREE.Scene) {
+        this._scene = scene;
+    }
+
+    position(x: number, y: number, z: number): StackShellBuilder {
+        this._position = new THREE.Vector3(x, y, z);
+        return this;
+    }
+
+    material(material: THREE.Material): StackShellBuilder {
+        this._material = material;
+        return this;
+    }
+
+    geometry(width: number, height: number, depth: number): StackShellBuilder {
+        this._geometry = new THREE.BoxGeometry(width, height, depth);
+        return this;
+    }
+
+    build(): Cube {
+        const item = new Cube(this._geometry, this._material, this._scene);
+        this.setPosition(item);
+        return item;
+    }
+
+    private setPosition(item: Cube): void {
+        item.x = this._position.x;
+        item.y = this._position.y;
+        item.z = this._position.z;
+    }
+}
+
+export default StackShellBuilder;
