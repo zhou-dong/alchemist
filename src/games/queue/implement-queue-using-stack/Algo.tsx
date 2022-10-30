ff837e53832cdb5f20076dc9cda3159bb65f45e9
Dong Zhou <dozhou@rubiconproject.com>
2022-10-29 17:05:33 -0700

    Update Algo.tsx

--- src/games/queue/implement-queue-using-stack/Algo.tsx
+++ src/games/queue/implement-queue-using-stack/Algo.tsx
@@ -9,6 +9,7 @@ import RemoveCircleOutlineOutlinedIcon from '@mui/icons-material/RemoveCircleOut
 import ModeStandbyOutlinedIcon from '@mui/icons-material/ModeStandbyOutlined';
 import Stack from "../../../data-structures/stack";
 import { wait } from "../../../data-structures/_commons/utils";
+import StackShellBuilder from './stackShellBuilder';
 
 const anchorOrigin: PopoverOrigin = {
     vertical: 'top',
@@ -20,17 +21,43 @@ const transformOrigin: PopoverOrigin = {
     horizontal: 'center',
 };
 
+const increaseShells = async (stack: Stack<string>, scene: THREE.Scene) => {
+    const size = await stack.size();
+    const increaseSize = size - stack.shellsLength;
+    for (let i = 0; i < increaseSize; i++) {
+        const shell = new StackShellBuilder(scene, true).build();
+        stack.increaseShells(shell);
+    }
+    if (size === stack.shellsLength) {
+        const shell = new StackShellBuilder(scene, true).build();
+        stack.increaseShells(shell);
+    }
+}
+
+const decreaseShells = async (stack: Stack<string>, minShellSize: number) => {
+    let isDifferent = stack.shellsLength > await stack.size();
+    while (stack.shellsLength > minShellSize && isDifferent) {
+        const shell = stack.decreaseShells();
+        if (shell) {
+            shell.hide();
+        }
+        isDifferent = stack.shellsLength > await stack.size();
+    }
+}
+
 const Enqueue = () => {
 
-    const { stackIn, scene, animate, cancelAnimate, actionsDisabled, setActionsDisabled } = useAlgoContext();
+    const { stackIn, scene, animate, cancelAnimate, actionsDisabled, setActionsDisabled, minShellSize } = useAlgoContext();
 
     const handleEnqueue = async (event: React.MouseEvent<HTMLButtonElement>) => {
         if (!stackIn) {
             return;
         }
         setActionsDisabled(true);
+        const value = event.currentTarget.value;
         animate();
-        await doEnqueue(stackIn, event.currentTarget.value);
+        await increaseShells(stackIn, scene);
+        await doEnqueue(stackIn, value);
         cancelAnimate();
         setActionsDisabled(false);
     }
@@ -75,7 +102,7 @@ const Enqueue = () => {
 
 const Dequeue = () => {
 
-    const { stackIn, stackOut, animate, cancelAnimate, setActionsDisabled } = useAlgoContext();
+    const { stackIn, stackOut, animate, cancelAnimate, setActionsDisabled, scene, minShellSize } = useAlgoContext();
 
     const handleDequeue = async () => {
         if (!stackIn || !stackOut) {
@@ -93,15 +120,21 @@ const Dequeue = () => {
 
         if (isOutEmpty) {
             let item = await inn.pop();
+            await decreaseShells(inn, minShellSize);
+
             while (item) {
+                await increaseShells(out, scene);
                 await out.push(item);
+
                 item = await inn.pop();
+                await decreaseShells(inn, minShellSize);
             }
         }
 
         const item = await out.pop();
         if (item) {
             item.hide();
+            await decreaseShells(out, minShellSize);
             await wait(0.1);
         }
     }
