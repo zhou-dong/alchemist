9b58b42aa2fab0821df12a1df06847b503242525
Dong Zhou <dozhou@rubiconproject.com>
2022-11-22 00:27:18 -0800

    Update Algo.tsx

--- src/games/stack/basic-calculator/Algo.tsx
+++ src/games/stack/basic-calculator/Algo.tsx
@@ -1,12 +1,45 @@
+import ArrowForwardOutlinedIcon from '@mui/icons-material/ArrowForwardOutlined';
+import { Button } from '@mui/material';
+import AlgoExpression from "./AlgoExpression";
+import { useAlgoContext } from './AlgoContext';
+import { State } from './AlgoState';
+
+const Next = () => {
+
+    const { index, setIndex, state } = useAlgoContext();
 
 
-
-const Main = () => {
+    const handleClick = () => {
+        setIndex(i => i + 1);
+    }
 
 
     return (
+
+        <div style={{ width: "100%", textAlign: "center", position: "fixed", bottom: "200px" }}>
+            <Button
+                aria-label="next"
+                size="large"
+                value="next"
+                onClick={handleClick}
+                disabled={state !== State.Playing}
+                variant="contained"
+                endIcon={<ArrowForwardOutlinedIcon />}
+                sx={{ color: "#FFF" }}
+            >
+                next
+            </Button>
+        </div>
+    );
+}
+
+const Main = () => {
+
+    return (
         <>
-            Main
+            <AlgoExpression />
+
+            <Next />
         </>
     )
 };
