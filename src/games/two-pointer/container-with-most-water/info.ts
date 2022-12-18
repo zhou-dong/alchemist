8c86ed8edf02368662b368ed3c0806a3fef5f829
Dong Zhou <dozhou@rubiconproject.com>
2022-12-18 04:22:04 -0800

    Create info.ts

---
+++ src/games/two-pointer/container-with-most-water/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/container-with-most-water",
+    categories: [Category.TwoPointers],
+    companies: [],
+    difficulty: Difficulty.Medium,
+    img: "/img/container-with-most-water.png",
+    leetcodeId: 11
+}
+
+export default info;
