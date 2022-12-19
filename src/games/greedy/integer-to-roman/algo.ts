0effef4f25a3331eaa87c8a9b6a5f859ce29a601
Dong Zhou <dozhou@rubiconproject.com>
2022-12-19 12:08:58 -0800

    Update algo.ts

--- src/games/greedy/integer-to-roman/algo.ts
+++ src/games/greedy/integer-to-roman/algo.ts
@@ -1,112 +1,50 @@
-export enum Action {
-    RemoveSpace,
-    AssignSign,
-    NonDigitCharacter,
-    BiggerThanMax,
-    LessThanMin,
-    Accumulate,
-    NumMultiplySign
-}
-
-interface Data {
-    index: number;
-    sign: number;
-    num: number;
-    linesToHighlight: number[]
+export interface Roman {
+    value: number | string;
+    symbol: number | string;
 }
 
 export interface Item {
-    action: Action;
-    data: Data;
+    i: number;
+    roman: Roman;
+    current: number;
+    next: number;
 }
 
-export function myAtoi(s: string): Item[] {
+export function intToRoman(num: number): Item[] {
 
     const items: Item[] = [];
 
-    const max = Math.pow(2, 31) - 1;
-    const min = Math.pow(-2, 31);
+    const valueSymbols = [
+        [1000, "M"],
+        [900, "CM"],
+        [500, "D"],
+        [400, "CD"],
+        [100, "C"],
+        [90, "XC"],
+        [50, "L"],
+        [40, "XL"],
+        [10, "X"],
+        [9, "IX"],
+        [5, "V"],
+        [4, "IV"],
+        [1, "I"]
+    ];
 
-    function isNumeric(str: string) {
-        const num = parseInt(str);
-        return !isNaN(num);
-    }
+    let roman = "";
 
-    let index = 0;
-    let sign = 1;
-    let num = 0;
+    for (let i = 0; i < valueSymbols.length; i++) {
+        const [value, symbol] = valueSymbols[i];
+        while (num >= value) {
 
-    while (s.charAt(index) === ' ') {
-        items.push({
-            data: { index, sign, num, linesToHighlight: [10] },
-            action: Action.RemoveSpace
-        });
-        index++;
-    }
+            const current = num;
+            const next = num - (+value);
 
-    if (s.charAt(index) === "+" || s.charAt(index) === "-") {
-        if (s.charAt(index) === "-") {
-            sign = -1;
+            roman += symbol;
+            num -= (+value);
+
+            items.push({ i, roman: { value, symbol }, current, next });
         }
-        items.push({
-            data: { index, sign, num, linesToHighlight: [15] },
-            action: Action.AssignSign
-        });
-        index++;
     }
-
-    for (; index < s.length; index++) {
-        if (!isNumeric(s.charAt(index))) {
-            items.push({
-                data: { index, sign, num: num * sign, linesToHighlight: [23] },
-                action: Action.NonDigitCharacter
-            });
-            return items;
-        }
-
-        const digit: number = +s.charAt(index);
-        if (num > ~~(max / 10)) {
-            const value = (sign === 1) ? max : min;
-            const action = (sign === 1) ? Action.BiggerThanMax : Action.LessThanMin;
-            items.push({
-                data: { index, sign, num: value, linesToHighlight: [25] },
-                action
-            });
-            return items;
-        }
-
-        if (num === ~~(max / 10)) {
-            if (sign === 1) {
-                if (digit > (max % 10)) {
-                    items.push({
-                        data: { index, sign, num: max, linesToHighlight: [30] },
-                        action: Action.BiggerThanMax
-                    });
-                    return items;
-                }
-            } else if (sign === -1) {
-                if (digit * -1 < (min % 10)) {
-                    items.push({
-                        data: { index, sign, num: min, linesToHighlight: [32] },
-                        action: Action.LessThanMin
-                    });
-                    return items;
-                }
-            }
-        }
-
-        num = num * 10 + digit;
-
-        items.push({
-            data: { index, sign, num, linesToHighlight: [37] },
-            action: Action.Accumulate
-        });
-    }
-
-    items.push({
-        data: { index, sign, num: num * sign, linesToHighlight: [40] },
-        action: Action.NumMultiplySign
-    });
 
     return items;
 };
