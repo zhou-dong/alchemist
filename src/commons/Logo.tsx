d4b8927e6566142af11304e5e888376c6d0f713a
Dong Zhou <dozhou@rubiconproject.com>
2022-12-24 01:51:17 -0800

    Update Logo.tsx

--- src/commons/Logo.tsx
+++ src/commons/Logo.tsx
@@ -3,23 +3,27 @@ import Typography from '@mui/material/Typography';
 import { Link as RouterLink } from "react-router-dom";
 import MaterialLink from "@mui/material/Link";
 import { green } from '@mui/material/colors';
+import { ThemeProvider } from '@mui/material';
+import theme from './theme';
 
 const Logo = () => (
-    <Toolbar sx={{ position: "fixed" }}>
-        <Typography variant="h6">
-            <MaterialLink
-                component={RouterLink}
-                to="/"
-                sx={{
-                    textDecoration: 'none',
-                    fontWeight: "normal",
-                    color: green[600],
-                }}
-            >
-                Alchemist
-            </MaterialLink>
-        </Typography>
-    </Toolbar>
+    <ThemeProvider theme={theme}>
+        <Toolbar sx={{ position: "fixed" }}>
+            <Typography variant="h6">
+                <MaterialLink
+                    component={RouterLink}
+                    to="/"
+                    sx={{
+                        textDecoration: 'none',
+                        fontWeight: "normal",
+                        color: green[600],
+                    }}
+                >
+                    Alchemist
+                </MaterialLink>
+            </Typography>
+        </Toolbar>
+    </ThemeProvider>
 );
 
 export default Logo;
