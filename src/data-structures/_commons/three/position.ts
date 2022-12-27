f6b4ab2760a72f3cc840d26f4314b72e98054745
Dong Zhou <dozhou@rubiconproject.com>
2022-12-26 18:14:56 -0800

    Create position.ts

---
+++ src/data-structures/_commons/three/position.ts
@@ -0,0 +1,34 @@
+import IPosition from "../position"
+
+export default class Position implements IPosition {
+
+    private mesh: THREE.Mesh;
+
+    constructor(mesh: THREE.Mesh) {
+        this.mesh = mesh;
+    }
+
+    public get x(): number {
+        return this.mesh.position.x;
+    }
+
+    public set x(v: number) {
+        this.mesh.position.setX(v);
+    }
+
+    public get y(): number {
+        return this.mesh.position.y;
+    }
+
+    public set y(v: number) {
+        this.mesh.position.setY(v);
+    }
+
+    public get z(): number {
+        return this.mesh.position.z;
+    }
+
+    public set z(v: number) {
+        this.mesh.position.setZ(v);
+    }
+}
