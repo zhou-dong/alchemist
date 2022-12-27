463f7d9468b27a3a430b317a19352e588a4bb4ed
Dong Zhou <dozhou@rubiconproject.com>
2022-12-26 18:14:40 -0800

    Create move.ts

---
+++ src/data-structures/_commons/three/move.ts
@@ -0,0 +1,18 @@
+import gsap from 'gsap';
+import IMover from "../move";
+import Position from '../position';
+import { wait } from "../utils";
+
+export default class Move implements IMover {
+
+    private mesh: THREE.Mesh;
+
+    constructor(mesh: THREE.Mesh) {
+        this.mesh = mesh;
+    }
+
+    move(position: Position, duration: number): Promise<void> {
+        gsap.to(this.mesh.position, { ...position, duration });
+        return wait(duration);
+    }
+}
