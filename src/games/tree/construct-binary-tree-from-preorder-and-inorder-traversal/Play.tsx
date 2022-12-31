d46b8c8e81b87ebdc3cf16b86cad951bc16a4262
Dong Zhou <dozhou@rubiconproject.com>
2022-12-31 01:57:41 -0800

    Update Play.tsx

--- src/games/tree/construct-binary-tree-from-preorder-and-inorder-traversal/Play.tsx
+++ src/games/tree/construct-binary-tree-from-preorder-and-inorder-traversal/Play.tsx
@@ -1,10 +1,58 @@
+import * as THREE from 'three';
+import { TextGeometryParameters } from "three/examples/jsm/geometries/TextGeometry";
 import { Button, ButtonGroup, Stack, Typography } from "@mui/material";
+import TreeNode from "../../../data-structures/tree/node";
+import { font } from '../../../commons/three';
 import { useAlgoContext } from "./AlgoContext";
+import TextSphere from '../../../data-structures/_commons/sphere/three/text-sphere';
+import { Direction } from './algo';
+import Position from '../../../data-structures/_commons/position';
+
+const duration = 1;
+
+const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(1, 32, 16);
+const sphereMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "yellow", opacity: 0.6, transparent: true });
+const textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "green" });
+const textGeometryParameters: TextGeometryParameters = { font, size: 1, height: 0.1 };
+const lineMaterial = new THREE.LineBasicMaterial({ color: "gold" });
 
 const rootColor = "success";
 const leftColor = "warning";
 const rightColor = "info";
 const defaultColor = "inherit";
+
+const startPosition: THREE.Vector3 = new THREE.Vector3(10, 0, 1);
+
+const buildTreeNode = (value: number, scene: THREE.Scene) => {
+    const textSphere = new TextSphere<number>(
+        value,
+        sphereGeometry,
+        sphereMaterial,
+        textMaterial,
+        textGeometryParameters,
+        scene
+    );
+
+    const { x, y, z } = startPosition;
+    textSphere.center.x = x;
+    textSphere.center.y = y;
+    textSphere.center.z = z;
+
+    switch (value.toString().length) {
+        case 1:
+            textSphere.textPosition.x = x - 1 / 2;
+            break;
+        case 2:
+            textSphere.textPosition.x = x - 3 / 4;
+            break;
+        default:
+            textSphere.textPosition.x = x;
+            break;
+    }
+    textSphere.textPosition.y = y - 1 / 2;
+    textSphere.textPosition.z = z;
+    return new TreeNode<number>(textSphere);
+}
 
 const PreorderDisplay = () => {
     const { inputOutput, index } = useAlgoContext();
@@ -88,16 +136,44 @@ const InorderDisplay = () => {
     )
 }
 
+const map: Map<number, TreeNode<number>> = new Map();
 
 const Next = () => {
-    const { setIndex } = useAlgoContext();
+    const { setIndex, index, inputOutput, scene, animate, cancelAnimate } = useAlgoContext();
+
+    const handleOnClick = async () => {
+        const step = inputOutput.steps[index];
+        if (!step) {
+            return;
+        }
+        const { node, parent, direction } = step;
+        const treeNode = buildTreeNode(node.val, scene);
+
+        map.set(node.val, treeNode);
+
+        animate();
+        treeNode.show();
+
+        if (parent && map.has(parent.val) && direction !== undefined) {
+            const { x, y, z } = map.get(parent.val)!.val!.center;
+            const parentNode = map.get(parent.val)!;
+            if (direction === Direction.Left) {
+                const position: Position = { x: x - 3, y: y - 3, z };
+                await parentNode.setLeft(treeNode, position, lineMaterial, duration, scene);
+            } else {
+                const position: Position = { x: x + 3, y: y - 3, z };
+                await parentNode.setRight(treeNode, position, lineMaterial, duration, scene);
+            }
+        } else {
+            await treeNode.val.move({ x: 0, y: 8, z: 0 }, 1);
+        }
+
+        cancelAnimate();
+        setIndex(i => i + 1);
+    }
 
     return (
-        <Button onClick={() => {
-
-            setIndex(i => i + 1);
-
-        }}>
+        <Button onClick={handleOnClick} variant="contained">
             Next
         </Button>
     )
@@ -105,12 +181,9 @@ const Next = () => {
 
 const Main = () => {
 
-
-
     return (
-        <Stack direction="column" style={{ justifyContent: "center", alignItems: "center", position: "fixed", width: "100%", top: "140px" }} spacing={2}>
+        <Stack direction="column" style={{ display: "flex", alignItems: "flex-end", width: "95%", position: "fixed", top: "100px" }} spacing={2}>
             <PreorderDisplay />
-            {/* <div /> */}
             <InorderDisplay />
             <Next />
         </Stack>
