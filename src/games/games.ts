7389dc6d0e5fbfa18e703937cc8ed482db997356
Dong Zhou <dozhou@rubiconproject.com>
2023-01-01 14:08:05 -0800

    Update games.ts

--- src/games/games.ts
+++ src/games/games.ts
@@ -50,6 +50,7 @@ import IntegerToRoman from "./greedy/integer-to-roman/info";
 import RomanToInteger from "./math/roman-to-integer/info";
 import MaximumDepthOfBinaryTree from "./tree/maximum-depth-of-binary-tree/info";
 import ConstructBinaryTreeFromPreorderAndInorderTraversal from "./tree/construct-binary-tree-from-preorder-and-inorder-traversal/info";
+import ConstructBinaryTreeFromInorderAndPostorderTraversal from "./tree/construct-binary-tree-from-inorder-and-postorder-traversal/info";
 
 export const games: Game[] = [
     editDistance,
@@ -101,5 +102,6 @@ export const games: Game[] = [
     IntegerToRoman,
     RomanToInteger,
     MaximumDepthOfBinaryTree,
-    ConstructBinaryTreeFromPreorderAndInorderTraversal
+    ConstructBinaryTreeFromPreorderAndInorderTraversal,
+    ConstructBinaryTreeFromInorderAndPostorderTraversal
 ];
