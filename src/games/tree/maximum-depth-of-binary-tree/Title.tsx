4e5985e17a650dcb08012d8d339d09c06ba42181
Dong Zhou <dozhou@rubiconproject.com>
2023-01-01 14:19:23 -0800

    Update Title.tsx

--- src/games/tree/maximum-depth-of-binary-tree/Title.tsx
+++ src/games/tree/maximum-depth-of-binary-tree/Title.tsx
@@ -1,13 +1,18 @@
 import Box from '@mui/material/Box';
+import { CheckCircleOutline } from '@mui/icons-material';
 import { Typography } from '@mui/material';
 import { title } from "./contents";
+import { useAlgoContext } from './AlgoContext';
+import { State } from './AlgoState';
 
 export default function Title() {
+
+    const { state } = useAlgoContext();
+
     return (
         <Box sx={{
             position: "fixed",
             top: 40,
-            flexGrow: 1,
             width: "100%",
             display: 'flex',
             alignItems: "center",
@@ -16,6 +21,7 @@ export default function Title() {
             <Typography variant='body1'>
                 {title}
             </Typography>
+            {state === State.Finished && <CheckCircleOutline sx={{ color: 'green' }} />}
         </Box>
     );
 }
