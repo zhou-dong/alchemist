e454e6d4abe7bcc8e3ff4795d3b31e717275ba68
Dong Zhou <dozhou@rubiconproject.com>
2023-01-03 14:54:17 -0800

    Create info.ts

---
+++ src/games/tree/minimum-depth-of-binary-tree/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/minimum-depth-of-binary-tree",
+    categories: [Category.Tree],
+    companies: [],
+    difficulty: Difficulty.Easy,
+    img: "/img/minimum-depth-of-binary-tree.png",
+    leetcodeId: 111
+}
+
+export default info;
