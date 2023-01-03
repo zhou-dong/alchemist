e35368aeb0cdf0ce537211e8f7e4d0567886e8d8
Dong Zhou <dozhou@rubiconproject.com>
2023-01-03 15:48:10 -0800

    Create algo.ts

---
+++ src/games/tree/minimum-depth-of-binary-tree/algo.ts
@@ -0,0 +1,44 @@
+import TreeNode from "../../../data-structures/tree/node";
+
+export enum Direction {
+    Left, Right, Back
+}
+
+export interface Step {
+    node: TreeNode<any>;
+    depth?: number;
+    direction?: Direction;
+}
+
+export function buildSteps<T>(root?: TreeNode<T>): Step[] {
+    const steps: Step[] = [];
+
+    function minDepth<T>(node?: TreeNode<T>, direction?: Direction, depth?: number): number {
+        if (node === undefined) {
+            return 0;
+        }
+
+        steps.push({ node, direction, depth });
+
+        const left = minDepth(node.left, Direction.Left, depth);
+        const right = minDepth(node.right, Direction.Right, depth);
+
+        if (node.left === undefined) {
+            steps.push({ node, direction: Direction.Back, depth: right + 1 });
+            return right + 1;
+        }
+
+        if (node.right === undefined) {
+            steps.push({ node, direction: Direction.Back, depth: left + 1 });
+            return left + 1;
+        }
+
+        const min = Math.min(left, right) + 1;
+        steps.push({ node, direction: Direction.Back, depth: min });
+
+        return min;
+    };
+
+    minDepth(root);
+    return steps;
+}
