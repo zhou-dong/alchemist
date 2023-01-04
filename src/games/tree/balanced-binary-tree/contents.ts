d590899082c149b8ab86e291a2240858fbb12021
Dong Zhou <dozhou@rubiconproject.com>
2023-01-03 17:55:57 -0800

    Create contents.ts

---
+++ src/games/tree/balanced-binary-tree/contents.ts
@@ -0,0 +1,54 @@
+export const title = "Balanced Binary Tree";
+
+export const formula = `/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isBalanced(root: TreeNode | null): boolean {
+
+    function height(node: TreeNode | null): number {
+        if (node === null) {
+            return 0;
+        }
+
+        const left = height(node.left);
+        if (left === -1) {
+            return -1;
+        }
+
+        const right = height(node.right);
+        if (right === -1) {
+            return -1;
+        }
+
+        const different = Math.abs(right - left);
+
+        if (different > 1) {
+            return -1;
+        }
+
+        return Math.max(left, right) + 1;
+    }
+
+    return height(root) !== -1;
+};`;
+
+export const description = `
+Given a binary tree, determine if it is ***height-balanced***.
+`;
+
+export const solution = ``;
+
+export const usecases = '';
+
+export const example = ``;
