37b4fd9970299f0947a7fa681c2047950b420b4e
Dong Zhou <dozhou@rubiconproject.com>
2023-01-04 01:03:23 -0800

    Update AlgoContext.tsx

--- src/games/tree/convert-sorted-array-to-binary-search-tree/AlgoContext.tsx
+++ src/games/tree/convert-sorted-array-to-binary-search-tree/AlgoContext.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import * as THREE from 'three';
 import { clearScene, registerOrbitControls } from '../../../commons/three';
-import { Step } from "./algo";
+import { InputOutput } from "./algo";
 import { State } from "./AlgoState";
+import TreeNode from "../../../data-structures/tree/node";
+
+const defaultInputOutput: InputOutput = { input: [], steps: [], xAxis: [] };
 
 const AlgoContext = React.createContext<{
     state: State,
@@ -10,20 +13,24 @@ const AlgoContext = React.createContext<{
     scene: THREE.Scene,
     animate: () => void,
     cancelAnimate: () => void,
-    steps: Step[],
-    setSteps: React.Dispatch<React.SetStateAction<Step[]>>,
+    inputOutput: InputOutput,
+    setInputOutput: React.Dispatch<React.SetStateAction<InputOutput>>,
     index: number,
     setIndex: React.Dispatch<React.SetStateAction<number>>,
+    map: Map<number, TreeNode<string>>,
+    setMap: React.Dispatch<React.SetStateAction<Map<number, TreeNode<string>>>>,
 }>({
     state: State.Typing,
     setState: () => { },
     scene: new THREE.Scene(),
     animate: () => { },
     cancelAnimate: () => { },
-    steps: [],
-    setSteps: () => { },
+    inputOutput: defaultInputOutput,
+    setInputOutput: () => { },
     index: 0,
     setIndex: () => { },
+    map: new Map(),
+    setMap: () => { }
 });
 
 let animationFrameId = -1;
@@ -35,10 +42,11 @@ export const AlgoContextProvider: React.FC<{
     scene: THREE.Scene,
 }> = ({ children, renderer, camera, scene }) => {
 
-    camera.position.z = 15;
+    camera.position.z = 20;
     const [state, setState] = React.useState(State.Typing);
-    const [steps, setSteps] = React.useState<Step[]>([]);
+    const [inputOutput, setInputOutput] = React.useState<InputOutput>(defaultInputOutput);
     const [index, setIndex] = React.useState(0);
+    const [map, setMap] = React.useState<Map<number, TreeNode<string>>>(new Map());
 
     function animate() {
         animationFrameId = requestAnimationFrame(animate);
@@ -70,10 +78,12 @@ export const AlgoContextProvider: React.FC<{
             scene,
             animate,
             cancelAnimate,
-            steps,
-            setSteps,
+            inputOutput,
+            setInputOutput,
             index,
             setIndex,
+            map,
+            setMap
         }}>
             {children}
             <div ref={ref}></div>
