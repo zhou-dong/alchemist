e1152e9b33c11553bc87da714958a818d921d9b4
Dong Zhou <dozhou@rubiconproject.com>
2023-01-04 04:18:46 -0800

    Update contents.ts

--- src/games/tree/validate-binary-search-tree/contents.ts
+++ src/games/tree/validate-binary-search-tree/contents.ts
@@ -1,6 +1,35 @@
-export const title = "";
+export const title = "Validate Binary Search Tree";
 
-export const formula = ``;
+export const formula = `/**
+* Definition for a binary tree node.
+* class TreeNode {
+*     val: number
+*     left: TreeNode | null
+*     right: TreeNode | null
+*     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+*         this.val = (val===undefined ? 0 : val)
+*         this.left = (left===undefined ? null : left)
+*         this.right = (right===undefined ? null : right)
+*     }
+* }
+*
+
+function isValidBST(root: TreeNode | null): boolean {
+
+    function isBST(root: TreeNode | null, lower: number, upper: number): boolean {
+        if (root === null) {
+            return true;
+        }
+
+        if (root.val <= lower || root.val >= upper) {
+            return false;
+        }
+
+        return isBST(root.left, lower, root.val) && isBST(root.right, root.val, upper);
+    }
+
+    return isBST(root, -Infinity, Infinity);
+};`;
 
 export const description = `
 Given the **root** of a binary tree, determine if it is a valid binary search tree (BST).
