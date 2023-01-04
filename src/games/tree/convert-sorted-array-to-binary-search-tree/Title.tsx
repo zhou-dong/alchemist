4d5d66ba8e902a5ee594c2cb23bb8cd06f3a4be3
Dong Zhou <dozhou@rubiconproject.com>
2023-01-04 02:54:43 -0800

    Update Title.tsx

--- src/games/tree/convert-sorted-array-to-binary-search-tree/Title.tsx
+++ src/games/tree/convert-sorted-array-to-binary-search-tree/Title.tsx
@@ -1,6 +1,5 @@
-import Box from '@mui/material/Box';
 import { CheckCircleOutline } from '@mui/icons-material';
-import { Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import { title } from "./contents";
 import { useAlgoContext } from './AlgoContext';
 import { State } from './AlgoState';
@@ -10,11 +9,11 @@ export default function Title() {
     const { state } = useAlgoContext();
 
     return (
-        <Box sx={{ textAlign: "center" }}>
+        <Stack sx={{ alignItems: "center", justifyContent: "center" }} direction="row">
             <Typography variant='body1' display="inline">
                 {title}
             </Typography>
             {state === State.Finished && <CheckCircleOutline sx={{ color: 'green' }} />}
-        </Box>
+        </Stack>
     );
 }
