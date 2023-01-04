a2f553dd3bb719cc434ec82d29a9fc26f4e38f28
Dong Zhou <dozhou@rubiconproject.com>
2023-01-04 01:02:53 -0800

    Update algo.ts

--- src/games/tree/convert-sorted-array-to-binary-search-tree/algo.ts
+++ src/games/tree/convert-sorted-array-to-binary-search-tree/algo.ts
@@ -1,3 +1,6 @@
+import { buildPerfectBinaryTree } from "../../../data-structures/tree/perfectBinaryTree";
+import { xAxisAlpha } from "./styles";
+
 export enum Direction {
     Left, Right, Back
 }
@@ -9,6 +12,12 @@ interface Node {
     right?: Node;
 }
 
+export interface InputOutput {
+    input: string[];
+    steps: Step[];
+    xAxis: number[];
+}
+
 export interface Step {
     node: Node;
     left: number;
@@ -17,7 +26,14 @@ export interface Step {
     direction?: Direction;
 }
 
-export function buildSteps<T>(input: string[]): Step[] {
+const calDepth = (root?: Node): number => {
+    if (root === undefined) return 0;
+    const left = calDepth(root.left);
+    const right = calDepth(root.right);
+    return Math.max(right, left) + 1;
+}
+
+export function buildSteps(input: string[]): InputOutput {
     const steps: Step[] = [];
 
     function buildTree(values: string[], left: number, right: number, index: number, direction?: Direction): Node | undefined {
@@ -32,10 +48,12 @@ export function buildSteps<T>(input: string[]): Step[] {
 
         node.left = buildTree(values, left, mid - 1, 2 * index + 1, Direction.Left);
         node.right = buildTree(values, mid + 1, right, 2 * index + 2, Direction.Right);
-
         return node;
     };
 
-    buildTree(input, 0, input.length - 1, 0);
-    return steps;
+    const root = buildTree(input, 0, input.length - 1, 0);
+    const depth: number = calDepth(root);
+    const xAxis: number[] = buildPerfectBinaryTree(depth, xAxisAlpha).map(node => node.x);
+
+    return { input, steps, xAxis };
 }
