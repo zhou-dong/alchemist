2099b9638b419dc6a517557dd8ff15d1228e882d
Dong Zhou <dozhou@rubiconproject.com>
2023-01-03 21:30:37 -0800

    Create info.ts

---
+++ src/games/tree/convert-sorted-array-to-binary-search-tree/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/convert-sorted-array-to-binary-search-tree",
+    categories: [Category.Tree],
+    companies: [],
+    difficulty: Difficulty.Easy,
+    img: "/img/convert-sorted-array-to-binary-search-tree.png",
+    leetcodeId: 108
+}
+
+export default info;
