b823cada6cb2b397ac643bd033a4d8127025c1e8
Dong Zhou <dozhou@rubiconproject.com>
2023-01-06 01:03:00 -0800

    Create algo.ts

---
+++ src/games/tree/same-tree/algo.ts
@@ -0,0 +1,53 @@
+import TreeNode from "../../../data-structures/tree/node";
+
+export enum Direction {
+    Left, Right, Back
+}
+
+export interface Step {
+    p?: TreeNode<string>;
+    q?: TreeNode<string>;
+    isSame?: boolean;
+    direction?: Direction;
+}
+
+export function buildSteps(p?: TreeNode<string>, q?: TreeNode<string>): Step[] {
+    const steps: Step[] = [];
+
+    function isSameTree(p?: TreeNode<string>, q?: TreeNode<string>, direction?: Direction): boolean {
+
+        if (p === undefined && q === undefined) {
+            return true;
+        }
+
+        if (p === undefined || q === undefined) {
+            steps.push({ p, q, direction, isSame: false });
+            return false;
+        }
+
+        if (p.val.value !== q.val.value) {
+            steps.push({ p, q, direction, isSame: false });
+            return false;
+        }
+
+        steps.push({ p, q, direction, isSame: true });
+
+        const isLeftTreeSame = isSameTree(p.left, q.left, Direction.Left);
+        if (!isLeftTreeSame) {
+            steps.push({ p, q, direction, isSame: false });
+            return false;
+        }
+
+        const isRightTreeSame = isSameTree(p.right, q.right, Direction.Right);
+        if (!isRightTreeSame) {
+            steps.push({ p, q, direction, isSame: false });
+            return false;
+        }
+
+        steps.push({ p, q, direction, isSame: true });
+        return true;
+    };
+
+    isSameTree(p, q);
+    return steps;
+}
