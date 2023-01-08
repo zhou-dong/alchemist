f759023b68c384e413481483908bca6e2c4eaa14
Dong Zhou <dozhou@rubiconproject.com>
2023-01-08 01:32:31 -0800

    Update AlgoInput.tsx

--- src/games/tree/lowest-common-ancestor-of-a-binary-tree/AlgoInput.tsx
+++ src/games/tree/lowest-common-ancestor-of-a-binary-tree/AlgoInput.tsx
@@ -15,25 +15,40 @@ import { State } from './AlgoState';
 import { buildSteps } from "./algo";
 import { clearScene } from '../../../commons/three';
 import { wait } from '../../../data-structures/_commons/utils';
-import { buildTree } from "./styles";
+import { buildTree, pqColor } from "./styles";
+import TreeNode from '../../../data-structures/tree/node';
 
-const input1 = [3, 9, 9, 5, 6, 6, 5, 4, null, 8, null, null, null, null, 4];
-const input2 = [3, 9, 9, 5, 6, 6, 5, 4, null, 8, null, null, 8, null, 4];
-const input3 = [1, 2, 2, null, 3, null, 3];
-const input4 = [1, 2, 2, 3, 4, 4, 3];
+const input1 = {
+    array: [15, 7, 30, 4, 9, 20, 35, 2, null, 8, 12, null, null, 32, 40],
+    p: 8,
+    q: 32
+}
+
+const input2 = {
+    array: [15, 7, 30, 4, 9, 20, 35, 2, null, 8, 12, null, null, 32, 40],
+    p: 8,
+    q: 12
+}
+
+const input3 = {
+    array: [15, 7, 30, 4, 9, 20, 35, 2, null, 8, 12, null, null, 32, 40],
+    p: 4,
+    q: 8
+}
 
 const DropDown: React.FC<{
     anchorEl: HTMLElement | null,
     setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>,
     open: boolean,
-    setValue: React.Dispatch<React.SetStateAction<string>>,
-}> = ({ anchorEl, setAnchorEl, open, setValue, }) => {
+    setNodes: React.Dispatch<React.SetStateAction<string>>,
+    setP: React.Dispatch<React.SetStateAction<string>>,
+    setQ: React.Dispatch<React.SetStateAction<string>>
+}> = ({ anchorEl, setAnchorEl, open, setNodes, setP, setQ }) => {
 
     const buildInInputs = [
         input1,
         input2,
         input3,
-        input4,
     ];
 
     const handleMenuClose = () => {
@@ -51,8 +66,11 @@ const DropDown: React.FC<{
                     <MenuItem
                         key={index}
                         onClick={() => {
+                            const { array, p, q } = item;
                             handleMenuClose();
-                            setValue(item.join(","));
+                            setNodes(array.join(","));
+                            setP(p + "");
+                            setQ(q + "");
                         }}
                         sx={{ width: "408px", overflow: "hidden" }}
                     >
@@ -60,7 +78,13 @@ const DropDown: React.FC<{
                             <InputIcon fontSize="small" />
                         </ListItemIcon>
                         <ListItemText>
-                            {item.join(",")}
+                            {item.array.join(",")}
+                        </ListItemText>
+                        <ListItemText>
+                            {item.p}
+                        </ListItemText>
+                        <ListItemText>
+                            {item.q}
                         </ListItemText>
                     </MenuItem>
                 ))
@@ -69,44 +93,87 @@ const DropDown: React.FC<{
     );
 }
 
-const parseInput = (input: string): (string | null)[] => {
+const parseInput = (input: string): (number | null)[] => {
     return input.split(",").map(ch => {
         switch (ch) {
             case "": return null;
             case "null": return null;
             case "undefined": return null;
             case undefined: return null;
-            default: return ch;
+            default: return +ch;
         }
     });
 }
 
-const Submit: React.FC<{
-    value: string,
-    setValue: React.Dispatch<React.SetStateAction<string>>,
-    setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
-}> = ({ value, setValue, setAnchorEl }) => {
-    const { scene, animate, cancelAnimate, setState, setRoot, setSteps, setIndex } = useAlgoContext();
+const enablePQColor = (p: number, q: number, node?: TreeNode<number>) => {
+    if (node === undefined) {
+        return;
+    }
 
-    const disabled = value.trim().length === 0;
+    if (node.val.value === p || node.val.value === q) {
+        node.sphereColor = pqColor;
+    }
+
+    enablePQColor(p, q, node.left);
+    enablePQColor(p, q, node.right);
+
+}
+
+const findNode = (value: number, root?: TreeNode<number>,): TreeNode<number> | undefined => {
+    if (root === undefined) {
+        return undefined;
+    }
+
+    if (root.val.value === value) {
+        return root;
+    }
+
+    return findNode(value, root.left) || findNode(value, root.right);
+}
+
+const Submit: React.FC<{
+    nodes: string,
+    setNodes: React.Dispatch<React.SetStateAction<string>>,
+    p: string,
+    setP: React.Dispatch<React.SetStateAction<string>>,
+    q: string,
+    setQ: React.Dispatch<React.SetStateAction<string>>
+    setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
+}> = ({ nodes, p, q, setNodes, setP, setQ, setAnchorEl }) => {
+    const { scene, animate, cancelAnimate, setState, setRoot, setSteps, setIndex, setCommonAncestors } = useAlgoContext();
+
+    const disabled = nodes.trim().length === 0 || p.trim().length === 0 || q.trim().length === 0;
 
     const handleSubmit = async () => {
         setState(State.Typing);
-        const array = parseInput(value);
-
+        const array = parseInput(nodes);
         animate();
-
         clearScene(scene);
         const root = buildTree(array, scene);
-        const steps = buildSteps(root);
+        const steps = buildSteps(+p, +q, root);
         setRoot(root);
         setSteps(steps);
+        enablePQColor(+p, +q, root);
+        const pNode = findNode(+p, root);
+        const qNode = findNode(+q, root);
+        if (pNode) {
+            setCommonAncestors(nodes => {
+                nodes.push(pNode);
+                return nodes;
+            });
+        }
+        if (qNode) {
+            setCommonAncestors(nodes => {
+                nodes.push(qNode);
+                return nodes;
+            });
+        }
         setIndex(0);
-
-        setValue("");
+        setNodes("");
+        setP("");
+        setQ("");
         setAnchorEl(null);
         await wait(0.2);
-
         cancelAnimate();
         setState(State.Playing);
     }
@@ -124,7 +191,21 @@ interface Props {
 
 export default function AlgoInput({ setAnchorEl }: Props) {
 
-    const [value, setValue] = React.useState("");
+    const [nodes, setNodes] = React.useState("");
+    const [p, setP] = React.useState("");
+    const [q, setQ] = React.useState("");
+
+    const handleNodesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setNodes(e.currentTarget.value);
+    };
+
+    const handlePChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setP(e.currentTarget.value);
+    };
+
+    const handleQChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setQ(e.currentTarget.value);
+    };
 
     const reference = React.useRef(null);
     const [menuAnchorEl, setMenuAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -136,11 +217,6 @@ export default function AlgoInput({ setAnchorEl }: Props) {
         }
     };
 
-    const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const text: string = e.currentTarget.value;
-        setValue(text);
-    };
-
     return (
         <>
             <Paper
@@ -150,7 +226,7 @@ export default function AlgoInput({ setAnchorEl }: Props) {
                 sx={{
                     p: '2px 4px',
                     display: 'flex',
-                    width: 400,
+                    width: 500,
                     alignItems: "center"
                 }}
             >
@@ -159,26 +235,49 @@ export default function AlgoInput({ setAnchorEl }: Props) {
                 </IconButton>
 
                 <InputBase
-                    sx={{ ml: 1, flex: 1 }}
+                    sx={{ ml: 1, flex: 1, }}
                     placeholder='Tree nodes, seprate by ","'
-                    value={value}
-                    onChange={handleValueChange}
+                    value={nodes}
+                    onChange={handleNodesChange}
                 />
                 <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+                <InputBase
+                    sx={{ ml: 1, width: 50, }}
+                    placeholder='p'
+                    value={p}
+                    onChange={handlePChange}
+                    type="number"
+                />
+                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
+                <InputBase
+                    sx={{ ml: 1, width: 50, }}
+                    placeholder='q'
+                    value={q}
+                    onChange={handleQChange}
+                    type="number"
+                />
+                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+
                 <IconButton type="button" sx={{ p: '10px' }} aria-label="clear" onClick={() => {
-                    setValue("");
+                    setNodes("");
+                    setP("");
+                    setQ("");
                 }}>
                     <ClearIcon />
                 </IconButton>
 
-                <Submit value={value} setValue={setValue} setAnchorEl={setAnchorEl} />
+                <Submit nodes={nodes} setNodes={setNodes} p={p} q={q} setP={setP} setQ={setQ} setAnchorEl={setAnchorEl} />
             </Paper>
 
             <DropDown
                 anchorEl={menuAnchorEl}
                 setAnchorEl={setMenuAnchorEl}
                 open={open}
-                setValue={setValue}
+                setNodes={setNodes}
+                setP={setP}
+                setQ={setQ}
             />
         </>
     );
