fbc7f9a9541ef2cd67ff2c2f5ad4f062821f1ccf
Dong Zhou <dozhou@rubiconproject.com>
2023-01-08 23:44:47 -0800

    Create info.ts

---
+++ src/games/tree/populating-next-right-pointers-in-each-node-ii/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/populating-next-right-pointers-in-each-node-ii",
+    categories: [Category.Tree],
+    companies: [],
+    difficulty: Difficulty.Medium,
+    img: "/img/populating-next-right-pointers-in-each-node-ii.png",
+    leetcodeId: 117
+}
+
+export default info;
