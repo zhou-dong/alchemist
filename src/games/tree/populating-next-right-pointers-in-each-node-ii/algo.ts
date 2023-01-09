26f480e6f34d80761c93810a635b836183609707
Dong Zhou <dozhou@rubiconproject.com>
2023-01-08 23:45:15 -0800

    Create algo.ts

---
+++ src/games/tree/populating-next-right-pointers-in-each-node-ii/algo.ts
@@ -0,0 +1,26 @@
+import TreeNode from "../../../data-structures/tree/node";
+
+export enum Direction {
+    Left, Right, Back
+}
+
+export interface Step {
+    node?: TreeNode<string>;
+    direction?: Direction;
+}
+
+export function buildSteps(root?: TreeNode<string>): Step[] {
+    const steps: Step[] = [];
+
+    function connect(node?: TreeNode<string>, direction?: Direction) {
+        if (node === undefined) {
+            return;
+        }
+        steps.push({ node, direction });
+        connect(node.right, Direction.Right); // go right first
+        connect(node.left, Direction.Left);
+    }
+
+    connect(root);
+    return steps;
+}
