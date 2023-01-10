71231089db05010d32555f25e35e090914f60fc8
Dong Zhou <dozhou@rubiconproject.com>
2023-01-09 22:15:35 -0800

    Update contents.ts

--- src/games/tree/kth-smallest-element-in-a-bst/contents.ts
+++ src/games/tree/kth-smallest-element-in-a-bst/contents.ts
@@ -1,7 +1,43 @@
 export const title = "Kth Smallest Element in a BST";
 
-export const formula = `
-`;
+export const formula = `/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// inorder
+function kthSmallest(root: TreeNode | null, k: number): number {
+    let index = 0;
+    let value = 0;
+
+    function dfs(node: TreeNode | null) {
+        if (node === null) {
+            return;
+        }
+
+        dfs(node.left);
+
+        index = index + 1;
+        if (index === k) {
+            value = node.val;
+            return;
+        }
+
+        dfs(node.right);
+    }
+
+    dfs(root);
+    return value;
+};`;
 
 export const description = `
 Given the **root** of a binary search tree, and an integer **k**, return the **kth** smallest value ***(1-indexed)*** of all the values of the nodes in the tree.
