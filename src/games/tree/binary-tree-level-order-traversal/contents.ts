2ab1c772afd9fa849190fc99b849f444e33ddd92
Dong Zhou <dozhou@rubiconproject.com>
2023-01-14 11:18:28 -0800

    binary tree level order traversal

---
+++ src/games/tree/binary-tree-level-order-traversal/contents.ts
@@ -0,0 +1,28 @@
+export const title = "Binary Tree Level Order Traversal";
+
+export const formula = `/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+
+`;
+
+export const description = `
+Given the **root** of a binary tree, return the ***level order traversal of its nodes' values***. (i.e., from left to right, level by level).
+`;
+
+export const solution = ``;
+
+export const usecases = '';
+
+export const example = ``;
