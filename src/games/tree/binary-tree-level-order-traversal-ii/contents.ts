64d5f94b22bc81713901b9c274d7f0e588a206f4
Dong Zhou <dozhou@rubiconproject.com>
2023-01-15 20:12:58 -0800

    Update contents.ts

--- src/games/tree/binary-tree-level-order-traversal-ii/contents.ts
+++ src/games/tree/binary-tree-level-order-traversal-ii/contents.ts
@@ -1,19 +1,5 @@
 export const title = "Binary Tree Level Order Traversal II";
 
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
 export const formula = `/**
  * Definition for a binary tree node.
  * class TreeNode {
