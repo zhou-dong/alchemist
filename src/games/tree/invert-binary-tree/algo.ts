162c642cd9076860c50d897146197dbe24c46c24
Dong Zhou <dozhou@rubiconproject.com>
2023-01-16 04:16:11 -0800

    init invert-binary-tree

---
+++ src/games/tree/invert-binary-tree/algo.ts
@@ -0,0 +1,46 @@
+import TreeNode from "../../../data-structures/tree/node";
+
+export enum Direction {
+    Left, Right, Back
+}
+
+export interface Step {
+    node: TreeNode<number>;
+    sum: number;
+    isLeaf: boolean;
+    total: number;
+    leaves: number[];
+}
+
+function isLeaf(node: TreeNode<number>) {
+    return !node.left && !node.right;
+}
+
+export function buildSteps(root?: TreeNode<number>): Step[] {
+
+    const steps: Step[] = [];
+
+    let total = 0;
+    const leaves: number[] = [];
+    function sumNumbers(node: TreeNode<number> | undefined, num: number) {
+        if (!node) {
+            return;
+        }
+
+        const sum = num * 10 + node.val.value;
+        if (isLeaf(node)) {
+            total += sum;
+            leaves.push(sum);
+            steps.push({ node, sum, total, isLeaf: true, leaves: [...leaves] });
+            return;
+        }
+
+        steps.push({ node, sum, total, isLeaf: false, leaves: [...leaves] });
+
+        sumNumbers(node.left, sum)
+        sumNumbers(node.right, sum);
+    }
+
+    sumNumbers(root, 0);
+    return steps;
+}
---
+++ src/games/tree/invert-binary-tree/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/invert-binary-tree",
+    categories: [Category.Tree],
+    companies: [],
+    difficulty: Difficulty.Easy,
+    img: "/img/invert-binary-tree.png",
+    leetcodeId: 226
+}
+
+export default info;
