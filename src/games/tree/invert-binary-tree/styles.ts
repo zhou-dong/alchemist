686a3d2eb29eb81f238bf5b229fe052ec2dba3ae
Dong Zhou <dozhou@rubiconproject.com>
2023-01-17 00:36:56 -0800

    Update styles.ts

--- src/games/tree/invert-binary-tree/styles.ts
+++ src/games/tree/invert-binary-tree/styles.ts
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { TextGeometry, TextGeometryParameters } from "three/examples/jsm/geometries/TextGeometry";
+import { TextGeometryParameters } from "three/examples/jsm/geometries/TextGeometry";
 import { font } from '../../../commons/three';
 import { buildBinaryTree } from "../../../data-structures/tree/binaryTreeBuilder";
 
@@ -14,14 +14,14 @@ const textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "green
 const textGeometryParameters: TextGeometryParameters = { font, size: 0.8, height: 0.1 };
 const lineMaterial = new THREE.LineBasicMaterial({ color: "gold" });
 
-export const buildTree = (array: (number | null)[], scene: THREE.Scene) => {
+export const buildTree = (array: (string | null)[], scene: THREE.Scene) => {
     const center = { x: 0, y: 8, z: 0 };
     const show = true;
     const duration = 0;
     const yDistance = 3;
     const xAxisAlpha = 2; // expend the tree size in xAxis.
 
-    return buildBinaryTree<number>(
+    return buildBinaryTree<string>(
         sphereGeometry,
         sphereMaterial,
         textMaterial,
@@ -35,13 +35,4 @@ export const buildTree = (array: (number | null)[], scene: THREE.Scene) => {
         array,
         show
     );
-}
-
-const indexTextMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "orange" });
-const indexUpperTextGeometryParameters: TextGeometryParameters = { font, size: 0.5, height: 0.1 };
-export const buildThreeText = (num: number, x: number, y: number, z: number): THREE.Mesh => {
-    const textGeometry = new TextGeometry(num + "", indexUpperTextGeometryParameters);
-    const mesh = new THREE.Mesh(textGeometry, indexTextMaterial);
-    mesh.position.set(x, y, z);
-    return mesh;
 }
