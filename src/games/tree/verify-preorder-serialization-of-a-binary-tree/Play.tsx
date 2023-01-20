e13ce4bba862d19207d1058f4b1d6b9b626ab988
Dong Zhou <dozhou@rubiconproject.com>
2023-01-20 08:31:12 -0800

    Update Play.tsx

--- src/games/tree/verify-preorder-serialization-of-a-binary-tree/Play.tsx
+++ src/games/tree/verify-preorder-serialization-of-a-binary-tree/Play.tsx
@@ -1,11 +1,17 @@
 import { useAlgoContext } from "./AlgoContext";
 import { Button } from '@mui/material';
-import { normalSphereColor, enabledSphereColor, stackItemSphereColor } from "./styles";
+import { normalSphereColor, enabledSphereColor, buildTreeNode } from "./styles";
 import { wait } from "../../../data-structures/_commons/utils";
 import { State } from "./AlgoState";
 import TreeNode from "../../../data-structures/tree/node";
 import StackItemBuilder from './stackItemBuilder';
 import Stack from '../../../data-structures/stack';
+
+const resetColor = (stack: TreeNode<string>[]) => {
+    stack.forEach(node => {
+        node.sphereColor = normalSphereColor;
+    })
+}
 
 const updateTreeColor = (root?: TreeNode<string>, current?: TreeNode<string>) => {
     if (root === undefined || current === undefined) {
@@ -22,52 +28,79 @@ const updateTreeColor = (root?: TreeNode<string>, current?: TreeNode<string>) =>
     updateTreeColor(root.right, current);
 }
 
-const pushToStack = async (stack: Stack<string>, treeNodeStack: TreeNode<string>[], node: TreeNode<string>, scene: THREE.Scene) => {
-    node.sphereColor = stackItemSphereColor;
-    treeNodeStack.push(node);
-    const stackItem = new StackItemBuilder(node.val.value, scene, true).build();
-    await stack.push(stackItem);
-    if (node.left) {
-        await pushToStack(stack, treeNodeStack, node.left, scene);
-    }
+const pushToStack = async (
+    stack: Stack<string>,
+    treeNodeStack: TreeNode<string>[],
+    treeNode: TreeNode<string>,
+    scene: THREE.Scene
+): Promise<number> => {
+    treeNodeStack.push(treeNode);
+    return stack.push(new StackItemBuilder(treeNode.val.value, scene, true).build());
 }
 
 const Main = () => {
 
-    const { animate, cancelAnimate, state, setState, stack, treeNodeStack, scene, root } = useAlgoContext();
+    const { animate, cancelAnimate, state, setState, stack, scene, root, index, steps, setIndex, treeNodeStack } = useAlgoContext();
 
     const handleNextClick = async () => {
         setState(State.Computing);
         animate();
 
         try {
-            const treeNode = treeNodeStack.pop();
-            if (treeNode) {
-                await doNext(treeNode);
+            const step = steps[index];
+            if (step) {
+                await doClick(step.node);
                 await wait(0.1);
             }
         } finally {
             cancelAnimate();
         }
 
-        if (!stack || treeNodeStack.length === 0) {
+        if (index >= steps.length - 1) {
             setState(State.Finished);
         } else {
             setState(State.Playing);
         }
+
+        setIndex(i => i + 1);
     }
 
-    const doNext = async (treeNode: TreeNode<string>) => {
-        updateTreeColor(root, treeNode)
-        if (!stack || !treeNode) {
+    const doClick = async (node: TreeNode<string>) => {
+        if (!stack) {
             return;
         }
-        const node = await stack.pop();
-        if (node) {
-            node.hide();
-        }
-        if (treeNode.right) {
-            await pushToStack(stack, treeNodeStack, treeNode.right, scene);
+        resetColor(treeNodeStack);
+        updateTreeColor(root, node);
+
+        await pushToStack(stack, treeNodeStack, node, scene);
+
+        while (
+            treeNodeStack.length >= 3 &&
+            treeNodeStack[treeNodeStack.length - 1].val.value === "#" &&
+            treeNodeStack[treeNodeStack.length - 2].val.value === "#" &&
+            treeNodeStack[treeNodeStack.length - 3].val.value !== "#"
+        ) {
+            const item1 = await stack.pop();
+            item1?.hide();
+            const node1 = treeNodeStack.pop();
+            node1?.hide();
+
+            const item2 = await stack.pop();
+            item2?.hide();
+            const node2 = treeNodeStack.pop();
+            node2?.hide();
+
+            const item3 = await stack.pop();
+            item3?.hide();
+            const node3 = treeNodeStack.pop();
+            if (node3) {
+                const { x, y, z } = node3.val.center
+                const newNode = buildTreeNode("#", scene, { x, y, z });
+                newNode.sphereColor = enabledSphereColor;
+                newNode.show();
+                node3.hide();
+                await pushToStack(stack, treeNodeStack, newNode, scene);
+            }
         }
     }
 
