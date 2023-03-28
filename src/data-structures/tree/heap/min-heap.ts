1f456e001d8d0a3de7ab034ca9fcaa1c6496d476
Dong Zhou <dozhou@rubiconproject.com>
2023-03-28 15:53:32 -0700

    refactor min-heap

--- src/data-structures/tree/heap/min-heap.ts
+++ src/data-structures/tree/heap/min-heap.ts
@@ -2,7 +2,7 @@ import * as THREE from 'three';
 import { TreeNodeProps } from '../nodes/v2/builder';
 import Heap from "./heap.class";
 
-class MaxHeap<T> extends Heap<T>{
+class MinHeap<T> extends Heap<T>{
 
     constructor(
         treeNodeProps: TreeNodeProps,
@@ -13,12 +13,12 @@ class MaxHeap<T> extends Heap<T>{
     }
 
     protected shouldBubbleUp(current: T, parent: T): boolean {
-        return current > parent;
+        return current < parent;
     }
 
     protected shouldBubbleDown(current: T, child: T): boolean {
-        return current < child;
+        return current > child;
     }
 }
 
-export default MaxHeap;
+export default MinHeap;
