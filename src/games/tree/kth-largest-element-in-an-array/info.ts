71c1383fc894f8532c3a8fac6720d6123dcf9374
Dong Zhou <dozhou@rubiconproject.com>
2023-03-28 23:14:18 -0700

    Create info.ts

---
+++ src/games/tree/kth-largest-element-in-an-array/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/kth-largest-element-in-an-array",
+    categories: [Category.Tree, Category.Heap],
+    companies: [],
+    difficulty: Difficulty.Medium,
+    img: "/img/kth-largest-element-in-an-array.png",
+    leetcodeId: 215
+}
+
+export default info;
