c65fabd310f35a7991e730f305a30e6f466d0615
Dong Zhou <dozhou@rubiconproject.com>
2023-04-10 02:15:29 -0700

    Update Title.tsx

--- src/games/tree/find-median-from-data-stream/Title.tsx
+++ src/games/tree/find-median-from-data-stream/Title.tsx
@@ -1,14 +1,8 @@
 import Box from '@mui/material/Box';
-import { CheckCircleOutline } from '@mui/icons-material';
 import { Typography } from '@mui/material';
 import { title } from "./contents";
-import { useAlgoContext } from './AlgoContext';
-import { State } from './AlgoState';
 
 export default function Title() {
-
-    const { state } = useAlgoContext();
-
     return (
         <Box sx={{
             position: "fixed",
@@ -21,7 +15,6 @@ export default function Title() {
             <Typography variant='body1'>
                 {title}
             </Typography>
-            {state === State.Finished && <CheckCircleOutline sx={{ color: 'green' }} />}
         </Box>
     );
 }
