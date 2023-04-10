0d51a5de3c17eaf5c48f20d0e18757d37345effc
Dong Zhou <dozhou@rubiconproject.com>
2023-04-10 15:26:44 -0700

    Update styles.ts

--- src/games/tree/find-median-from-data-stream/styles.ts
+++ src/games/tree/find-median-from-data-stream/styles.ts
@@ -6,25 +6,25 @@ import MinHeap from '../../../data-structures/tree/heap/min-heap';
 import { Props, TreeNodeProps } from '../../../data-structures/tree/heap/props';
 import Position from '../../../data-structures/_commons/params/position.interface';
 
-const lineColor = "gold";
-const normalSphereColor = "yellow";
-const cubeColor = "yellow";
-const enabledColor = "orange";
-
+const enabledColor = "gold";
 const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(1, 32, 16);
-const sphereMaterial = () => new THREE.MeshBasicMaterial({ color: normalSphereColor, opacity: 0.4, transparent: true });
-const textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "green" });
 const textGeometryParameters: TextGeometryParameters = { font, size: 0.8, height: 0.1 };
-const lineMaterial = new THREE.LineBasicMaterial({ color: lineColor });
-const cubeMaterial = () => new THREE.MeshBasicMaterial({ color: cubeColor, opacity: 0.5, transparent: true });
-const cubeGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(2, 2, 2);
+const cubeGeometry: THREE.BoxGeometry = new THREE.BoxGeometry(2, 1.5, 1.5);
 
 interface HeapPosition {
     arrayPosition: Position;
     treePosition: Position;
 }
 
-const buildHeapProps = (initPosition: HeapPosition, heapPosition: HeapPosition, scene: THREE.Scene): Props => {
+const buildHeapProps = (
+    initPosition: HeapPosition,
+    heapPosition: HeapPosition,
+    sphereMaterial: () => THREE.Material,
+    cubeMaterial: () => THREE.Material,
+    textMaterial: THREE.Material,
+    lineMaterial: THREE.LineBasicMaterial,
+    scene: THREE.Scene
+): Props => {
 
     const treeInitDepth = 4;
     const treeNodeDistance = { x: 2.5, y: 2.5 };
@@ -65,10 +65,17 @@ const buildHeapProps = (initPosition: HeapPosition, heapPosition: HeapPosition, 
     return props;
 }
 
+export const smallerHeapColor = "orange";
+export const greaterHeapColor = "green";
+
 export const buildSmaller = (scene: THREE.Scene): MaxHeap<number> => {
     return new MaxHeap(buildHeapProps(
         { arrayPosition: { x: 0, y: 0, z: 0 }, treePosition: { x: 0, y: 0, z: 0 } },
         { arrayPosition: { x: -5, y: 10, z: 0 }, treePosition: { x: -10, y: -5, z: 0 } },
+        () => new THREE.MeshBasicMaterial({ color: smallerHeapColor, opacity: 0.6, transparent: true }),
+        () => new THREE.MeshBasicMaterial({ color: smallerHeapColor, opacity: 0.5, transparent: true }),
+        new THREE.MeshBasicMaterial({ color: "#000" }),
+        new THREE.LineBasicMaterial({ color: smallerHeapColor }),
         scene
     ));
 }
@@ -77,6 +84,10 @@ export const buildGreater = (scene: THREE.Scene): MinHeap<number> => {
     return new MinHeap(buildHeapProps(
         { arrayPosition: { x: 0, y: 0, z: 0 }, treePosition: { x: 0, y: 0, z: 0 } },
         { arrayPosition: { x: 15, y: 10, z: 0 }, treePosition: { x: 10, y: -5, z: 0 } },
+        () => new THREE.MeshBasicMaterial({ color: greaterHeapColor, opacity: 0.6, transparent: true }),
+        () => new THREE.MeshBasicMaterial({ color: greaterHeapColor, opacity: 0.5, transparent: true }),
+        new THREE.MeshBasicMaterial({ color: "#000" }),
+        new THREE.LineBasicMaterial({ color: greaterHeapColor }),
         scene
     ));
 }
