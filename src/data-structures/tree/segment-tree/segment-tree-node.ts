a463731f4297a29d8a98e690a055de65b6c37b77
Dong Zhou <dozhou@rubiconproject.com>
2023-06-13 22:05:35 -0700

    Update segment-tree-node.ts

--- src/data-structures/tree/segment-tree/segment-tree-node.ts
+++ src/data-structures/tree/segment-tree/segment-tree-node.ts
@@ -1,9 +1,13 @@
+import * as THREE from 'three';
+import { TextGeometryParameters } from "three/examples/jsm/geometries/TextGeometry";
 import { TextSphere } from "../../_commons/sphere/text-sphere.interface";
 import TreeNode from "../nodes/v2/node";
+import Text from '../../_commons/params/text.interface';
+import TextImpl from '../../_commons/three/text.class';
 
 export default class SegmentTreeNode extends TreeNode<number> {
-    start: number;
-    end: number;
+    start: Text<number>;
+    end: Text<number>;
     left?: SegmentTreeNode;
     right?: SegmentTreeNode;
 
@@ -11,10 +15,13 @@ export default class SegmentTreeNode extends TreeNode<number> {
         value: TextSphere<number>,
         start: number,
         end: number,
+        rangeMaterial: THREE.Material,
+        rangeGeometryParameters: TextGeometryParameters,
+        scene: THREE.Scene,
         index?: number
     ) {
         super(value, index);
-        this.start = start;
-        this.end = end;
+        this.start = new TextImpl(start, rangeMaterial, rangeGeometryParameters, scene);
+        this.end = new TextImpl(end, rangeMaterial, rangeGeometryParameters, scene);
     }
 }
