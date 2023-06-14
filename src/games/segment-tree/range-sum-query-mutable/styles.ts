64283c65f01ce54ea2b4dbfe4411fe1aac96f859
Dong Zhou <dozhou@rubiconproject.com>
2023-06-13 22:05:28 -0700

    Update styles.ts

--- src/games/segment-tree/range-sum-query-mutable/styles.ts
+++ src/games/segment-tree/range-sum-query-mutable/styles.ts
@@ -5,11 +5,16 @@ import { font } from '../../../commons/three';
 export const lineColor = "gold";
 export const normalSphereColor = "yellow";
 export const enabledSphereColor = "orange";
+export const textColor = "green";
+export const rangeColor = "orange";
 
 export const sphereGeometry: THREE.SphereGeometry = new THREE.SphereGeometry(1, 32, 16);
 export const sphereMaterial = (): THREE.Material => {
     return new THREE.MeshBasicMaterial({ color: normalSphereColor, opacity: 0.4, transparent: true });
 }
-export const textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: "green" });
+export const textMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: textColor });
 export const textGeometryParameters: TextGeometryParameters = { font, size: 0.8, height: 0.1 };
 export const lineMaterial = new THREE.LineBasicMaterial({ color: lineColor });
+
+export const rangeMaterial: THREE.Material = new THREE.MeshBasicMaterial({ color: rangeColor });
+export const rangeGeometryParameters: TextGeometryParameters = { font, size: 0.4, height: 0.1 };
