fda3b8068ebd1ce7f5304999c5b0e87338b10671
Dong Zhou <dozhou@rubiconproject.com>
2023-06-13 22:34:53 -0700

    Update segment-tree-node.ts

--- src/data-structures/tree/segment-tree/segment-tree-node.ts
+++ src/data-structures/tree/segment-tree/segment-tree-node.ts
@@ -4,6 +4,8 @@ import { TextSphere } from "../../_commons/sphere/text-sphere.interface";
 import TreeNode from "../nodes/v2/node";
 import Text from '../../_commons/params/text.interface';
 import TextImpl from '../../_commons/three/text.class';
+import Position from '../../_commons/params/position.interface';
+import { calDestination, calDistance } from '../../_commons/utils';
 
 export default class SegmentTreeNode extends TreeNode<number> {
     start: Text<number>;
@@ -24,4 +26,32 @@ export default class SegmentTreeNode extends TreeNode<number> {
         this.start = new TextImpl(start, rangeMaterial, rangeGeometryParameters, scene);
         this.end = new TextImpl(end, rangeMaterial, rangeGeometryParameters, scene);
     }
+
+    show() {
+        super.show();
+        this.start.show();
+        this.end.show();
+        return this;
+    }
+
+    hide() {
+        super.hide();
+        this.start.hide();
+        this.end.hide();
+        return this;
+    }
+
+    async moveTo(dest: Position, duration: number, onUpdate?: () => void): Promise<void> {
+        const distance = calDistance(this.value.center, dest);
+        const startDest = calDestination(this.start, distance);
+        const endDest = calDestination(this.end, distance);
+
+        const nodeMove = super.moveTo(dest, duration);
+        const startMove = this.start.move(startDest, duration);
+        const endMove = this.end.move(endDest, duration);
+
+        return Promise.all([nodeMove, startMove, endMove]).then(() => { });
+    }
+
+
 }
