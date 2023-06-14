9a1f492ef7295a50c9eb013749b7000d31599b0e
Dong Zhou <dozhou@rubiconproject.com>
2023-06-14 01:10:49 -0700

    Update Play.tsx

--- src/games/segment-tree/range-sum-query-mutable/Play.tsx
+++ src/games/segment-tree/range-sum-query-mutable/Play.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import SendIcon from '@mui/icons-material/Send';
 import { useAlgoContext } from "./AlgoContext";
 import ISegmentTree from "../../../data-structures/tree/segment-tree/segment-tree.interface";
 import SegmentTree from "../../../data-structures/tree/segment-tree/segment-tree.class";
-import { Button } from "@mui/material";
+import { Button, FormControl, IconButton, InputLabel, MenuItem, OutlinedInput, Paper, Select, SelectChangeEvent, Stack, TextField } from "@mui/material";
 import { State } from "./AlgoState";
 import { enabledSphereColor, lineMaterial, normalSphereColor, rangeGeometryParameters, rangeMaterial, sphereGeometry, sphereMaterial, textGeometryParameters, textMaterial } from "./styles";
 import { wait } from "../../../data-structures/_commons/utils";
@@ -12,9 +13,8 @@ const duration = 1;
 const BuildSegmentTree: React.FC<{ setSegmentTree: React.Dispatch<React.SetStateAction<ISegmentTree | undefined>> }> = ({ setSegmentTree }) => {
 
     const { nums, state, scene, animate, cancelAnimate, setState } = useAlgoContext();
-    const [buildingTree, setBuildingTree] = React.useState<boolean>(false);
 
-    const disabled = state !== State.Standby || nums.length === 0 || buildingTree;
+    const disabled = state !== State.Standby || nums.length === 0;
 
     const position = { x: 0, y: -5, z: 0 };
     const initPosition = { x: 0, y: 0, z: 0 };
@@ -24,7 +24,7 @@ const BuildSegmentTree: React.FC<{ setSegmentTree: React.Dispatch<React.SetState
         if (nums.length === 0) {
             return;
         }
-        setBuildingTree(true);
+        setState(State.Playing);
 
         const size = 2 * nums.length;
         const depth = Math.floor(Math.log2(size)) + 1;
@@ -56,7 +56,6 @@ const BuildSegmentTree: React.FC<{ setSegmentTree: React.Dispatch<React.SetState
         }
         cancelAnimate();
 
-        setBuildingTree(false);
         setState(State.Ready);
     }
 
@@ -70,23 +69,129 @@ const BuildSegmentTree: React.FC<{ setSegmentTree: React.Dispatch<React.SetState
     );
 }
 
+const IndexSelector: React.FC<{
+    label: string,
+    labelId: string,
+    setValue: React.Dispatch<React.SetStateAction<number | undefined>>
+}> = ({ label, labelId, setValue }) => {
+    const { nums } = useAlgoContext();
+    const indices = Array.from(Array(nums.length).keys());
+    type SelectType = number | undefined;
+
+    const handleChange = (event: SelectChangeEvent<SelectType>) => {
+        const { target: { value } } = event;
+        if (value !== undefined && typeof value === "number") {
+            setValue(value);
+        }
+    };
+
+    return (
+        <FormControl sx={{
+            width: 85
+        }}>
+            <InputLabel id={labelId}>{label}</InputLabel>
+            <Select
+                labelId={labelId}
+                onChange={handleChange}
+                input={<OutlinedInput label={label} />}
+            >
+                {
+                    indices.map(index =>
+                        <MenuItem key={index} value={index}>
+                            {index}
+                        </MenuItem>
+                    )
+                }
+            </Select>
+        </FormControl>
+    );
+}
+
 const Update: React.FC<{ segmentTree: ISegmentTree | undefined }> = ({ segmentTree }) => {
 
-    // TODO
+    const { state, animate, cancelAnimate, setState } = useAlgoContext();
+
+    const [index, setIndex] = React.useState<number>();
+    const [value, setValue] = React.useState<number>();
+
+    const disabled = state !== State.Ready || index === undefined || value === undefined || segmentTree === undefined;
+
+    const handleValueChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const targetValue = event.target.value;
+        if (targetValue !== undefined) {
+            try {
+                setValue(+targetValue);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+
+    const handleUpdate = async () => {
+        if (disabled) {
+            return;
+        }
+        setState(State.Playing);
+        animate();
+        try {
+            await segmentTree.update(index, value, duration);
+            await wait(0.1);
+        } catch (error) {
+            console.log(error);
+        }
+        cancelAnimate();
+        setIndex(undefined);
+        setValue(undefined);
+        setState(State.Ready);
+    };
+
     return (
         <>
-            update
+            <IndexSelector label="index" labelId="index-selector" setValue={setIndex} />
+            <TextField label="value" variant="standard" type="number" disabled={state !== State.Ready} onChange={handleValueChanged} />
+            <IconButton onClick={handleUpdate} disabled={disabled} sx={{ width: "55px", height: "55px" }}>
+                <SendIcon />
+            </IconButton>
         </>
     );
 }
 
 const Query: React.FC<{ segmentTree: ISegmentTree | undefined }> = ({ segmentTree }) => {
 
-    // TODO
+    const { state, animate, cancelAnimate, setState } = useAlgoContext();
+    const [left, setLeft] = React.useState<number>();
+    const [right, setRight] = React.useState<number>();
+    const disabled = state !== State.Ready || left === undefined || right === undefined || segmentTree === undefined;
+
+    const handleQuery = async () => {
+        if (disabled) {
+            return;
+        }
+        setState(State.Playing);
+        animate();
+        try {
+            const value = await segmentTree.query(left, right, duration);
+            console.log(value); // todo
+            await wait(0.1);
+        } catch (error) {
+            console.log(error);
+        }
+        cancelAnimate();
+        setLeft(undefined);
+        setRight(undefined);
+        setState(State.Ready);
+    };
+
     return (
-        <>
-            query
-        </>
+        <Paper>
+            <Stack direction="row" spacing={2}>
+                <IndexSelector label="left" labelId="left-range" setValue={setLeft} />
+                <IndexSelector label="right" labelId="right-range" setValue={setRight} />
+                <IconButton onClick={handleQuery} disabled={disabled} sx={{ width: "55px", height: "55px" }}>
+                    <SendIcon />
+                </IconButton>
+            </Stack>
+        </Paper>
     );
 }
 
