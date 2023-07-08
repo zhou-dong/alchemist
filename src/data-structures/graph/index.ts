28de72c1c50ea70bb20e56e545651d5bb50e21c3
Dong Zhou <dozhou@rubiconproject.com>
2023-07-08 15:03:59 -0700

    Update index.ts

--- src/data-structures/graph/index.ts
+++ src/data-structures/graph/index.ts
@@ -1,3 +1,55 @@
-import Graph from "graphology";
+import Graphology from "graphology";
+import forceAtlas2 from 'graphology-layout-forceatlas2';
+import { GraphNode } from "./node.interface";
+import { GraphEdge } from "./edge.interface";
 
-const graph = new Graph();
+export class Graph<T> {
+
+    nodes: GraphNode<T>[];
+    edges: GraphEdge<T>[];
+
+    constructor(nodes: GraphNode<T>[], edges: GraphEdge<T>[]) {
+        this.nodes = nodes;
+        this.edges = edges;
+        this.setPositions();
+    }
+
+    private setPositions() {
+        const positions = this.computePositions();
+
+        this.nodes.forEach(node => {
+            const { x, y } = positions[node.id];
+            node.position.x = x;
+            node.position.y = y;
+        });
+
+        this.edges.forEach(edge => {
+            edge.refresh();
+        });
+    }
+
+    private computePositions() {
+
+        const graph = new Graphology();
+
+        this.nodes.forEach(node => {
+            const x = Math.random() * 100; // Set initial x position
+            const y = Math.random() * 100; // Set initial y position
+            graph.addNode(node.id, { x, y });
+        });
+
+        this.edges.forEach(edge => {
+            graph.addEdge(edge.source.id, edge.target.id);
+        })
+
+        const positions = forceAtlas2(graph, {
+            iterations: 50,
+            settings: {
+                gravity: 10
+            }
+        });
+
+        return positions;
+    }
+
+}
