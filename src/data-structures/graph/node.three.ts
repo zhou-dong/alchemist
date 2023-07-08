77b88454470d0f2d2d635e4200b311136af06d66
Dong Zhou <dozhou@rubiconproject.com>
2023-07-08 02:34:56 -0700

    Create node.three.ts

---
+++ src/data-structures/graph/node.three.ts
@@ -0,0 +1,72 @@
+import * as THREE from 'three';
+import { TextGeometry, TextGeometryParameters } from 'three/examples/jsm/geometries/TextGeometry';
+import Displayer from "../_commons/params/displayer.interface";
+import DisplayerImpl from "../_commons/three/displayer.class";
+import Position from "../_commons/params/position.interface";
+import PositionImpl from '../_commons/three/position.class';
+import Mover from '../_commons/params/mover.interface';
+import MoverImpl from '../_commons/three/mover.class';
+import Color from '../_commons/params/color.interface';
+import ColorImpl from '../_commons/three/color.class';
+import { Base as IBase, GraphSkin as ISkin, GraphText as IText } from "./node.interface";
+
+class Base implements Mover, Displayer, IBase {
+
+    public readonly color: Color;
+    public readonly position: Position;
+    private displayer: Displayer;
+    private mover: Mover;
+
+    constructor(
+        scene: THREE.Scene,
+        geometry: THREE.BufferGeometry,
+        material: THREE.Material
+    ) {
+        const mesh = new THREE.Mesh(geometry, material);
+        this.position = new PositionImpl(mesh);
+        this.color = new ColorImpl(material);
+        this.displayer = new DisplayerImpl(scene, mesh);
+        this.mover = new MoverImpl(mesh);
+    }
+
+    show() {
+        this.displayer.show();
+    }
+
+    hide() {
+        this.displayer.hide();
+    }
+
+    move(position: Position, duration: number, onUpdate?: (() => void) | undefined) {
+        return this.mover.move(position, duration, onUpdate);
+    }
+}
+
+export class GraphSkin extends Base implements ISkin {
+
+    constructor(
+        scene: THREE.Scene,
+        geometry: THREE.SphereGeometry,
+        material: THREE.Material,
+    ) {
+        super(scene, geometry, material);
+    }
+
+}
+
+export class GraphText extends Base implements IText {
+
+    public text: string;
+
+    constructor(
+        text: string,
+        scene: THREE.Scene,
+        geometryParameters: TextGeometryParameters,
+        material: THREE.Material,
+    ) {
+        const geometry = new TextGeometry(text, geometryParameters);
+        super(scene, geometry, material);
+        this.text = text;
+    }
+
+}
