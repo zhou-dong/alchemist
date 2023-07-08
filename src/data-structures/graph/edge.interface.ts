416c7254a391b1f8f9d2c5babf09a184301456f0
Dong Zhou <dozhou@rubiconproject.com>
2023-07-08 14:13:22 -0700

    Create edge.interface.ts

---
+++ src/data-structures/graph/edge.interface.ts
@@ -0,0 +1,18 @@
+import Displayer from "../_commons/params/displayer.interface";
+import Mover from "../_commons/params/mover.interface";
+import { GraphNode } from "./node.interface";
+
+export interface GraphEdge<T> extends Displayer {
+    id: string;
+    source: GraphNode<T>;
+    target: GraphNode<T>;
+    refresh(): void;
+}
+
+export interface DirectedGraphEdge<T> extends GraphEdge<T> {
+
+}
+
+export interface UndirectedGraphEdge<T> extends GraphEdge<T> {
+
+}
