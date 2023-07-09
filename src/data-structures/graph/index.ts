af6f2206ac64ff2ea843aaa6b4ae050d8edd188b
Dong Zhou <dozhou@rubiconproject.com>
2023-07-09 01:47:53 -0700

    Update index.ts

--- src/data-structures/graph/index.ts
+++ src/data-structures/graph/index.ts
@@ -2,6 +2,8 @@ import Graphology from "graphology";
 import forceAtlas2 from 'graphology-layout-forceatlas2';
 import { GraphNode } from "./node.interface";
 import { GraphEdge } from "./edge.interface";
+import { SimpleGraphSkin, SimpleGraphText } from "./node.three";
+import { SimpleDirectedGraphEdge, SimpleUndirectedGraphEdge } from "./edge.three";
 
 export class Graph<T> {
 
@@ -52,6 +54,77 @@ export class Graph<T> {
         });
 
         return positions;
+    }
+
+}
+
+const buildGraphNodes = <T>(
+    nodeSkinColor: string,
+    nodeTextColor: string,
+    adjacency: T[][],
+    scene: THREE.Scene
+): Map<T, GraphNode<T>> => {
+    const nodes: Set<T> = new Set();
+    adjacency.forEach(connection => {
+        const [a, b] = connection;
+        nodes.add(a);
+        nodes.add(b);
+    });
+
+    const nodeMap: Map<T, GraphNode<T>> = new Map();
+    let index = 0;
+    nodes.forEach(node => {
+        const graphSkin = new SimpleGraphSkin(scene, nodeSkinColor);
+        const graphText = new SimpleGraphText(node + "", scene, nodeTextColor);
+        const graphNode = new GraphNode<T>(index, node, graphSkin, graphText);
+        nodeMap.set(node, graphNode);
+        index += 1;
+    });
+
+    return nodeMap;
+};
+
+export class SimpleDirectedGraph<T> extends Graph<T> {
+
+    constructor(
+        nodeSkinColor: string,
+        nodeTextColor: string,
+        edgeColor: string,
+        adjacency: T[][],
+        scene: THREE.Scene
+    ) {
+        const nodeMap = buildGraphNodes(nodeSkinColor, nodeTextColor, adjacency, scene);
+        const edges: GraphEdge<T>[] = [];
+        adjacency.forEach(connection => {
+            const [a, b] = connection;
+            const source: GraphNode<T> = nodeMap.get(a)!;
+            const target: GraphNode<T> = nodeMap.get(b)!;
+            const edge = new SimpleDirectedGraphEdge(source, target, scene, edgeColor);
+            edges.push(edge);
+        });
+        super(Array.from(nodeMap.values()), edges);
+    }
+}
+
+export class SimpleUndirectedGraph<T> extends Graph<T> {
+
+    constructor(
+        nodeSkinColor: string,
+        nodeTextColor: string,
+        edgeColor: string,
+        adjacency: T[][],
+        scene: THREE.Scene
+    ) {
+        const nodeMap = buildGraphNodes(nodeSkinColor, nodeTextColor, adjacency, scene);
+        const edges: GraphEdge<T>[] = [];
+        adjacency.forEach(connection => {
+            const [a, b] = connection;
+            const source: GraphNode<T> = nodeMap.get(a)!;
+            const target: GraphNode<T> = nodeMap.get(b)!;
+            const edge = new SimpleUndirectedGraphEdge(source, target, scene, edgeColor);
+            edges.push(edge);
+        });
+        super(Array.from(nodeMap.values()), edges);
     }
 
 }
