f8ae8a785ad2ddf0bcb7c9b1cc5f1d15093aae69
Dong Zhou <dozhou@rubiconproject.com>
2023-07-10 03:25:05 -0700

    Update Play.tsx

--- src/games/graph/course-schedule/Play.tsx
+++ src/games/graph/course-schedule/Play.tsx
@@ -1,46 +1,23 @@
-import * as THREE from "three";
 import { useAlgoContext } from "./AlgoContext";
-import { Button, Stack } from '@mui/material';
+import { Button, ButtonGroup, Stack } from '@mui/material';
 import { wait } from "../../../data-structures/_commons/utils";
 import { State } from "./AlgoState";
-import { SimpleGraphSkin, SimpleGraphText } from "../../../data-structures/graph/node.three";
-import { GraphNode } from "../../../data-structures/graph/node.interface";
-import { GraphEdge } from "../../../data-structures/graph/edge.interface";
-import { SimpleUndirectedGraphEdge } from "../../../data-structures/graph/edge.three";
-import { Graph } from "../../../data-structures/graph";
-import { clearScene } from '../../../commons/three';
 
 const Main = () => {
 
-    const { animate, cancelAnimate, index, steps, setIndex, state, depthTreeSteps, setState, scene } = useAlgoContext();
+    const { animate, cancelAnimate, state, setState, graph } = useAlgoContext();
 
-    const handleOnClick = async () => {
-        clearScene(scene);
-
-        const nodeA = new GraphNode<string>(0, "A", new SimpleGraphSkin(scene, "blue"), new SimpleGraphText("A", scene, "green"));
-        const nodeB = new GraphNode<string>(1, "B", new SimpleGraphSkin(scene, "blue"), new SimpleGraphText("A", scene, "green"));
-        const nodeC = new GraphNode<string>(2, "C", new SimpleGraphSkin(scene, "blue"), new SimpleGraphText("A", scene, "green"));
-
-        const edgeAB: GraphEdge<string> = new SimpleUndirectedGraphEdge(nodeA, nodeB, scene, "gold");
-        const edgeBC: GraphEdge<string> = new SimpleUndirectedGraphEdge(nodeB, nodeC, scene, "pink");
-        const graph = new Graph([nodeA, nodeB, nodeC], [edgeAB, edgeBC]);
-
-        animate();
-        try {
-            graph.nodes.forEach(node => {
-                node.show();
-            });
-
-            graph.edges.forEach(edge => {
-                edge.show();
-            });
-
-            await wait(0.2);
-            console.log("haha")
-        } catch (error) {
-            console.log(error);
+    const handleRefresh = async () => {
+        if (!graph) {
+            return;
         }
+        setState(State.Computing);
+        graph.resetPositions();
+        graph.show();
+        animate();
+        wait(0.2);
         cancelAnimate();
+        setState(State.Playing);
     }
 
     return (
@@ -57,16 +34,28 @@ const Main = () => {
             }}
         >
             <div>
-                <Button
-                    variant="contained"
-                    size="large"
-                    onClick={handleOnClick}
-                    sx={{ color: "#FFF", zIndex: 1 }}
-                    // disabled={state !== State.Playing}
-                    color="info"
-                >
-                    next
-                </Button>
+                <ButtonGroup>
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={handleRefresh}
+                        sx={{ color: "#FFF", zIndex: 1 }}
+                        disabled={state !== State.Playing}
+                        color="info"
+                    >
+                        next
+                    </Button>
+                    <Button
+                        variant="contained"
+                        size="large"
+                        onClick={handleRefresh}
+                        sx={{ color: "#FFF", zIndex: 1 }}
+                        disabled={state !== State.Playing}
+                        color="info"
+                    >
+                        refresh
+                    </Button>
+                </ButtonGroup>
             </div>
         </Stack>
     );
