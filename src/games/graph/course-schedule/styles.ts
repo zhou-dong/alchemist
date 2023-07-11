0f33f47983e94ce5b3d4da8ddac00d88d34e3b21
Dong Zhou <dozhou@rubiconproject.com>
2023-07-11 01:37:18 -0700

    Update styles.ts

--- src/games/graph/course-schedule/styles.ts
+++ src/games/graph/course-schedule/styles.ts
@@ -1,6 +1,6 @@
 export const nodeOriginalSkinColor: string = "yellow";
 export const nodeOriginalTextColor: string = "green";
 
-export const nodeEnabledSkinColor: string = "gold";
-export const nodeEnabledTextColor: string = "green";
+export const nodeEnabledSkinColor: string = "green";
+export const nodeEnabledTextColor: string = "gray";
 export const edgeColor: string = "gold";
