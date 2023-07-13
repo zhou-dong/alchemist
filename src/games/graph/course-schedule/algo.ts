b0b5e81d7b1e91d74c8df5c31a440737d0f1a5e0
Dong Zhou <dozhou@rubiconproject.com>
2023-07-12 17:05:49 -0700

    Update algo.ts

--- src/games/graph/course-schedule/algo.ts
+++ src/games/graph/course-schedule/algo.ts
@@ -1,49 +1,77 @@
-import TreeNode from "../../../data-structures/tree/nodes/v1/node";
-
-export enum Direction {
-    Left, Right, Back
-}
-
 export interface Step {
-    node: TreeNode<any>;
-    depth?: number;
-    direction?: Direction;
+    visited: number[];
+    current?: number;
+    canFinish?: boolean;
 }
 
-export function buildSteps<T>(root?: TreeNode<T>): Step[] {
+const calculateNumCourses = (prerequisites: number[][]): number => {
+    let numCourses: number = prerequisites
+        .map(array => Math.max(...array))
+        .reduce((prev, curr) => Math.max(prev, curr), 0);
+
+    numCourses += 1;
+    return numCourses;
+}
+
+export function canFinish(prerequisites: number[][]): Step[] {
+
+    let numCourses: number = calculateNumCourses(prerequisites);
+
     const steps: Step[] = [];
 
-    function height<T>(node?: TreeNode<T>, direction?: Direction, depth?: number): number {
-        if (node === undefined) {
-            return 0;
+    const adjacency: Map<number, number[]> = new Map();
+
+    for (let i = 0; i < numCourses; i++) {
+        adjacency.set(i, []);
+    }
+
+    prerequisites.forEach(prerequisite => {
+        const [a, b] = prerequisite;
+        adjacency.get(b)?.push(a);
+    });
+
+    const visited: Set<number> = new Set();
+
+    const dfs = (current: number): boolean => {
+
+        const returnEarly: boolean = visited.has(current) || adjacency.get(current)!.length === 0;
+        if (returnEarly) { // only add new step if return-early to avoid add same node multple times.
+            steps.push({ visited: Array.from(visited), current });
         }
 
-        steps.push({ node, direction, depth });
-
-        const left = height(node.left, Direction.Left, depth);
-        if (left === -1) {
-            steps.push({ node, direction: Direction.Back, depth: -1 });
-            return -1;
+        if (visited.has(current)) {
+            return false;
         }
 
-        const right = height(node.right, Direction.Right, depth);
-        if (right === -1) {
-            steps.push({ node, direction: Direction.Back, depth: -1 });
-            return -1;
+        if (adjacency.get(current)!.length === 0) {
+            return true;
         }
 
-        const different = Math.abs(right - left);
-        if (different > 1) {
-            steps.push({ node, direction: Direction.Back, depth: -1 });
-            return -1;
+        visited.add(current);
+        steps.push({ visited: Array.from(visited), current });
+
+        const children = adjacency.get(current)!;
+        for (let i = 0; i < children.length; i++) {
+            const child = children[i];
+            if (!dfs(child)) {
+                return false;
+            }
         }
 
-        const max = Math.max(left, right) + 1;
-        steps.push({ node, direction: Direction.Back, depth: max });
+        visited.delete(current);
+        adjacency.set(current, []);
+        steps.push({ visited: Array.from(visited), current });
+        return true;
+    }
 
-        return max;
-    };
+    for (let i = 0; i < numCourses; i++) {
+        if (!dfs(i)) {
+            steps.push({ visited: Array.from(visited), current: i, canFinish: false });
+            return steps;
+        }
+    }
 
-    height(root);
+    steps.push({ visited: Array.from(visited), canFinish: true });
+
     return steps;
-}
+};
