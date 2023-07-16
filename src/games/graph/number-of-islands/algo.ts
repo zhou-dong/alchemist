80cd521f95ac30f5db22945069472934e34c8043
Dong Zhou <dozhou@rubiconproject.com>
2023-07-16 01:44:39 -0700

    Update algo.ts

--- src/games/graph/number-of-islands/algo.ts
+++ src/games/graph/number-of-islands/algo.ts
@@ -1,20 +1,70 @@
-const createDPTable = (stringOne: string, stringTwo: string): number[][] => {
-    const rows = stringTwo.length + 1;
-    const cols = stringOne.length + 1;
+import { Point } from "../../commons/point";
 
-    const table = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+const random = (): number => Math.round(Math.random());
 
-    for (let row = 1; row < rows; row++) {
-        for (let col = 1; col < cols; col++) {
-            if (stringOne.charAt(col - 1) === stringTwo.charAt(row - 1)) {
-                table[row][col] = table[row - 1][col - 1] + 1;
-            } else {
-                table[row][col] = Math.max(table[row][col - 1], table[row - 1][col]);
+export const buildGrid = (rows: number, cols: number): number[][] => {
+    const grid: number[][] = [];
+    for (let row = 0; row < rows; row++) {
+        grid.push([]);
+        for (let col = 0; col < cols; col++) {
+            grid[row].push(random());
+        }
+    }
+    return grid;
+}
+
+const cloneGrid = (grid: number[][]): number[][] => grid.map(row => [...row]);
+
+export enum Direction {
+    Left, Right, Up, Down
+}
+
+export interface Step {
+    grid: number[][];
+    point: Point;
+    direction?: Direction;
+    numIslands: number;
+}
+
+export const buildSteps = (grid: number[][]): Step[] => {
+
+    const steps: Step[] = [];
+
+    let numIslands = 0;
+
+    const land = 1;
+    const visited = 2;
+
+    const inArea = (row: number, col: number): boolean => {
+        return row >= 0 && row < grid.length && col >= 0 && col < grid[row].length;
+    }
+
+    const dfs = (row: number, col: number, direction?: Direction) => {
+        if (!inArea(row, col)) {
+            return;
+        }
+        if (grid[row][col] !== land) {
+            return
+        }
+
+        grid[row][col] = visited;
+        steps.push({ grid: cloneGrid(grid), direction, point: { row, col }, numIslands });
+
+        dfs(row - 1, col, Direction.Up);
+        dfs(row, col + 1, Direction.Right);
+        dfs(row + 1, col, Direction.Down);
+        dfs(row, col - 1, Direction.Left);
+    }
+
+    for (let row = 0; row < grid.length; row++) {
+        for (let col = 0; col < grid[row].length; col++) {
+            steps.push({ grid: cloneGrid(grid), point: { row, col }, numIslands });
+            if (grid[row][col] === land) {
+                numIslands += 1;
+                dfs(row, col);
             }
         }
     }
 
-    return table;
-};
-
-export default createDPTable;
+    return steps;
+}
