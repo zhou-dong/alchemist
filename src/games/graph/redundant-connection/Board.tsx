33625a5a665c3dbeba52599a52c1b89eb2ba9252
Dong Zhou <dozhou@rubiconproject.com>
2023-07-29 20:36:40 -0700

    Update Board.tsx

--- src/games/graph/redundant-connection/Board.tsx
+++ src/games/graph/redundant-connection/Board.tsx
@@ -1,40 +1,20 @@
 import { yellow } from '@mui/material/colors';
-import { grey } from '@mui/material/colors';
 import { useAlgoContext } from "./AlgoContext";
 import { Table, TableHead, TableRow, TableCell, TableBody } from '@mui/material';
 import { State } from './AlgoState';
 
 export const Board = () => {
-    const { board, state, steps, index } = useAlgoContext();
-
-    const step = steps[index];
-    const cols = board[0]?.length || 0;
-
-    const IndexStyle: React.CSSProperties = {
-        border: "1px solid gray",
-        padding: "6px",
-        backgroundColor: yellow[500],
-        textAlign: "center",
-        width: 20
-    };
+    const { state, index, edges } = useAlgoContext();
 
     const baseDataStyle: React.CSSProperties = {
         border: "1px solid gray",
         padding: "6px",
         textAlign: "center",
-        width: 20
+        width: 30
     };
 
-    const DataStyle = (i: number, j: number): React.CSSProperties => {
-        if (j < i) {
-            return { ...baseDataStyle, backgroundColor: grey[500], color: "#fff" };
-        }
-
-        if (!step) {
-            return baseDataStyle;
-        }
-        const { row, col } = step;
-        if (i === row && j === col) {
+    const DataStyle = (i: number): React.CSSProperties => {
+        if (i === index) {
             return { ...baseDataStyle, backgroundColor: yellow[500] };
         } else {
             return baseDataStyle;
@@ -45,28 +25,18 @@ export const Board = () => {
         <Table>
             <TableHead>
                 <TableRow>
-                    <TableCell colSpan={cols + 1} sx={{ textAlign: "center" }} padding='none'>
-                        Is Connected Map
+                    <TableCell colSpan={2} sx={{ textAlign: "center" }} padding='none'>
+                        Edges
                     </TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
-                <TableRow>
-                    <TableCell padding='none' sx={IndexStyle} />
-                    {
-                        Array.from(Array(cols).keys()).map((c, j) =>
-                            <TableCell key={j} padding='none' sx={IndexStyle}>
-                                {c}
-                            </TableCell>)
-                    }
-                </TableRow>
                 {
-                    board.map((row, i) =>
+                    edges.map((row, i) =>
                         <TableRow key={i}>
-                            <TableCell padding='none' sx={IndexStyle}>{i}</TableCell>
                             {
                                 row.map((col, j) =>
-                                    <TableCell key={j} padding='none' sx={DataStyle(i, j)}>
+                                    <TableCell key={j} padding='none' sx={DataStyle(i)}>
                                         {col}
                                     </TableCell>)
                             }
