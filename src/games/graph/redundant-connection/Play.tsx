1ce6e44b9a14ae5c749967633c65b7ed8a0c0f67
Dong Zhou <dozhou@rubiconproject.com>
2023-07-30 03:52:48 -0700

    Update Play.tsx

--- src/games/graph/redundant-connection/Play.tsx
+++ src/games/graph/redundant-connection/Play.tsx
@@ -1,22 +1,22 @@
+import * as THREE from "three";
 import { styled } from '@mui/system';
 import MergeIcon from '@mui/icons-material/Merge';
-import CompressIcon from '@mui/icons-material/Compress';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { useAlgoContext } from "./AlgoContext";
-import { Button, ButtonGroup, Stack } from '@mui/material';
+import { Button, ButtonGroup } from '@mui/material';
 import { wait } from "../../../data-structures/_commons/utils";
 import { State } from "./AlgoState";
-import { edgeOriginalColor } from "./styles";
+import { redundantEdgeColor, regularEdgeColor } from "./styles";
 import { Board } from "./Board";
-import { DirectedGraphEdge } from '../../../data-structures/graph/edge.three';
+import { DirectedGraphEdge, UndirectedGraphEdge } from '../../../data-structures/graph/edge.three';
 import { GraphNode } from '../../../data-structures/graph/node.interface';
 import { GraphEdge } from '../../../data-structures/graph/edge.interface';
 import { layoutCalculator } from "./layout";
 
 const DashboardPosition = styled('div')({
     position: "fixed",
-    top: "10%",
-    left: "8%",
+    top: "18%",
+    left: "18%",
 });
 
 const ActionPanelPosition = styled('div')({
@@ -28,30 +28,53 @@ const ActionPanelPosition = styled('div')({
     alignItems: "center"
 });
 
-const buildDirectedGraphEdge = (source: GraphNode<number>, target: GraphNode<number>, scene: THREE.Scene): GraphEdge<number> => {
+const buildDirectedGraphEdge = (
+    source: GraphNode<number>,
+    target: GraphNode<number>,
+    scene: THREE.Scene
+): GraphEdge<number> => {
     const headLength: number = 1.2;
     const headWidth: number = 0.3;
-    return new DirectedGraphEdge(source, target, scene, edgeOriginalColor, headLength, headWidth);
+    return new DirectedGraphEdge(source, target, scene, regularEdgeColor, headLength, headWidth);
+}
+
+const buildUndirectedGraphEdge = (
+    source: GraphNode<number>,
+    target: GraphNode<number>,
+    scene: THREE.Scene
+): GraphEdge<number> => {
+    const material = new THREE.LineBasicMaterial({ color: redundantEdgeColor });
+    return new UndirectedGraphEdge(source, target, scene, material);
 }
 
 const ActionPanel = () => {
 
-    const { animate, cancelAnimate, state, setState, graph, steps, index, setIndex, disjointSet, scene } = useAlgoContext();
+    const { animate, cancelAnimate, state, setState, graph, index, setIndex, disjointSet, scene, edges } = useAlgoContext();
 
-    const reDrawGraph = async () => {
+    const reDrawGraph = async (redundant: number[]) => {
         if (!graph) {
             return;
         }
 
-        [...graph.edges].forEach(edge => graph.dropEdge(edge));
+        const [a, b] = redundant;
+        if (a !== undefined && b !== undefined) {
+            const source = graph.nodes.filter(node => node.value === a)[0];
+            const target = graph.nodes.filter(node => node.value === b)[0];
+            graph.addEdge(buildUndirectedGraphEdge(source, target, scene));
+        }
 
         Array.from(disjointSet.map.entries()).forEach(entry => {
             const [key, value] = entry;
             const source = graph.nodes.filter(node => node.value === key)[0];
             const target = graph.nodes.filter(node => node.value === value.parent.value)[0];
-            // if (source !== target) {
-            graph.addEdge(buildDirectedGraphEdge(source, target, scene));
-            // }
+
+            if (source !== target) {
+                if ((a === source.value && b === target.value) || (a === target.value && b === source.value)) {
+                    graph.addEdge(buildUndirectedGraphEdge(source, target, scene));
+                } else {
+                    graph.addEdge(buildDirectedGraphEdge(source, target, scene));
+                }
+            }
         });
 
         graph.show();
@@ -61,31 +84,49 @@ const ActionPanel = () => {
         cancelAnimate();
     }
 
+
     const handleUnion = async () => {
-        const step = steps[index];
-        if (!step) {
+        if (index >= edges.length) {
             return;
         }
 
-        setState(State.Computing);
-        const { row, col } = step;
-        disjointSet.union(row, col);
-
-        await reDrawGraph()
-
-        if (index === steps.length - 1) {
-            setState(State.Compress);
-        } else {
-            setState(State.Playing);
+        if (!graph) {
+            return;
         }
-        setIndex(i => i + 1);
-    }
 
-    const handleCompress = async () => {
+        const findEdges = (source: number, target: number) => {
+            return graph.edges.filter(edge =>
+                (edge.source.value === source && edge.target.value === target) ||
+                (edge.target.value === source && edge.source.value === target)
+            );
+        }
+
         setState(State.Computing);
-        disjointSet.compress();
-        await reDrawGraph();
-        setState(State.Finished);
+        const [a, b] = edges[index];
+
+        if (a !== undefined && b !== undefined) {
+            findEdges(a, b).forEach(edge => {
+                graph.dropEdge(edge);
+            })
+        }
+
+        const rootA = disjointSet.find(a);
+        const rootB = disjointSet.find(b);
+
+        if (rootA !== rootB) {
+            disjointSet.union(a, b);
+            await reDrawGraph([]);
+            if (index === edges.length - 1) {
+                setState(State.Finished);
+            } else {
+                setState(State.Playing);
+            }
+            setIndex(i => i + 1);
+        } else {
+            disjointSet.union(a, b);
+            await reDrawGraph([a, b]);
+            setState(State.Finished);
+        }
     }
 
     const handleResetPositions = async () => {
@@ -112,14 +153,6 @@ const ActionPanel = () => {
             </Button>
             <Button
                 sx={{ textTransform: 'none' }}
-                disabled={state !== State.Compress}
-                onClick={handleCompress}
-                startIcon={<CompressIcon />}
-            >
-                Compress
-            </Button>
-            <Button
-                sx={{ textTransform: 'none' }}
                 onClick={handleResetPositions}
                 startIcon={<RefreshIcon />}
                 disabled={graph === undefined || graph.nodes.length === 0}
@@ -130,36 +163,10 @@ const ActionPanel = () => {
     );
 }
 
-const Roots = () => {
-    const { roots, state } = useAlgoContext();
-
-    const Display = () => (
-        <ButtonGroup variant='contained' color='success'>
-            <Button sx={{ textTransform: 'none' }}>Roots</Button>
-            {
-                roots.map((root, i) => <Button key={i}>{root}</Button>)
-            }
-        </ButtonGroup>
-    );
-
-    return (
-        <>
-            {state === State.Finished && <Display />}
-        </>
-    );
-}
-
 const Main = () => (
     <>
         <DashboardPosition>
-            <Stack
-                direction="column"
-                spacing={2}
-                sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
-            >
-                <Board />
-                <Roots />
-            </Stack>
+            <Board />
         </DashboardPosition>
         <ActionPanelPosition>
             <ActionPanel />
