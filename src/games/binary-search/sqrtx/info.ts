cc02ce20dab89518690ed6e4a43be79dc6375fae
Dong Zhou <dozhou@rubiconproject.com>
2023-08-06 16:40:53 -0700

    Create info.ts

---
+++ src/games/binary-search/sqrtx/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/sqrtx",
+    categories: [Category.BinarySearch],
+    companies: [],
+    difficulty: Difficulty.Easy,
+    img: "/img/sqrtx.png",
+    leetcodeId: 69
+}
+
+export default info;
