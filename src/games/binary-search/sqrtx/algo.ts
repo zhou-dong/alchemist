a27a8c1f9b07b342de8c65215a9cd3a78f42aec6
Dong Zhou <dozhou@rubiconproject.com>
2023-08-06 21:18:43 -0700

    Update algo.ts

--- src/games/binary-search/sqrtx/algo.ts
+++ src/games/binary-search/sqrtx/algo.ts
@@ -2,18 +2,30 @@ export interface Step {
     left: number;
     right: number;
     answer: number;
-    mid?: number;
+    mid: number;
+    square: number;
 }
 
-export function mySqrt(x: number): Step[] {
+export interface Result {
+    steps: Step[];
+    sqrt: number;
+}
+
+const calculateMid = (left: number, right: number): number => {
+    return left + Math.floor((right - left) / 2);
+}
+
+export function mySqrt(x: number): Result {
 
     const steps: Step[] = [];
 
     let [left, right, answer] = [0, x, -1];
-    steps.push({ left, right, answer });
+    const mid = calculateMid(left, right);
+    steps.push({ left, right, mid, square: mid * mid, answer });
 
     while (left <= right) {
-        const mid = left + Math.floor((right - left) / 2);
+        const mid = calculateMid(left, right);
+
         if (mid * mid <= x) {
             answer = mid;
             left = mid + 1;
@@ -21,8 +33,11 @@ export function mySqrt(x: number): Step[] {
             right = mid - 1;
         }
 
-        steps.push({ left, right, answer, mid });
+        const nextMid = calculateMid(left, right);
+        steps.push({ left, right, answer, mid: nextMid, square: nextMid * nextMid });
     }
 
-    return steps;
+    steps.pop();
+
+    return { steps, sqrt: answer };
 };
