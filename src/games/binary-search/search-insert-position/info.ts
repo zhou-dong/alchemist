1495f71a159a88710e7258d0e79612e181b951cb
Dong Zhou <dozhou@rubiconproject.com>
2023-08-07 00:08:37 -0700

    Create info.ts

---
+++ src/games/binary-search/search-insert-position/info.ts
@@ -0,0 +1,16 @@
+import { title } from "./contents";
+import { Game } from "../../commons/game";
+import Category from "../../commons/segments/category";
+import Difficulty from "../../commons/segments/difficulty";
+
+const info: Game = {
+    name: title,
+    path: "/algorithms/search-insert-position",
+    categories: [Category.BinarySearch],
+    companies: [],
+    difficulty: Difficulty.Easy,
+    img: "/img/search-insert-position.png",
+    leetcodeId: 35
+}
+
+export default info;
