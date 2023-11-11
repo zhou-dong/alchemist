6552f98404914b96046c35645e277db84308998f
DONG ZHOU <82224165@qq.com>
2023-11-11 02:04:18 -0800

    Create Header.tsx

---
+++ src/games/two-pointer/remove-element/Header.tsx
@@ -0,0 +1,33 @@
+import { styled } from '@mui/system';
+import { Stack, Typography } from '@mui/material';
+import Switcher, { Solution } from "./Switcher";
+import { title } from "./description";
+
+interface Props {
+    solution: Solution;
+    setSolution: React.Dispatch<React.SetStateAction<Solution>>;
+}
+
+const Position = styled("div")({
+    position: "fixed",
+    top: 40,
+    width: "100%",
+    zIndex: 1,
+});
+
+const Title = () => (
+    <Typography variant='body1'>
+        {title}
+    </Typography>
+);
+
+const Main = ({ solution, setSolution }: Props) => (
+    <Position>
+        <Stack direction="row" spacing={2} sx={{ alignItems: "center", justifyContent: "center" }}>
+            <Title />
+            <Switcher solution={solution} setSolution={setSolution} />
+        </Stack>
+    </Position>
+);
+
+export default Main;
