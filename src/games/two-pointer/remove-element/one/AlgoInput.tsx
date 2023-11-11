fa79fca9e7010de0aa97f9e0b9b9632865d4f2f5
DONG ZHOU <82224165@qq.com>
2023-11-11 02:12:31 -0800

    init solution one

---
+++ src/games/two-pointer/remove-element/one/AlgoInput.tsx
@@ -0,0 +1,79 @@
+import * as React from 'react';
+import OutputIcon from '@mui/icons-material/Output';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import { Divider, InputBase } from '@mui/material';
+import ClearIcon from '@mui/icons-material/Clear';
+import { defaultValue, useAlgoContext } from "./AlgoContext";
+import { State } from './AlgoState';
+import { plusOne } from "./algo";
+
+interface Props {
+    setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
+}
+
+const Submit: React.FC<{
+    input: string,
+    setInput: React.Dispatch<React.SetStateAction<string>>,
+    setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
+}> = ({ input, setInput, setAnchorEl }) => {
+
+    const { setValue, setState, setIndex, setActions } = useAlgoContext();
+
+    const handleSubmit = () => {
+        let newValue: string = (!input) ? defaultValue + "" : input;
+
+        const digits = newValue.split("").map(num => +num);
+
+        setValue(+newValue);
+        setState(State.Playing);
+        setInput("");
+        setAnchorEl(null);
+
+        setIndex(0);
+        setActions(plusOne(digits))
+    }
+
+    return (
+        <IconButton sx={{ p: '10px' }} aria-label="submit input" onClick={handleSubmit}>
+            <OutputIcon />
+        </IconButton>
+    );
+}
+
+const Main = ({ setAnchorEl }: Props) => {
+
+    const [input, setInput] = React.useState("");
+
+    const handleTextFieldChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value);
+    }
+
+    return (
+        <Paper
+            variant="elevation"
+            elevation={8}
+            sx={{
+                p: '2px 4px',
+                display: 'flex',
+                width: 200,
+                alignItems: "center"
+            }}
+        >
+            <InputBase
+                sx={{ ml: 1, flex: 1 }}
+                placeholder={defaultValue + ""}
+                value={input}
+                onChange={handleTextFieldChange}
+                type='number'
+            />
+            <IconButton type="button" sx={{ p: '10px' }} aria-label="clear" onClick={() => setInput("")}>
+                <ClearIcon />
+            </IconButton>
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <Submit input={input} setInput={setInput} setAnchorEl={setAnchorEl} />
+        </Paper>
+    );
+}
+
+export default Main;
---
+++ src/games/two-pointer/remove-element/two/index.tsx
@@ -0,0 +1,11 @@
+const Main = () => {
+
+
+    return (
+        <>
+            Two
+        </>
+    )
+}
+
+export default Main;
