264aab75d4ba13e1a60e736e37c61dcc78f3792c
DONG ZHOU <82224165@qq.com>
2023-11-23 02:17:44 -0800

    init merge-two-sorted-lists

---
+++ src/games/two-pointer/merge-two-sorted-lists/one/Introduction.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+import ReactMarkdown from "react-markdown";
+import MuiStack from '@mui/material/Stack';
+import { Popover, PopoverOrigin, ToggleButton } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import Instruction from '../../../../commons/Instruction';
+import { description, examples } from "../description";
+import AlgoInput from "./AlgoInput";
+import LightTooltip from '../../../../commons/LightTooltip';
+import InputIcon from '@mui/icons-material/Input';
+
+const capitalize = (name: string): string => {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+const anchorOrigin: PopoverOrigin = {
+    vertical: 'center',
+    horizontal: 'right',
+};
+
+const transformOrigin: PopoverOrigin = {
+    vertical: 'center',
+    horizontal: 'left',
+};
+
+const StyledReactMarkdown = styled(ReactMarkdown)(() => ({
+    fontSize: "16px",
+    marginTop: 0,
+    paddingTop: 0,
+    paddingLeft: 10,
+    paddingRight: 10,
+}));
+
+const Input = () => {
+
+    const name = "input";
+    const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
+    const open = Boolean(anchorEl);
+
+    const handleToggle = (event: React.MouseEvent<HTMLElement>) => {
+        anchorEl ? setAnchorEl(null) : setAnchorEl(event.currentTarget);
+    }
+
+    const reference = React.useRef(null);
+
+    React.useEffect(() => {
+        setAnchorEl(reference.current);
+    }, [reference])
+
+    return (
+        <>
+            <LightTooltip title={capitalize(name)} placement="right">
+                <ToggleButton
+                    ref={reference}
+                    onChange={handleToggle}
+                    aria-label={name}
+                    size="large"
+                    sx={{ borderRadius: "50%" }}
+                    value={name}
+                    selected={open}
+                >
+                    <InputIcon fontSize="medium" />
+                </ToggleButton>
+            </LightTooltip>
+            <Popover
+                open={open}
+                anchorEl={anchorEl}
+                onClose={() => setAnchorEl(null)}
+                anchorOrigin={anchorOrigin}
+                transformOrigin={transformOrigin}
+            >
+                <AlgoInput setAnchorEl={setAnchorEl} />
+            </Popover>
+        </>
+    )
+}
+
+const Main = () => (
+    <MuiStack spacing={2} sx={{ position: 'fixed', top: 112, left: 40, zIndex: 1 }}>
+        <Input />
+        <Instruction
+            name="Description"
+            icon={<DescriptionOutlinedIcon fontSize="medium" />}
+            popover={<StyledReactMarkdown>{description + examples}</StyledReactMarkdown>}
+            anchorOrigin={anchorOrigin}
+            transformOrigin={transformOrigin}
+        />
+    </MuiStack>
+);
+
+export default Main;
---
+++ src/games/two-pointer/merge-two-sorted-lists/one/Play.tsx
@@ -0,0 +1,229 @@
+import { styled } from '@mui/system';
+import { Button, ButtonGroup, Paper, Stack, Table, TableBody, TableCell, TableRow } from "@mui/material";
+import { useAlgoContext } from "./AlgoContext";
+import { State } from "./AlgoState";
+import React from 'react';
+import CodeBlock, { languages } from '../../../dp/_components/CodeBlock';
+import ChangeCircleIcon from '@mui/icons-material/ChangeCircle';
+import NextPlanIcon from '@mui/icons-material/NextPlan';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
+const formula = `function removeElement(nums: number[], val: number): number {
+
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] !== val) {
+            nums[left] = nums[right];
+            left++;
+        }
+    }
+
+    return left;
+};`;
+
+const CodeDisplay = () => {
+    const { index, actions } = useAlgoContext();
+    const action = actions[index];
+    const linesToHighlight = (!action) ? [] : action.linesToHighlight;
+
+    return (
+        <Paper>
+            <CodeBlock
+                code={formula}
+                language={languages.Typescript}
+                showLineNumbers={true}
+                linesToHighlight={linesToHighlight}
+                wrapLines={true}
+            />
+        </Paper>
+    );
+}
+
+const Dashboard = () => {
+    const { actions, index } = useAlgoContext();
+    const action = actions[index];
+
+    const nums: number[] = action?.nums || [];
+    const left: number = action?.left || 0;
+    const right: number = action?.right || 0;
+    const val: string = (action) ? action.val + "" : ""
+
+    const baseStyle: React.CSSProperties = { minWidth: 50 };
+    const indexStyle: React.CSSProperties = { ...baseStyle, border: "none" };
+
+    const getStyle = (i: number): React.CSSProperties => {
+        if (left === i) {
+            return { ...baseStyle, backgroundColor: "gold" };
+        } else if (right === i) {
+            return { ...baseStyle, backgroundColor: "green" };
+        } else {
+            return { ...baseStyle };
+        }
+    }
+
+    const getLeftStyle = (i: number): React.CSSProperties => {
+        if (i === left) {
+            return { ...indexStyle, border: "none", backgroundColor: "gold" };
+        } else {
+            return indexStyle;
+        }
+    }
+
+    const getRightStyle = (i: number): React.CSSProperties => {
+        if (i === right) {
+            return { ...indexStyle, backgroundColor: "green" };
+        } else {
+            return indexStyle;
+        }
+    }
+    return (
+        <Table>
+            <TableBody>
+                <TableRow>
+                    <TableCell padding='none' sx={indexStyle}>val</TableCell>
+                    <TableCell padding='none' sx={indexStyle} colSpan={nums.length} >{val}</TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell padding='none' sx={baseStyle}>nums</TableCell>
+                    {
+                        nums.map((num, i) => <TableCell key={i} padding='none' sx={getStyle(i)}>{num}</TableCell>)
+                    }
+                </TableRow>
+                <TableRow>
+                    <TableCell padding='none' sx={indexStyle}>left</TableCell>
+                    {
+                        nums.map((num, i) =>
+                            <TableCell key={i} padding='none' sx={getLeftStyle(i)}>
+                                {(i === left) ? num : ""}
+                            </TableCell>
+                        )
+                    }
+                </TableRow>
+                <TableRow>
+                    <TableCell padding='none' sx={indexStyle}>right</TableCell>
+                    {
+                        nums.map((num, i) =>
+                            <TableCell key={i} padding='none' sx={getRightStyle(i)} >
+                                {(i === right) ? num : ""}
+                            </TableCell>
+                        )
+                    }
+                </TableRow>
+            </TableBody>
+        </Table>
+    );
+};
+
+const Result = () => {
+    const { actions } = useAlgoContext();
+
+    if (actions.length === 0) {
+        return <></>;
+    }
+
+    const last = actions[actions.length - 1];
+    const left = last.left;
+    const nums = last.nums;
+
+    return (
+        <Table>
+            <TableBody>
+                <TableRow>
+                    <TableCell padding='none'>result</TableCell>
+                    {
+                        nums.slice(0, left).map((num, i) => <TableCell key={i} padding='none'>{num}</TableCell>)
+                    }
+                </TableRow>
+            </TableBody>
+        </Table>
+    );
+}
+
+const Action = () => {
+
+    const { setIndex, index, actions } = useAlgoContext();
+    const step = actions[index];
+    const action = step?.action;
+    const disabled = (index >= actions.length);
+
+    type BntColor = "info" | "error";
+    const [color, setColor] = React.useState<BntColor>("info");
+
+    return (
+        <ButtonGroup disabled={disabled} color={color}>
+            <Button
+                disabled={action !== "Start"} startIcon={<PlayCircleOutlineIcon />} onClick={() => {
+                    setIndex(i => i + 1);
+                }}>
+                Start
+            </Button>
+            <Button disabled={action === "Start" || action === "Finish"} startIcon={<NextPlanIcon />} onClick={() => {
+                if (action === "Next") {
+                    setColor("info");
+                    setIndex(i => i + 1);
+                } else {
+                    setColor("error");
+                }
+            }}>
+                next
+            </Button>
+            <Button disabled={action === "Start" || action === "Finish"} startIcon={<ChangeCircleIcon />} onClick={() => {
+                if (action === "Update") {
+                    setColor("info");
+                    setIndex(i => i + 1);
+                } else {
+                    setColor("error");
+                }
+            }}>
+                update
+            </Button>
+            <Button disabled={action !== "Finish"} startIcon={<CheckCircleOutlineIcon />} onClick={() => {
+                setIndex(i => i + 1);
+            }}>
+                finish
+            </Button>
+        </ButtonGroup>
+    );
+}
+
+const Main = () => {
+    const { actions, index } = useAlgoContext();
+    const action = actions[index];
+
+    return (
+        <Stack
+            sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+            spacing={4}
+            direction="column"
+        >
+            <CodeDisplay />
+            {(action) ? <Dashboard /> : < Result />}
+            <Action />
+        </Stack>
+    );
+};
+
+const Position = styled("div")({
+    position: "fixed",
+    top: 200,
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "40%",
+    zIndex: 0
+});
+
+const Play = () => {
+    const { state } = useAlgoContext();
+
+    return (
+        <Position>
+            {state !== State.Typing && <Main />}
+        </Position>
+    );
+}
+
+export default Play;
