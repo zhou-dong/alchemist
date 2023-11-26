faa04cd2e9f57a43e3ceee835a088d753e14dfe1
DONG ZHOU <82224165@qq.com>
2023-11-26 03:26:39 -0800

    Update AlgoContext.tsx

--- src/games/two-pointer/merge-two-sorted-lists/one/AlgoContext.tsx
+++ src/games/two-pointer/merge-two-sorted-lists/one/AlgoContext.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import * as THREE from 'three';
 import { State } from "./AlgoState";
 import { Step } from "./algo";
+import { clearScene } from "../../../../commons/three";
 
 const AlgoContext = React.createContext<{
+    scene: THREE.Scene,
+    animate: () => void,
+    cancelAnimate: () => void,
     state: State,
     setState: React.Dispatch<React.SetStateAction<State>>,
     index: number,
@@ -10,6 +15,9 @@ const AlgoContext = React.createContext<{
     actions: Step[],
     setActions: React.Dispatch<React.SetStateAction<Step[]>>
 }>({
+    scene: new THREE.Scene(),
+    animate: () => { },
+    cancelAnimate: () => { },
     state: State.Typing,
     setState: () => { },
     index: 0,
@@ -18,6 +26,8 @@ const AlgoContext = React.createContext<{
     setActions: () => { }
 });
 
+let animationFrameId = -1;
+
 export const AlgoContextProvider: React.FC<{
     children: React.ReactNode,
     renderer: THREE.Renderer,
@@ -25,12 +35,40 @@ export const AlgoContextProvider: React.FC<{
     scene: THREE.Scene
 }> = ({ children, renderer, camera, scene }) => {
 
+    camera.position.z = 20;
     const [state, setState] = React.useState(State.Typing);
     const [index, setIndex] = React.useState(0);
     const [actions, setActions] = React.useState<Step[]>([]);
 
+    function animate() {
+        animationFrameId = requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+
+    function cancelAnimate() {
+        cancelAnimationFrame(animationFrameId);
+    }
+
+    function init() {
+        clearScene(scene);
+        renderer.render(scene, camera);
+    }
+
+    init();
+
+    const ref = React.useRef<HTMLDivElement>(null);
+
+    React.useEffect(() => {
+        if (ref && ref.current) {
+            ref.current.appendChild(renderer.domElement);
+        }
+    }, [ref, renderer]);
+
     return (
         <AlgoContext.Provider value={{
+            scene,
+            animate,
+            cancelAnimate,
             state,
             setState,
             index,
@@ -39,6 +77,7 @@ export const AlgoContextProvider: React.FC<{
             setActions
         }}>
             {children}
+            <div ref={ref} />
         </AlgoContext.Provider>
     );
 }
