8e3e0245a9be3bbc6bc966b0e886f4be9a463ba8
DONG ZHOU <82224165@qq.com>
2023-12-22 04:02:17 -0800

    Update node.three.ts

--- src/data-structures/list/linked-list/node.three.ts
+++ src/data-structures/list/linked-list/node.three.ts
@@ -4,31 +4,11 @@ import { TextCube } from '../../_commons/cube/three/text-cube';
 export class LinkedListNode<T> implements ILinkedListNode<TextCube<T>> {
     data: TextCube<T>;
 
-    private _next?: LinkedListNode<T>;
-    private _prev?: LinkedListNode<T>;
+    next?: LinkedListNode<T>;
+    prev?: LinkedListNode<T>;
 
     constructor(data: TextCube<T>) {
         this.data = data;
-    }
-
-    set next(promise: Promise<LinkedListNode<T>> | undefined) {
-        promise?.then(node => this._next = node);
-    }
-
-    get next(): Promise<LinkedListNode<T>> | undefined {
-        if (this._next) {
-            return Promise.resolve(this._next);
-        }
-    }
-
-    set prev(promise: Promise<LinkedListNode<T>> | undefined) {
-        promise?.then(node => this._prev = node);
-    }
-
-    get prev(): Promise<LinkedListNode<T>> | undefined {
-        if (this._prev) {
-            return Promise.resolve(this._prev);
-        }
     }
 
 }
