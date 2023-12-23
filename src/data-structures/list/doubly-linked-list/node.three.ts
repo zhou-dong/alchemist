210568d9c18faea942e9b38b8c2108e68614dda5
DONG ZHOU <82224165@qq.com>
2023-12-23 15:16:26 -0800

    Update node.three.ts

--- src/data-structures/list/doubly-linked-list/node.three.ts
+++ src/data-structures/list/doubly-linked-list/node.three.ts
@@ -1,86 +1,9 @@
 import * as THREE from 'three';
-import { TextGeometry, TextGeometryParameters } from 'three/examples/jsm/geometries/TextGeometry';
-import Displayer from "../../_commons/params/displayer.interface";
-import DisplayerImpl from "../../_commons/three/displayer.class";
-import Position from "../../_commons/params/position.interface";
-import PositionImpl from '../../_commons/three/position.class';
-import Mover from '../../_commons/params/mover.interface';
-import MoverImpl from '../../_commons/three/mover.class';
-import Color from '../../_commons/params/color.interface';
-import ColorImpl from '../../_commons/three/color.class';
+import { TextGeometryParameters } from 'three/examples/jsm/geometries/TextGeometry';
 import { font } from '../../../commons/three';
 import { DoublyLinkedListNode as ILinkedListNode } from "./node.interface";
-import { calDestination, calDistance } from '../../_commons/utils';
 import { Link } from '../link.three';
 import { LinkedListBaseNode } from '../list-node-base';
-
-class NodeBase extends PositionImpl implements Mover, Displayer, Position, Color {
-
-    private displayer: Displayer;
-    private mover: Mover;
-    private readonly colour: Color;
-
-    constructor(
-        scene: THREE.Scene,
-        geometry: THREE.BufferGeometry,
-        material: THREE.Material
-    ) {
-        const mesh = new THREE.Mesh(geometry, material);
-        super(mesh);
-        this.colour = new ColorImpl(material);
-        this.displayer = new DisplayerImpl(scene, mesh);
-        this.mover = new MoverImpl(mesh);
-    }
-
-    setColor(color: string): Promise<void> {
-        return this.colour.setColor(color);
-    }
-
-    get color(): string {
-        return this.colour.color;
-    }
-
-    set color(c: string) {
-        this.colour.color = c;
-    }
-
-    show() {
-        this.displayer.show();
-    }
-
-    hide() {
-        this.displayer.hide();
-    }
-
-    move(position: Position, duration: number, onUpdate?: (() => void) | undefined) {
-        return this.mover.move(position, duration, onUpdate);
-    }
-}
-
-class NodeSkin extends NodeBase {
-    distance(position: Position): Position {
-        return calDistance({ x: this.x, y: this.y, z: this.z }, position);
-    }
-}
-
-class NodeText extends NodeBase {
-    text: string;
-
-    constructor(
-        text: string,
-        scene: THREE.Scene,
-        geometryParameters: TextGeometryParameters,
-        material: THREE.Material
-    ) {
-        const geometry = new TextGeometry(text, geometryParameters);
-        super(scene, geometry, material);
-        this.text = text;
-    }
-
-    destinate(distance: Position) {
-        return calDestination({ x: this.x, y: this.y, z: this.z }, distance);
-    }
-}
 
 export class DoublyLinkedListNode<T> extends LinkedListBaseNode<T> implements ILinkedListNode<T> {
 
