87798df6d13fde58d0e674c5aac498e6543385cb
DONG ZHOU <82224165@qq.com>
2023-12-22 21:21:10 -0800

    Update node.interface.ts

--- src/data-structures/list/linked-list/node.interface.ts
+++ src/data-structures/list/linked-list/node.interface.ts
@@ -1,9 +1,8 @@
-import Color from "../../_commons/params/color.interface";
 import Displayer from "../../_commons/params/displayer.interface";
 import Mover from "../../_commons/params/mover.interface";
 import Position from "../../_commons/params/position.interface";
 
-export interface LinkedListNode<T> extends Mover, Displayer, Color, Position {
+export interface LinkedListNode<T> extends Mover, Displayer, Position {
     data: T;
     next?: LinkedListNode<T>;
     prev?: LinkedListNode<T>;
