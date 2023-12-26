549b08967145c6856fab1aafb0aeff92d6f4aafe
DONG ZHOU <82224165@qq.com>
2023-12-25 21:00:26 -0800

    Update AlgoInput.tsx

--- src/games/two-pointer/merge-two-sorted-lists/one/AlgoInput.tsx
+++ src/games/two-pointer/merge-two-sorted-lists/one/AlgoInput.tsx
@@ -6,10 +6,8 @@ import { Stack, TextField } from '@mui/material';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { useAlgoContext } from "./AlgoContext";
 import { State } from './AlgoState';
-import { LinkedList } from "../../../../data-structures/list/linked-list/list.three";
-import { LinkedListNode } from "../../../../data-structures/list/linked-list/node.three";
 import { clearScene } from "../../../../commons/three";
-import { buildLinkedListNode, linkColor } from "../styles";
+import { buildList } from "../styles";
 import { buildActions } from './code';
 
 const buildTwoArraies = () => {
@@ -67,38 +65,12 @@ const Submit: React.FC<{
         setAnchorEl(null);
         clearScene(scene);
 
-        const linkLength = 4;
-        const duration = 1;
-
-        const buildHead = (i: number, y: number): LinkedListNode<number> => {
-            const textX = ((i + "").length === 1) ? -0.4 : -0.6;
-            return buildLinkedListNode(scene, i, { x: -8, y, z: 0 }, { x: -8 + textX, y: y + 0.3, z: 0 })
-        }
-
-        const buildNode = (i: number): LinkedListNode<number> => {
-            const textX = ((i + "").length === 1) ? -0.4 : -0.6;
-            return buildLinkedListNode(scene, i, { x: 0, y: 0, z: 0 }, { x: textX, y: 0.3, z: 0 })
-        }
-
-        const buildList = async (array: number[], y: number): Promise<LinkedListNode<number>> => {
-            const list = new LinkedList<number>(scene, duration, linkColor, linkLength);
-            const head = buildHead(array[0], y);
-            await list.push(head);
-            for (let i = 1; i < array.length; i++) {
-                await list.push(buildNode(array[i]));
-            }
-            return head;
-        }
-
         try {
             animate();
-
-            const head1 = await buildList(first, 8);
-            const head2 = await buildList(second, 2);
-
+            const head1 = await buildList(scene, first, 8);
+            const head2 = await buildList(scene, second, 2);
             setList1(head1);
             setList2(head2);
-
             const actions = buildActions(head1, head2);
             setActions(actions);
         } catch (error) {
