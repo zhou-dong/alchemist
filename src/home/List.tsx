448fde4697f29c6c935bf567bd630bc33c4df1e6
DONG ZHOU <82224165@qq.com>
2023-12-27 04:14:36 -0800

    Update List.tsx

--- src/home/List.tsx
+++ src/home/List.tsx
@@ -9,8 +9,8 @@ interface AlgorithmProps {
 }
 
 const Algorithm = ({ title, path, img }: AlgorithmProps) => (
-    <Grid item xs={6} sm={4} md={3} lg={2}>
-        <Card variant="outlined" sx={{ borderRadius: "15px" }}>
+    <Grid item xs={6} sm={4} md={3} lg={2} xl={1.5}>
+        <Card variant="outlined" sx={{ borderRadius: "15px", padding: 1, paddingBottom: 0 }}>
             <CardActionArea component={RouterLink} to={path}>
                 <CardMedia
                     component="img"
@@ -18,7 +18,7 @@ const Algorithm = ({ title, path, img }: AlgorithmProps) => (
                 >
                 </CardMedia>
             </CardActionArea>
-            <CardContent sx={{ maxHeight: "64px" }}>
+            <CardContent style={{ paddingBottom: 1 }} sx={{ maxHeight: "64px" }}>
                 <Typography variant="body1">
                     {title}
                 </Typography>
@@ -32,7 +32,7 @@ const Sorting = () => {
     const { games } = useGames();
 
     return (
-        <Grid container spacing={3}>
+        <Grid container spacing={1}>
             {
                 games.map((game, index) => (
                     <Algorithm
