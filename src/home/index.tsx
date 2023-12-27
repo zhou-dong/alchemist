d2f54982b5b6144b2d3f4096ca1e7fb577164aec
DONG ZHOU <82224165@qq.com>
2023-12-27 04:14:41 -0800

    Update index.tsx

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,24 +1,24 @@
-
 import * as React from 'react';
 import Filters from "./Filters";
 import List from './List';
 import Footer from '../commons/Footer';
-import { Container, ThemeProvider } from '@mui/material';
+import { ThemeProvider } from '@mui/material';
 import theme from '../commons/theme';
 import Header from "./Header";
+
 const Home = () => {
     const [open, setOpen] = React.useState<boolean>(false);
 
     return (
         <ThemeProvider theme={theme}>
-            <Container maxWidth="xl">
+            <div style={{ marginLeft: 30, marginRight: 30 }}>
                 <Header open={open} setOpen={setOpen} />
                 <Filters open={open} setOpen={setOpen} />
                 <List />
                 <Footer />
-            </Container>
+            </div>
         </ThemeProvider>
-    )
+    );
 };
 
 export default Home;
