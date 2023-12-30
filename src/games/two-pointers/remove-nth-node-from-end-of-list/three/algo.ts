c797b44bfdd0a24f2318358182b497949faa0323
DONG ZHOU <82224165@qq.com>
2023-12-30 05:46:55 -0800

    Update algo.ts

--- src/games/two-pointers/remove-nth-node-from-end-of-list/three/algo.ts
+++ src/games/two-pointers/remove-nth-node-from-end-of-list/three/algo.ts
@@ -1,29 +1,80 @@
-// Definition for singly-linked list.
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
+import Position from "../../../../data-structures/_commons/params/position.interface";
+import { LinkedListNode } from "../../../../data-structures/list/linked-list/node.three";
+import { adjustX, adjustY, buildLinkedListNode } from "../styles";
+
+const skinDummyColor = "lightgray";
+const dummySkinPosition: Position = { x: -15, y: 9, z: 0 }
+const dummyTextPosition: Position = { x: adjustX(0, dummySkinPosition.x), y: adjustY(dummySkinPosition.y), z: dummySkinPosition.z };
+const buildDummy = (scene: THREE.Scene) => {
+    const dummy: LinkedListNode<number> = buildLinkedListNode(scene, -1, "D", dummySkinPosition, dummyTextPosition);
+    dummy.nodeSkin.color = skinDummyColor;
+    return dummy;
 }
 
-export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+export enum Action {
+    Ready,
+    Define_Length,
+    Define_Current,
+    Count_Length,
+    New_Dummy,
+    Link_Dummy_Head,
+    Set_Current_To_Dummy,
+    Forward_Current,
+    Remove_Next,
+    Return_Head
+}
 
-    const dummy = new ListNode();
-    let current = dummy;
+export interface Item {
+    action: Action;
+    length?: number;
+    dummy?: LinkedListNode<number>;
+    current?: LinkedListNode<number>;
+    linesToHighlight: number[],
+}
 
-    while (list1 !== null && list2 !== null) {
-        if (list1.val < list2.val) {
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
+export function buildItems(scene: THREE.Scene, head: LinkedListNode<number> | undefined, n: number): Item[] {
+    const items: Item[] = [];
+
+    function removeNthFromEnd(head: LinkedListNode<number> | undefined, n: number): LinkedListNode<number> | undefined {
+
+        items.push({ action: Action.Ready, linesToHighlight: [] });
+
+        let length = 0;
+        items.push({ length, action: Action.Define_Length, linesToHighlight: [] });
+
+        let current = head;
+        items.push({ length, current, action: Action.Define_Current, linesToHighlight: [] });
+
+        while (current) {
+            length++;
+            current = current.next;
+            items.push({ length, current, action: Action.Count_Length, linesToHighlight: [] });
         }
-        current = current.next;
-    }
 
-    current.next = list1 === null ? list2 : list1;
-    return dummy.next;
-};
+        const dummy = buildDummy(scene);
+        items.push({ length, current, dummy, action: Action.New_Dummy, linesToHighlight: [] });
+
+        dummy.next = head;
+        items.push({ length, current, dummy, action: Action.Link_Dummy_Head, linesToHighlight: [] });
+
+        current = dummy;
+        items.push({ length, current, dummy, action: Action.Set_Current_To_Dummy, linesToHighlight: [] });
+
+        for (let i = 0; i < length - n; i++) {
+            current = current?.next;
+            items.push({ length, current, dummy, action: Action.Forward_Current, linesToHighlight: [] });
+        }
+
+        if (current?.next) {
+            current.next = current.next.next;
+            items.push({ length, dummy, action: Action.Remove_Next, linesToHighlight: [] });
+        }
+
+        items.push({ length, dummy, action: Action.Return_Head, linesToHighlight: [] });
+        return dummy.next;
+    };
+
+
+    removeNthFromEnd(head, n);
+    return items;
+}
