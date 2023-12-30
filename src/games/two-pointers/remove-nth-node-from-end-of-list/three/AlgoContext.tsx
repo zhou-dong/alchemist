a10a8e12e9cd83b238aac5ac01dc99e65e9925cb
DONG ZHOU <82224165@qq.com>
2023-12-30 04:44:48 -0800

    Update AlgoContext.tsx

--- src/games/two-pointers/remove-nth-node-from-end-of-list/three/AlgoContext.tsx
+++ src/games/two-pointers/remove-nth-node-from-end-of-list/three/AlgoContext.tsx
@@ -32,7 +32,7 @@ const AlgoContext = React.createContext<{
     setState: () => { },
     setNode1: () => { },
     setNode2: () => { },
-    linesToHighlight: [4],
+    linesToHighlight: [1],
     setLinesToHighlight: () => { },
     current: (null as any),
     setCurrent: () => { },
@@ -58,7 +58,7 @@ export const AlgoContextProvider: React.FC<{
     const [node1, setNode1] = React.useState<LinkedListNode<number>>();
     const [node2, setNode2] = React.useState<LinkedListNode<number>>();
     const [current, setCurrent] = React.useState<LinkedListNode<number>>((null as any));
-    const [linesToHighlight, setLinesToHighlight] = React.useState<number[]>([4]);
+    const [linesToHighlight, setLinesToHighlight] = React.useState<number[]>([1]);
 
     const [list, setList] = React.useState("");
     const [n, setN] = React.useState(0);
