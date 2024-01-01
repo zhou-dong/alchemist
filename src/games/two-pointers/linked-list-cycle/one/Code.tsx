d307ba58d6ac14fd31c0455aa5b7b70aedac421b
DONG ZHOU <82224165@qq.com>
2024-01-01 03:45:27 -0800

    Update Code.tsx

--- src/games/two-pointers/linked-list-cycle/one/Code.tsx
+++ src/games/two-pointers/linked-list-cycle/one/Code.tsx
@@ -14,26 +14,19 @@ export const skinFastColor = "lightgreen";
 export const skinSlowColor = "green";
 export const skinDummyColor = "lightgray";
 
-const formula = `function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+const formula = `function hasCycle(head: ListNode | null): boolean {
+    let fast = head
+    let slow = head;
 
-    const dummy = new ListNode();
-    dummy.next = head;
-
-    let fast = dummy;
-    let slow = dummy;
-
-    for (let i = 0; i < n; i++) {
-        fast = fast.next;
-    }
-
-    while (fast.next) {
-        fast = fast.next;
+    while (fast !== null && fast.next !== null) {
+        fast = fast.next.next;
         slow = slow.next;
+        if (fast === slow) {
+            return true;
+        }
     }
 
-    slow.next = slow.next.next;
-
-    return dummy.next;
+    return false;
 };`
 
 const getLinesToHighlight = (action: Action): number[] => {
