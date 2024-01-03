be6c6801356f9a2b482cc953d0ca6c02a64491da
DONG ZHOU <82224165@qq.com>
2024-01-03 01:52:55 -0800

    Update index.tsx

--- src/games/two-pointers/linked-list-cycle/index.tsx
+++ src/games/two-pointers/linked-list-cycle/index.tsx
@@ -1,3 +1,7 @@
+import GameWrapper from '../../commons/GameWrapper';
+import info from "./info";
+import { ThemeProvider } from '@mui/material';
+import theme from '../../dp/_commons/theme';
 import { AlgoContextProvider } from "./AlgoContext";
 import Introduction from "./Introduction";
 import Play from "./Play";
@@ -10,11 +14,15 @@ interface Props {
 }
 
 const Main = ({ renderer, camera, scene }: Props) => (
-    <AlgoContextProvider renderer={renderer} camera={camera} scene={scene}>
-        <Header />
-        <Introduction />
-        <Play />
-    </AlgoContextProvider>
+    <GameWrapper path={info.path}>
+        <ThemeProvider theme={theme}>
+            <AlgoContextProvider renderer={renderer} camera={camera} scene={scene}>
+                <Header />
+                <Introduction />
+                <Play />
+            </AlgoContextProvider>
+        </ThemeProvider>
+    </GameWrapper>
 );
 
 export default Main;
