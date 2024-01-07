108bab969553a4d8fd9a9e9704444e2d6b99c85b
DONG ZHOU <82224165@qq.com>
2024-01-07 04:47:14 -0800

    Update AlgoInput.tsx

--- src/games/linked-list/swap-nodes-in-pairs/one/AlgoInput.tsx
+++ src/games/linked-list/swap-nodes-in-pairs/one/AlgoInput.tsx
@@ -1,46 +1,36 @@
 import * as React from 'react';
 import OutputIcon from '@mui/icons-material/Output';
 import IconButton from '@mui/material/IconButton';
+import InputIcon from '@mui/icons-material/Input';
 import Paper from '@mui/material/Paper';
-import { Stack, TextField } from '@mui/material';
+import { InputBase } from '@mui/material';
 import RefreshIcon from '@mui/icons-material/Refresh';
 import { useAlgoContext } from "./AlgoContext";
 import { State } from '../AlgoState';
 import { clearScene } from "../../../../commons/three";
-import { buildList } from "../styles";
+import { buildList, center } from "../styles";
 import { buildActions } from './algo';
+import ClearIcon from '@mui/icons-material/Clear';
 
-const buildTwoArraies = () => {
-
+const buildRandomList = (length: number): number[] => {
     const max = 20;
-    const arrayLength = 6;
 
     const pool: number[] = [];
     for (let i = 0; i < max; i++) {
         pool.push(i);
     }
 
-    const first: number[] = [];
-    const second: number[] = [];
-
-    for (let i = 0; i < arrayLength; i++) {
+    const list: number[] = [];
+    for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * pool.length);
         const selectedNumber = pool[randomIndex];
-        first.push(selectedNumber);
+        list.push(selectedNumber);
         pool.splice(randomIndex, 1);
     }
 
-    for (let i = 0; i < arrayLength; i++) {
-        const randomIndex = Math.floor(Math.random() * pool.length);
-        const selectedNumber = pool[randomIndex];
-        second.push(selectedNumber);
-        pool.splice(randomIndex, 1);
-    }
-
-    first.sort((a, b) => a - b);
-    second.sort((a, b) => a - b);
-
-    return [first, second];
+    const result = list.map(n => n + 1);
+    result.sort((a, b) => a - b);
+    return result;
 }
 
 interface Props {
@@ -48,15 +38,13 @@ interface Props {
 }
 
 const Submit: React.FC<{
-    nums1: string,
-    nums2: string,
+    list: string,
     setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
-}> = ({ nums1, nums2, setAnchorEl }) => {
+}> = ({ list, setAnchorEl }) => {
 
-    const first: number[] = nums1.split(",").map(num => +num);
-    const second: number[] = nums2.split(",").map(num => +num);
+    const nums: number[] = list.split(",").map(num => +num);
 
-    const disabled = !nums1 || !nums2 || first.length === 0 || second.length === 0;
+    const disabled = !list || nums.length === 0;
 
     const { setState, animate, cancelAnimate, scene, setActions, setIndex } = useAlgoContext();
 
@@ -65,12 +53,13 @@ const Submit: React.FC<{
         setAnchorEl(null);
         clearScene(scene);
         setActions([]);
+        setIndex(0);
 
         try {
             animate();
-            const head1 = await buildList(scene, first, 9);
-            const head2 = await buildList(scene, second, 5);
-            const actions = buildActions(head1, head2);
+            const head = await buildList(scene, nums, 7);
+            await center(head);
+            const actions = buildActions(head);
             setActions(actions);
         } catch (error) {
             console.error(error);
@@ -78,7 +67,6 @@ const Submit: React.FC<{
             cancelAnimate();
         }
 
-        setIndex(0);
         setState(State.Playing);
     }
 
@@ -91,21 +79,15 @@ const Submit: React.FC<{
 
 const Main = ({ setAnchorEl }: Props) => {
 
-    const [nums1, setNums1] = React.useState("");
-    const [nums2, setNums2] = React.useState("");
+    const maxLength = 8;
+    const [list, setList] = React.useState(() => buildRandomList(maxLength).join(","));
 
-    const handleNums1Change = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNums1(e.currentTarget.value);
-    }
-
-    const handleNums2Change = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setNums2(e.currentTarget.value);
+    const handleListChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setList(e.currentTarget.value);
     }
 
     const handleFresh = () => {
-        const [first, second] = buildTwoArraies();
-        setNums1(() => first.join(","));
-        setNums2(() => second.join(","));
+        setList(() => buildRandomList(maxLength).join(","));
     }
 
     return (
@@ -115,20 +97,38 @@ const Main = ({ setAnchorEl }: Props) => {
             sx={{
                 p: '2px 4px',
                 display: 'flex',
-                width: 300,
+                width: 360,
                 alignItems: "center"
             }}
         >
-            <Stack sx={{ width: "100%", padding: 2 }} direction="column">
-                <TextField fullWidth label='List1, seprate by ","' variant="standard" value={nums1} onChange={handleNums1Change} />
-                <TextField fullWidth label='List2, seprate by ","' variant="standard" value={nums2} onChange={handleNums2Change} />
-                <Stack direction="row" justifyContent="end">
-                    <IconButton type="button" sx={{ p: '10px' }} aria-label="clear" onClick={handleFresh}>
-                        <RefreshIcon />
-                    </IconButton>
-                    <Submit nums1={nums1} nums2={nums2} setAnchorEl={setAnchorEl} />
-                </Stack>
-            </Stack>
+            <IconButton sx={{ p: '10px' }} aria-label="menu">
+                <InputIcon />
+            </IconButton>
+
+            <InputBase
+                sx={{ ml: 1, flex: 1, }}
+                placeholder='list, seprate by ","'
+                value={list}
+                onChange={handleListChange}
+            />
+
+            <IconButton sx={{ p: '10px' }} aria-label="menu" onClick={handleFresh}>
+                <RefreshIcon />
+            </IconButton>
+
+            <IconButton
+                type="button"
+                sx={{ p: '10px' }}
+                aria-label="clear"
+                disabled={!list.length}
+                onClick={() => {
+                    setList("");
+                }}
+            >
+                <ClearIcon />
+            </IconButton>
+
+            <Submit list={list} setAnchorEl={setAnchorEl} />
         </Paper>
     );
 }
