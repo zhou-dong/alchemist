165cc7335fc9c50c37554f7cbfbab0fd745f8864
DONG ZHOU <82224165@qq.com>
2024-01-12 02:39:59 -0800

    Update algo.ts

--- src/games/linked-list/swap-nodes-in-pairs/two/algo.ts
+++ src/games/linked-list/swap-nodes-in-pairs/two/algo.ts
@@ -1,29 +1,48 @@
-// Definition for singly-linked list.
-class ListNode {
-    val: number
-    next: ListNode | null
-    constructor(val?: number, next?: ListNode | null) {
-        this.val = (val === undefined ? 0 : val)
-        this.next = (next === undefined ? null : next)
-    }
+import * as THREE from 'three';
+import { LinkedListNode } from "../../../../data-structures/list/linked-list/node.three";
+import { buildLinkedListNode } from '../styles';
+
+export enum Order {
+    PreOrder, PostOrder
 }
 
-export function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+export enum Action {
+    Return_Head,
+    Define_Temp,
+    Swap
+}
 
-    const dummy = new ListNode();
-    let current = dummy;
+export interface Step {
+    // action: Action;
+    dummy: LinkedListNode<number>;
+    linesToHighlight: number[]
+}
 
-    while (list1 !== null && list2 !== null) {
-        if (list1.val < list2.val) {
-            current.next = list1;
-            list1 = list1.next;
-        } else {
-            current.next = list2;
-            list2 = list2.next;
+export function buildSteps(head: LinkedListNode<number>, scene: THREE.Scene, x: number, y: number): Step[] {
+    const steps: Step[] = [];
+
+    function swapPairs(head: LinkedListNode<number>): LinkedListNode<number> | undefined {
+        const dummy = buildLinkedListNode(scene, -1, "D", { x, y, z: 0 }, { x: x - 0.4, y: y - 0.2, z: 0 });
+        dummy.next = head;
+        dummy.show()
+
+        steps.push({ dummy, linesToHighlight: [2] });
+
+        let current = dummy;
+        while (current.next && current.next.next) {
+            const a = current.next;
+            const b = current.next.next;
+
+            current.next = b;
+            a.next = b.next;
+            b.next = a;
+            current = a;
         }
-        current = current.next;
-    }
 
-    current.next = list1 === null ? list2 : list1;
-    return dummy.next;
-};
+        return dummy.next;
+    };
+
+    swapPairs(head);
+
+    return steps;
+}
