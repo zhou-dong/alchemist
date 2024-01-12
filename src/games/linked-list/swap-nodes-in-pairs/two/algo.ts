c60601994dc3a0d6f1801797a5534651f7686a4d
DONG ZHOU <82224165@qq.com>
2024-01-12 02:52:29 -0800

    Update algo.ts

--- src/games/linked-list/swap-nodes-in-pairs/two/algo.ts
+++ src/games/linked-list/swap-nodes-in-pairs/two/algo.ts
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { LinkedListNode } from "../../../../data-structures/list/linked-list/node.three";
-import { buildLinkedListNode } from '../styles';
+import { buildLinkedListNode, linkColor } from '../styles';
+import { SimpleLink } from '../../../../data-structures/list/link.three';
+import Position from '../../../../data-structures/_commons/params/position.interface';
 
 export enum Order {
     PreOrder, PostOrder
@@ -18,13 +20,27 @@ export interface Step {
     linesToHighlight: number[]
 }
 
+
+
 export function buildSteps(head: LinkedListNode<number>, scene: THREE.Scene, x: number, y: number): Step[] {
     const steps: Step[] = [];
 
     function swapPairs(head: LinkedListNode<number>): LinkedListNode<number> | undefined {
-        const dummy = buildLinkedListNode(scene, -1, "D", { x, y, z: 0 }, { x: x - 0.4, y: y - 0.2, z: 0 });
+        const dummy: LinkedListNode<number> = buildLinkedListNode(scene, -1, "D", { x, y, z: 0 }, { x: x - 0.4, y: y - 0.2, z: 0 });
+        dummy.nodeSkin.color = "lightgray";
         dummy.next = head;
+        const adjustSource = ({ x, y, z }: Position): Position => {
+            const width = dummy.width;
+            return { x: x + width / 2, y, z };
+        }
+        const adjustTarget = ({ x, y, z }: Position): Position => {
+            const width = head.width;
+            return { x: x - width / 2, y, z };
+        }
+        dummy.linkToNext = new SimpleLink(dummy, adjustSource, head, adjustTarget, scene, linkColor);
+
         dummy.show()
+        dummy.linkToNext.show();
 
         steps.push({ dummy, linesToHighlight: [2] });
 
