7a9f2156ac1edefdf30304b50ea901690b9edb74
DONG ZHOU <82224165@qq.com>
2024-01-14 05:22:11 -0800

    Update index.tsx

--- src/games/linked-list/remove-duplicates-from-sorted-list/index.tsx
+++ src/games/linked-list/remove-duplicates-from-sorted-list/index.tsx
@@ -1,6 +1,11 @@
+import { ThemeProvider } from "@mui/material";
+import GameWrapper from "../../commons/GameWrapper";
 import { AlgoContextProvider } from "./AlgoContext";
 import Introduction from "./Introduction";
 import Play from "./Play";
+import info from "./info";
+import theme from '../../dp/_commons/theme';
+import Header from "./Header";
 
 interface Props {
     renderer: THREE.Renderer;
@@ -9,10 +14,15 @@ interface Props {
 }
 
 const Main = ({ renderer, camera, scene }: Props) => (
-    <AlgoContextProvider renderer={renderer} camera={camera} scene={scene}>
-        <Introduction />
-        <Play />
-    </AlgoContextProvider>
+    <GameWrapper path={info.path}>
+        <ThemeProvider theme={theme}>
+            <AlgoContextProvider renderer={renderer} camera={camera} scene={scene}>
+                <Header />
+                <Introduction />
+                <Play />
+            </AlgoContextProvider>
+        </ThemeProvider>
+    </GameWrapper>
 );
 
 export default Main;
