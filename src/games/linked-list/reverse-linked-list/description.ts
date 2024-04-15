28895a95aba94c656babe374d675378cdc83f9c2
DONG ZHOU <82224165@qq.com>
2024-04-14 23:28:00 -0700

    Update description.ts

--- src/games/linked-list/reverse-linked-list/description.ts
+++ src/games/linked-list/reverse-linked-list/description.ts
@@ -1,8 +1,13 @@
-export const title = "Rotate List";
+export const title = "Reverse Linked List";
 
 export const description = `#### Description
 
-Given the head of a linked list, rotate the list to the right by k places.
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+For instance:
+
+- Input: [1, 2, 3, 4, 5]
+- Output: [5, 4, 3, 2, 1]
 `;
 
 export const examples = ``;
