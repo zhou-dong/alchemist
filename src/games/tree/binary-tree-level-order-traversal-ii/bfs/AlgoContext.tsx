4b8c3dcc02b43363dcc49d10a03203ddfe619121
DONG ZHOU <82224165@qq.com>
2024-04-17 23:54:14 -0700

    Update AlgoContext.tsx

--- src/games/tree/binary-tree-level-order-traversal-ii/bfs/AlgoContext.tsx
+++ src/games/tree/binary-tree-level-order-traversal-ii/bfs/AlgoContext.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as THREE from 'three';
-import { clearScene, registerOrbitControls } from '../../../../commons/three';
+import { clearScene } from '../../../../commons/three';
 import Queue from "../../../../data-structures/queue";
 import TreeNode from "../../../../data-structures/tree/nodes/v1/node";
 import { Step } from "./algo";
@@ -69,7 +69,6 @@ export const AlgoContextProvider: React.FC<{
     React.useEffect(() => {
         const init = () => {
             clearScene(scene);
-            registerOrbitControls(camera, renderer, scene);
             renderer.render(scene, camera);
         }
         if (ref && ref.current) {
