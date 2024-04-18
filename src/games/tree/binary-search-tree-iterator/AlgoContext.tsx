fd4971ff5f760b2d25b0539b09319bd579efed78
DONG ZHOU <82224165@qq.com>
2024-04-17 23:53:06 -0700

    Update AlgoContext.tsx

--- src/games/tree/binary-search-tree-iterator/AlgoContext.tsx
+++ src/games/tree/binary-search-tree-iterator/AlgoContext.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import * as THREE from 'three';
-import { clearScene, registerOrbitControls } from '../../../commons/three';
+import { clearScene } from '../../../commons/three';
 import Stack from "../../../data-structures/stack";
 import TreeNode from "../../../data-structures/tree/nodes/v1/node";
 import { State } from "./AlgoState";
@@ -63,7 +63,6 @@ export const AlgoContextProvider: React.FC<{
     React.useEffect(() => {
         const init = () => {
             clearScene(scene);
-            registerOrbitControls(camera, renderer, scene);
             renderer.render(scene, camera);
         }
         if (ref && ref.current) {
