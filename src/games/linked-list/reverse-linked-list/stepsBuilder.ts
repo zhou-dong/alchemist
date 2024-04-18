24fcf6748facf8e5aaaff43728708175f8692eb1
DONG ZHOU <82224165@qq.com>
2024-04-17 21:26:46 -0700

    Update stepsBuilder.ts

--- src/games/linked-list/reverse-linked-list/stepsBuilder.ts
+++ src/games/linked-list/reverse-linked-list/stepsBuilder.ts
@@ -1,5 +1,4 @@
-import { build } from "../_commons/listBuilder";
-import { ListNode } from "../_commons/listNode";
+import { LinkedListNode } from "../../../data-structures/list/linked-list/node.three";
 
 export enum Action {
     return_head,
@@ -12,39 +11,37 @@ export enum Action {
 export class Step {
     action: Action;
     linesToHighlight: number[];
+    head: LinkedListNode<number>;
 
-    constructor(action: Action, linesToHighlight: number[]) {
+    constructor(action: Action, linesToHighlight: number[], head: LinkedListNode<number>) {
         this.action = action;
         this.linesToHighlight = linesToHighlight;
+        this.head = head;
     }
 }
 
-export function buildSteps(nums: number[]): Step[] {
+export function buildSteps(listHead: LinkedListNode<number>): [Step[], LinkedListNode<number>] {
     const steps: Step[] = [];
 
-    function reverseList(head: ListNode<number>): ListNode<number> {
+    function reverseList(head: LinkedListNode<number>): LinkedListNode<number> {
         if (!head.next) {
-            steps.push(new Step(Action.return_head, [2]));
+            steps.push(new Step(Action.return_head, [2], head));
             return head;
         }
 
-        steps.push(new Step(Action.recurse, [3]));
+        steps.push(new Step(Action.recurse, [3], head.next));
         const last = reverseList(head.next);
 
-        steps.push(new Step(Action.reverse, [4]));
+        steps.push(new Step(Action.reverse, [4], head));
         head.next.next = head;
 
-        steps.push(new Step(Action.remove_next, [5]));
+        steps.push(new Step(Action.remove_next, [5], head));
         head.next = undefined;
 
-        steps.push(new Step(Action.return_last, [6]));
+        steps.push(new Step(Action.return_last, [6], head));
         return last;
     };
 
-    const head = build(nums);
-    if (head) {
-        reverseList(head);
-    }
-
-    return steps;
+    const last: LinkedListNode<number> = reverseList(listHead);
+    return [steps, last];
 }
