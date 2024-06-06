3e56dbbe011598a8ac9588334db9a3908fb91d4a
DONG ZHOU <82224165@qq.com>
2024-06-06 00:07:37 -0700

    Update description.ts

--- src/games/linked-list/insertion-sort-list/description.ts
+++ src/games/linked-list/insertion-sort-list/description.ts
@@ -2,13 +2,13 @@ export const title = "Insertion Sort List";
 
 export const description = `#### Description
 
-Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+Given the **head** of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
 
 The steps of the insertion sort algorithm:
 
-Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
-At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
-It repeats until no input elements remain.
+1. Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+2. At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
+3. It repeats until no input elements remain.
 `;
 
 export const examples = ``;
