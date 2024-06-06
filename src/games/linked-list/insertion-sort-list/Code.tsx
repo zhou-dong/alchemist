ec8ffb967067f4c25a019374ffd8dadadc532ad4
DONG ZHOU <82224165@qq.com>
2024-06-06 00:09:02 -0700

    Update Code.tsx

--- src/games/linked-list/insertion-sort-list/Code.tsx
+++ src/games/linked-list/insertion-sort-list/Code.tsx
@@ -1,14 +1,10 @@
 import { styled } from '@mui/system';
 import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import NumbersIcon from '@mui/icons-material/Numbers';
-import { Button, Chip, Divider, IconButton, Paper, Stack, Toolbar, Typography } from "@mui/material";
+import { Divider, IconButton, Paper, Stack, Toolbar, Typography } from "@mui/material";
 import { useAlgoContext } from "./AlgoContext";
 import Draggable from 'react-draggable';
 import CodeBlock, { languages } from '../../dp/_components/CodeBlock';
 import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
-import React from 'react';
 
 const formula = `function insertionSortList(head: ListNode | null): ListNode | null {
     const dummyHead = new ListNode();
@@ -36,21 +32,6 @@ const formula = `function insertionSortList(head: ListNode | null): ListNode | n
 };`
 
 const Head = () => {
-    const { index, steps } = useAlgoContext();
-    const step = steps[index];
-
-    const DisplayLeft: React.FC<{ left: number }> = ({ left }) => (
-        <Button size='small' variant='outlined' startIcon={<ArrowBackIosIcon />} color='success'>left: {left}</Button>
-    );
-
-    const DisplayRight: React.FC<{ right: number }> = ({ right }) => (
-        <Button size='small' variant='outlined' endIcon={<ArrowForwardIosIcon />} color='success'>right: {right}</Button>
-    );
-
-    const DisplayN: React.FC<{ num: number }> = ({ num }) => (
-        <Chip icon={<NumbersIcon />} label={"n: " + num} variant="outlined" color='success' />
-    );
-
     return (
         <Toolbar variant='dense' sx={{ display: "flex" }}>
             <IconButton disabled>
@@ -58,9 +39,6 @@ const Head = () => {
             </IconButton>
             <Stack sx={{ flexGrow: 1, alignItems: "center" }} spacing={2} direction="row">
                 <Typography>Solution</Typography>
-                {step && step.left && <DisplayLeft left={step.left} />}
-                {step && step.right && <DisplayRight right={step.right} />}
-                {step && step.n && <DisplayN num={step.n} />}
             </Stack>
             <IconButton color='info'>
                 <DragIndicatorIcon fontSize='medium' />
