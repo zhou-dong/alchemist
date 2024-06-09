5442800607253e34376dcb5d15b0e6902e76f312
DONG ZHOU <82224165@qq.com>
2024-06-08 23:41:55 -0700

    Update stepsBuilder.ts

--- src/games/linked-list/remove-linked-list-elements/one/stepsBuilder.ts
+++ src/games/linked-list/remove-linked-list-elements/one/stepsBuilder.ts
@@ -3,15 +3,21 @@ import { build } from "../../_commons/listBuilder";
 
 export enum Action {
     return_null,
+    recursive,
     found_next,
     head_next_to_next,
     return_head_next,
     return_head,
 }
 
+export enum Order {
+    PreOrder, PostOrder
+}
+
 const getlinesToHighlight = (action: Action): number[] => {
     switch (action) {
         case Action.return_null: return [3];
+        case Action.recursive: return [5];
         case Action.found_next: return [5];
         case Action.head_next_to_next: return [5];
         case Action.return_head_next: return [6];
@@ -22,10 +28,16 @@ const getlinesToHighlight = (action: Action): number[] => {
 export class Step {
     action: Action;
     linesToHighlight: number[];
+    order: Order;
+    num: number;
+    head?: number;
 
-    constructor(action: Action) {
+    constructor(action: Action, order: Order, num: number, head?: number) {
         this.action = action;
         this.linesToHighlight = getlinesToHighlight(action);
+        this.order = order;
+        this.num = num;
+        this.head = head;
     }
 }
 
@@ -33,22 +45,25 @@ export const buildSteps = (nums: number[], val: number): Step[] => {
     const steps: Step[] = [];
 
     function removeElements(head: ListNode<number> | undefined, val: number): ListNode<number> | undefined {
+
+        steps.push(new Step(Action.recursive, Order.PreOrder, val, head?.val));
+
         if (head === undefined) {
-            steps.push(new Step(Action.return_null));
+            steps.push(new Step(Action.return_null, Order.PostOrder, val, undefined));
             return head;
         }
 
         const next = removeElements(head.next, val);
-        steps.push(new Step(Action.found_next));
+        steps.push(new Step(Action.found_next, Order.PostOrder, val, head.val));
 
         head.next = next;
-        steps.push(new Step(Action.head_next_to_next));
+        steps.push(new Step(Action.head_next_to_next, Order.PostOrder, val, head.val));
 
         if (head.val === val) {
-            steps.push(new Step(Action.return_head_next));
+            steps.push(new Step(Action.return_head_next, Order.PostOrder, val, head.val));
             return head.next;
         } else {
-            steps.push(new Step(Action.return_head));
+            steps.push(new Step(Action.return_head, Order.PostOrder, val, head.val));
             return head;
         }
     };
