8e6352c23ab7777567688ebcb7ce87736be3bfad
DONG ZHOU <82224165@qq.com>
2024-07-09 23:42:59 -0700

    Update stepsBuilder.ts

--- src/games/linked-list/odd-even-linked-list/stepsBuilder.ts
+++ src/games/linked-list/odd-even-linked-list/stepsBuilder.ts
@@ -1,99 +1,105 @@
 import { LinkedListNode } from "../../../data-structures/list/linked-list/node.three";
+import { ListNode } from "../_commons/listNode";
 
 export enum Action {
-    assign_front_pointer,
-    node_null_return_true,
-    get_into_recursively_check,
-    recursively_check_start,
-    recursively_check,
-    recursively_check_false,
-    recursively_non_eq_front_pointer_val,
-    recursively_eq_front_pointer_val,
-    update_front_pointer,
-    recursively_check_return_true,
-    return_final,
+    stand_by,
+    define_even_head,
+    define_even,
+    define_odd,
+    odd_next_to_even_next,
+    odd_to_odd_next,
+    even_next_to_odd_next,
+    even_to_even_next,
+    odd_next_to_even_head,
+    return_head,
 }
 
 const getlinesToHighlight = (action: Action): number[] => {
     switch (action) {
-        case Action.recursively_check_start: return [3];
-        case Action.assign_front_pointer: return [18];
-        case Action.get_into_recursively_check: return [19];
-        case Action.node_null_return_true: return [5];
-        case Action.recursively_check: return [7];
-        case Action.recursively_check_false: return [8]
-        case Action.recursively_non_eq_front_pointer_val: return [11];
-        case Action.update_front_pointer: return [13];
-        case Action.recursively_check_return_true: return [14];
-        case Action.return_final: return [19];
-        case Action.recursively_eq_front_pointer_val: return [12];
+        case Action.stand_by: return [1];
+        case Action.define_even_head: return [5];
+        case Action.define_even: return [6];
+        case Action.define_odd: return [7];
+        case Action.odd_next_to_even_next: return [9];
+        case Action.odd_to_odd_next: return [10];
+        case Action.even_next_to_odd_next: return [11]
+        case Action.even_to_even_next: return [12];
+        case Action.odd_next_to_even_head: return [14];
+        case Action.return_head: return [15];
     }
 }
 
 export class Step {
     action: Action;
     linesToHighlight: number[];
-    head: LinkedListNode<number | string>;
-    frontPointer: LinkedListNode<number | string> | undefined;
-    current: LinkedListNode<number | string> | undefined;
+    evenHead: LinkedListNode<number | string> | undefined;
+    even: LinkedListNode<number | string> | undefined;
+    odd: LinkedListNode<number | string> | undefined;
 
     constructor(
         action: Action,
-        head: LinkedListNode<number | string>,
-        frontPointer: LinkedListNode<number | string> | undefined,
-        current: LinkedListNode<number | string> | undefined
+        evenHead: LinkedListNode<number | string> | undefined,
+        even: LinkedListNode<number | string> | undefined,
+        odd: LinkedListNode<number | string> | undefined,
     ) {
         this.action = action;
         this.linesToHighlight = getlinesToHighlight(action);
-        this.head = head;
-        this.frontPointer = frontPointer;
-        this.current = current;
+        this.odd = odd;
+        this.even = even;
+        this.evenHead = evenHead;
     }
+}
+
+const buildProxyList = (head: LinkedListNode<number | string>): ListNode<LinkedListNode<number | string>> => {
+    const proxyHead = new ListNode<LinkedListNode<number | string>>(head);
+    let previous = proxyHead;
+    let current: LinkedListNode<number | string> | undefined = head.next;
+    while (current) {
+        previous.next = new ListNode<LinkedListNode<number | string>>(current);
+        previous = previous.next;
+        current = current.next;
+    }
+    return proxyHead;
 }
 
 export function buildSteps(head: LinkedListNode<number | string>): Step[] {
 
     const steps: Step[] = [];
 
-    let frontPointer: LinkedListNode<number | string> | undefined;
+    function oddEvenList(head: ListNode<LinkedListNode<number | string>>): ListNode<LinkedListNode<number | string>> {
+        steps.push(new Step(Action.stand_by, undefined, undefined, undefined));
 
-    function recursivelyCheck(node: LinkedListNode<number | string> | undefined): boolean {
-        steps.push(new Step(Action.recursively_check_start, head, frontPointer, node));
-        if (node === undefined) {
-            steps.push(new Step(Action.node_null_return_true, head, frontPointer, node));
-            return true;
+        const evenHead = head.next;
+        steps.push(new Step(Action.define_even_head, evenHead?.val, undefined, undefined));
+
+        let even = evenHead;
+        steps.push(new Step(Action.define_even, evenHead?.val, even?.val, undefined));
+
+        let odd = head;
+        steps.push(new Step(Action.define_odd, evenHead?.val, even?.val, odd.val));
+
+        while (even !== undefined && even.next !== undefined) {
+            odd.next = even.next;
+            steps.push(new Step(Action.odd_next_to_even_next, evenHead?.val, even?.val, odd.val));
+
+            odd = odd.next;
+            steps.push(new Step(Action.odd_to_odd_next, evenHead?.val, even?.val, odd.val));
+
+            even.next = odd.next;
+            steps.push(new Step(Action.even_next_to_odd_next, evenHead?.val, even?.val, odd.val));
+
+            even = even.next;
+            steps.push(new Step(Action.even_to_even_next, evenHead?.val, even?.val, odd.val));
         }
+        odd.next = evenHead;
+        steps.push(new Step(Action.odd_next_to_even_head, evenHead?.val, even?.val, odd.val));
 
-        steps.push(new Step(Action.recursively_check, head, frontPointer, node));
-        if (!recursivelyCheck(node.next)) {
-            steps.push(new Step(Action.recursively_check_false, head, frontPointer, node));
-            return false;
-        }
-
-        if (node.data !== frontPointer?.data) {
-            steps.push(new Step(Action.recursively_non_eq_front_pointer_val, head, frontPointer, node));
-            return false;
-        }
-
-        steps.push(new Step(Action.recursively_eq_front_pointer_val, head, frontPointer, node));
-
-        frontPointer = frontPointer.next;
-        steps.push(new Step(Action.update_front_pointer, head, frontPointer, node));
-        return true;
-    }
-
-    function isPalindrome(head: LinkedListNode<number | string>): boolean {
-        frontPointer = head;
-        steps.push(new Step(Action.assign_front_pointer, head, frontPointer, undefined));
-
-        steps.push(new Step(Action.get_into_recursively_check, head, frontPointer, undefined));
-
-        const result = recursivelyCheck(head);
-        steps.push(new Step(Action.return_final, head, frontPointer, undefined));
-        return result;
+        steps.push(new Step(Action.return_head, evenHead?.val, even?.val, odd.val));
+        return head;
     };
 
-    isPalindrome(head);
+    const proxyHead = buildProxyList(head);
+    oddEvenList(proxyHead);
 
     return steps;
 }
