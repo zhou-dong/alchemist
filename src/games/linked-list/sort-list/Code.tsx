2d45b8f902b0f9286112e070d55e9871d1bf7331
DONG ZHOU <82224165@qq.com>
2024-07-15 01:00:54 -0700

    Update Code.tsx

--- src/games/linked-list/sort-list/Code.tsx
+++ src/games/linked-list/sort-list/Code.tsx
@@ -6,21 +6,55 @@ import Draggable from 'react-draggable';
 import CodeBlock, { languages } from '../../dp/_components/CodeBlock';
 import EmojiObjectsOutlinedIcon from '@mui/icons-material/EmojiObjectsOutlined';
 
-const formula = `function oddEvenList(head: ListNode | null): ListNode | null {
+const formula = `function merge(head1: ListNode | null, head2: ListNode | null) {
+    const dummyHead = new ListNode();
+    let temp = dummyHead;
+    let temp1 = head1;
+    let temp2 = head2;
+    while (temp1 !== null && temp2 !== null) {
+        if (temp1.val <= temp2.val) {
+            temp.next = temp1;
+            temp1 = temp1.next;
+        } else {
+            temp.next = temp2;
+            temp2 = temp2.next;
+        }
+        temp = temp.next;
+    }
+    if (temp1 !== null) {
+        temp.next = temp1;
+    } else if (temp2 !== null) {
+        temp.next = temp2;
+    }
+    return dummyHead.next;
+}
+
+function sort(head: ListNode | null, tail: ListNode | null) {
     if (head === null) {
-        return null;
+        return head;
     }
-    let odd = head;
-    const evenHead = head.next;
-    let even = evenHead;
-    while (even !== null && even.next !== null) {
-        odd.next = even.next;
-        odd = odd.next;
-        even.next = odd.next;
-        even = even.next;
+    if (head.next == tail) {
+        head.next = null;
+        return head;
     }
-    odd.next = evenHead;
-    return head;
+    let slow = head;
+    let fast = head;
+    while (fast !== tail) {
+        slow = slow.next;
+        fast = fast.next;
+        if (fast !== tail) {
+            fast = fast.next;
+        }
+    }
+    let mid = slow;
+    const list1 = sort(head, mid);
+    const list2 = sort(mid, tail);
+    const sorted = merge(list1, list2);
+    return sorted;
+}
+
+function sortList(head: ListNode | null): ListNode | null {
+    return sort(head, null);
 };`
 
 const Code = () => {
