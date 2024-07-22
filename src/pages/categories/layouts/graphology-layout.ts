10cd71b3cc3d218422df8fe90486c180ef408a3a
DONG ZHOU <82224165@qq.com>
2024-07-22 03:27:27 -0700

    Create graphology-layout.ts

---
+++ src/pages/categories/layouts/graphology-layout.ts
@@ -0,0 +1,57 @@
+import Graphology from "graphology";
+import forceAtlas2 from 'graphology-layout-forceatlas2';
+import circular from 'graphology-layout/circular';
+import random from 'graphology-layout/random';
+import { CategoryType } from "./category";
+
+export function getCircleLayout(categories: string[]) {
+
+    const graphology = new Graphology();
+
+    categories.forEach(category => {
+        graphology.addNode(category);
+    });
+
+    return circular(graphology, { scale: 350 });
+};
+
+
+export function getRandomLayout(categories: string[]) {
+
+    const graphology = new Graphology();
+
+    categories.forEach(category => {
+        graphology.addNode(category);
+    });
+
+    return random(graphology, { scale: 600 });
+};
+
+
+export function getForceAtlas2Layout(nodes: CategoryType[], edges: CategoryType[][]) {
+
+    const graphology = new Graphology();
+
+    nodes.forEach(node => {
+        graphology.addNode(node);
+    });
+
+    edges.forEach(edge => {
+        const [a, b] = edge;
+        if (!graphology.hasEdge(a, b)) {
+            graphology.addEdge(a, b);
+        }
+    })
+
+    graphology.forEachNode((_, attributes) => {
+        attributes.x = Math.random() * 10; // Set initial x position
+        attributes.y = Math.random() * 10; // Set initial y position
+    });
+
+    const sensibleSettings = forceAtlas2.inferSettings(graphology);
+
+    return forceAtlas2(graphology, {
+        iterations: 50,
+        settings: sensibleSettings,
+    });
+};
