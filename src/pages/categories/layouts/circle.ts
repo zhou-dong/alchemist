d4b0954006d98423cad280340ed4fd4858946934
DONG ZHOU <82224165@qq.com>
2024-07-28 02:15:59 -0700

    Update circle.ts

--- src/pages/categories/layouts/circle.ts
+++ src/pages/categories/layouts/circle.ts
@@ -1,3 +1,5 @@
+import { Category } from "./category";
+
 export interface Circle {
     x: number;
     y: number;
@@ -16,4 +18,35 @@ export function isInsideCircle(x: number, y: number, circle: Circle): boolean {
     const dy = y - circle.y;
     const distance = Math.sqrt(dx * dx + dy * dy);
     return distance <= circle.radius;
+}
+
+export const drawCircle = (context: CanvasRenderingContext2D, circle: Circle, category: Category) => {
+
+    const { x, y, radius } = circle;
+
+    const { emoji, categoryType } = category;
+    const backgroundColor = "#fff";
+
+    context.shadowColor = 'rgba(0, 0, 0, 0.5)';
+    context.shadowBlur = 10;
+    context.shadowOffsetX = 5;
+    context.shadowOffsetY = 5;
+
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = backgroundColor;
+    context.fill();
+
+    context.shadowColor = '#fff';
+    context.shadowBlur = 0;
+    context.shadowOffsetX = 0;
+    context.shadowOffsetY = 0;
+
+    context.font = '300 20px "Roboto"';
+    context.fillStyle = "#000";
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+
+    context.fillText(emoji, x, y - radius / 4);
+    context.fillText(categoryType, x, y + radius / 5); // TODO
 }
