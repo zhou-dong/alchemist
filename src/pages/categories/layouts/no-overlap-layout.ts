84099ffbce9e92f498fd4ad82dad7b6fec4f17dd
DONG ZHOU <82224165@qq.com>
2024-07-27 23:19:51 -0700

    Update no-overlap-layout.ts

--- src/pages/categories/layouts/no-overlap-layout.ts
+++ src/pages/categories/layouts/no-overlap-layout.ts
@@ -1,11 +1,4 @@
-import { Circle } from "./circle";
-
-function isOverlap(a: Circle, b: Circle): boolean {
-    const dx = a.x - b.x;
-    const dy = a.y - b.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-    return distance < (a.radius + b.radius);
-}
+import { Circle, isOverlap } from "./circle";
 
 function isOverlapWithOthers(circle: Circle, others: Circle[]): boolean {
     for (let other of others) {
@@ -16,7 +9,7 @@ function isOverlapWithOthers(circle: Circle, others: Circle[]): boolean {
     return false;
 }
 
-export function getCircles(
+export function getNoOverlapCircles(
     canvasWidth: number,
     canvasHeight: number,
     radiusList: number[],
