4c7d83d2372a93d65af857126cb224d630ff6666
DONG ZHOU <82224165@qq.com>
2024-08-03 01:50:37 -0700

    Update Menu.tsx

--- src/pages/categories/Menu.tsx
+++ src/pages/categories/Menu.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { IconButton } from "@mui/material";
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import { green } from '@mui/material/colors';
+import GridViewIcon from '@mui/icons-material/GridView';
+import { grey } from '@mui/material/colors';
 
 interface Props {
     open: boolean;
@@ -12,15 +12,20 @@ const MenuButton = ({ open, setOpen }: Props) => (
     <IconButton
         size="medium"
         sx={{
-            ...(open && { display: 'none' }),
-            backgroundColor: green[600],
-            "&:hover": {
-                backgroundColor: green[800],
-            }
+            // ...(open && { display: 'none' }),
+            border: "1px solid",
+            borderColor: grey[300],
+            margin: "10px 0",
+            float: "right",
+            // backgroundColor: green[600],
+            // "&:hover": {
+            //     backgroundColor: green[800],
+            // }
         }}
         onClick={() => setOpen(true)}
+        disabled={open}
     >
-        <MenuRoundedIcon sx={{ color: "#FFF" }} />
+        <GridViewIcon />
     </IconButton>
 );
 
