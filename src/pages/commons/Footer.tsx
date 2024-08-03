994b41897184817ba699378aa969d47b630610d6
DONG ZHOU <82224165@qq.com>
2024-08-03 01:03:51 -0700

    Update Footer.tsx

--- src/pages/commons/Footer.tsx
+++ src/pages/commons/Footer.tsx
@@ -2,14 +2,12 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
-import Chip from '@mui/material/Chip';
-import { Grid } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
 import { grey } from '@mui/material/colors';
 
 function Copyright() {
     return (
         <Typography
-            variant="body2"
             color="text.secondary"
             sx={{
                 color: "#000",
@@ -18,12 +16,6 @@ function Copyright() {
         >
             © Alchemist {new Date().getFullYear()}. All rights reserved.
         </Typography>
-    );
-}
-
-function Mail() {
-    return (
-        <Chip icon={<MailOutlineIcon />} label="alchemist.dong@gmail.com" variant="outlined" sx={{ border: "none", color: "#000" }} />
     );
 }
 
@@ -40,11 +32,16 @@ export default function Footer() {
         >
             <Container maxWidth="sm">
                 <Grid container>
-                    <Grid item xs={12} sm={6}>
+                    <Grid item xs={12} sm={6} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
                         <Copyright />
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                        <Mail />
+                    <Grid item xs={12} sm={6} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                        <Stack direction="row">
+                            <MailOutlineIcon sx={{ marginRight: "2px" }} />
+                            <Typography>
+                                alchemist.dong@gmail.com
+                            </Typography>
+                        </Stack>
                     </Grid>
                 </Grid>
             </Container>
