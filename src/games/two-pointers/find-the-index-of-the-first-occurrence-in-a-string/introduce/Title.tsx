28d6ac7431c26e4c86a4920019f3db842210db05
DONG ZHOU <82224165@qq.com>
2024-10-28 02:35:42 -0700

    Update Title.tsx

--- src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/introduce/Title.tsx
+++ src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/introduce/Title.tsx
@@ -2,54 +2,51 @@ import { Stack, Typography } from "@mui/material";
 import StarIcon from '@mui/icons-material/Star';
 import { title } from "../contents";
 
+const TitleContent = () => (
+    <Typography
+        variant="h5"
+        component="h1"
+        sx={{
+            fontWeight: 400,
+            textAlign: "center",
+        }}
+        color="primary"
+    >
+        {title}
+    </Typography>
+);
+
+const Start = () => (
+    <StarIcon
+        fontSize="large"
+        sx={{
+            color: "gold",
+            animation: 'rotate 2.5s linear infinite',
+            '@keyframes rotate': {
+                '0%': {
+                    transform: 'rotate(0deg)',
+                },
+                '100%': {
+                    transform: 'rotate(360deg)',
+                },
+            },
+        }}
+    />
+);
+
 const Main = () => (
-    <div
-        style={{
+    <Stack
+        direction="row"
+        spacing={1}
+        sx={{
             display: "flex",
-            alignItems: "center",
             justifyContent: "center",
+            alignItems: "center",
         }}
     >
-        <Stack
-            direction="row"
-            spacing={1}
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "10px 20px",
-                borderRadius: "40px"
-            }}
-        >
-            <StarIcon
-                fontSize="large"
-                sx={{
-                    color: "gold",
-                    animation: 'rotate 2.5s linear infinite',
-                    '@keyframes rotate': {
-                        '0%': {
-                            transform: 'rotate(0deg)',
-                        },
-                        '100%': {
-                            transform: 'rotate(360deg)',
-                        },
-                    },
-                }}
-            />
-
-            <Typography
-                variant="h5"
-                component="h1"
-                sx={{
-                    fontWeight: 400,
-                    textAlign: "center",
-                }}
-                color="primary"
-            >
-                {title}
-            </Typography>
-        </Stack>
-    </div>
+        <Start />
+        <TitleContent />
+    </Stack>
 );
 
 export default Main;
