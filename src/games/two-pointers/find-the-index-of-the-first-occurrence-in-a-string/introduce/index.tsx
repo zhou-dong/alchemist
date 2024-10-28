8f30534fadc9708d09051227fff092f9e651262e
DONG ZHOU <82224165@qq.com>
2024-10-28 04:44:14 -0700

    Update index.tsx

--- src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/introduce/index.tsx
+++ src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/introduce/index.tsx
@@ -1,22 +1,35 @@
+import React from 'react';
 import Sections from "./Sections";
 import Title from "./Title";
 import { Container, Divider } from "@mui/material";
+import Welcome from "./Welcome";
+import { statments } from './Statements';
 
-const Main = () => (
-    <Container
-        maxWidth="lg"
-        sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            flexDirection: "column",
-            height: "100vh"
-        }}
-    >
-        <Title />
-        <Divider variant="middle" />
-        <Sections />
-    </Container>
-);
+const Main = () => {
+
+    const [statmentIndex, setStatementIndex] = React.useState(-1);
+
+    return (
+        <Container
+            maxWidth="lg"
+            sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+                height: "100vh"
+            }}
+        >
+            <Title />
+            <Welcome />
+            <Divider variant="middle" />
+            <Sections
+                statmentIndex={statmentIndex}
+                setStatementIndex={setStatementIndex}
+                statements={statments}
+            />
+        </Container>
+    );
+};
 
 export default Main;
