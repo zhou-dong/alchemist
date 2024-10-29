16551a7cfd5925a5290ee25b09e016751fb747f9
DONG ZHOU <82224165@qq.com>
2024-10-29 00:41:09 -0700

    Update index.tsx

--- src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/index.tsx
+++ src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/index.tsx
@@ -2,22 +2,35 @@ import GameWrapper from '../../commons/GameWrapper';
 import info from "./info";
 import { ThemeProvider } from '@mui/material';
 import theme from '../../dp/_commons/theme';
-import { AlgoContextProvider } from "./AlgoContext";
+import { AlgoContextProvider, useAlgoContext } from "./AlgoContext";
 import Introduction from "./Introduction";
 import Play from './Play';
 import Introduce from './introduce';
 
-const Main = () => (
-    <GameWrapper path={info.path}>
-        <ThemeProvider theme={theme}>
-            <AlgoContextProvider>
-                <Introduce />
-                {/* <Header /> */}
-                {/* <Introduction /> */}
-                {/* <Play /> */}
-            </AlgoContextProvider>
-        </ThemeProvider>
-    </GameWrapper>
-);
+const Game = () => {
+    const { displayIntroduce } = useAlgoContext();
 
+    return (
+        <>
+            {/* <Header />  */}
+            {displayIntroduce && <Introduce />}
+            {!displayIntroduce && <Introduction />}
+            {!displayIntroduce && <Play />}
+        </>
+    );
+}
+
+const Main = () => {
+
+
+    return (
+        <GameWrapper path={info.path}>
+            <ThemeProvider theme={theme}>
+                <AlgoContextProvider>
+                    <Game />
+                </AlgoContextProvider>
+            </ThemeProvider>
+        </GameWrapper>
+    );
+}
 export default Main;
