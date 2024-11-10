a18059769cddf253a4033f6a1a26e488a58bc28f
DONG ZHOU <82224165@qq.com>
2024-11-10 00:00:41 -0800

    Update index.tsx

--- src/games/two-pointers/longest-common-prefix/input/index.tsx
+++ src/games/two-pointers/longest-common-prefix/input/index.tsx
@@ -1,10 +1,31 @@
+import { Stack } from "@mui/material";
+import Title from "../description/Title";
+import { useAlgoContext } from "../AlgoContext";
+import { State } from "../AlgoState";
 
 
 
 
-const Main = () => (
-    <>
-    </>
-);
+const Main = () => {
+    const { setState } = useAlgoContext();
+
+    const handleGoBack = () => {
+        setState(State.Description);
+    }
+
+    return (
+        <>
+            input
+
+            <Stack>
+
+                <Title displayStar={true} />
+
+
+
+            </Stack>
+        </>
+    );
+}
 
 export default Main;
