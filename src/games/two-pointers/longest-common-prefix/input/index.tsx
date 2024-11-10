0607d4b0d22b633ffddac380971fd6eb028710b6
DONG ZHOU <82224165@qq.com>
2024-11-10 01:29:06 -0800

    Update index.tsx

--- src/games/two-pointers/longest-common-prefix/input/index.tsx
+++ src/games/two-pointers/longest-common-prefix/input/index.tsx
@@ -37,7 +37,7 @@ const Main = () => {
                 }}
             >
 
-                <Title displayStar={true} />
+                <Title displayStar={false} />
 
                 <GameInput />
 
