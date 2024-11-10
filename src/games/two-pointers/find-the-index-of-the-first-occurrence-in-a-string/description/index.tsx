f4d04107d1e0bfc47cf170051455133e0e449496
DONG ZHOU <82224165@qq.com>
2024-11-10 11:09:40 -0800

    Update index.tsx

--- src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/description/index.tsx
+++ src/games/two-pointers/find-the-index-of-the-first-occurrence-in-a-string/description/index.tsx
@@ -6,12 +6,11 @@ import { contents, DisplayContents } from './Contents';
 import InputIcon from '@mui/icons-material/Input';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import SportsEsportsOutlinedIcon from '@mui/icons-material/SportsEsportsOutlined';
+import LastPageIcon from '@mui/icons-material/LastPage';
 import { green } from '@mui/material/colors';
 import { useAlgoContext } from '../AlgoContext';
-import testCases from "../toolbox/test-cases.json";
-import { buildSteps, createTable, createTableStyle } from '../game/algo';
 import { State } from '../AlgoState';
+import Toolbox from '../toolbox';
 
 const Navigator = styled(IconButton)({
     width: 60,
@@ -29,8 +28,7 @@ const Navigator = styled(IconButton)({
 
 const Main = () => {
 
-    const { table, setTable, setTableStyle, setIndex, setState, setSteps, setHaystack, setNeedle } = useAlgoContext();
-
+    const { setState } = useAlgoContext();
 
     const [statmentIndex, setStatementIndex] = React.useState(-1);
 
@@ -46,59 +44,74 @@ const Main = () => {
         }
     };
 
+    const handleLastPageClick = () => {
+        setStatementIndex(contents.length - 1);
+    }
+
     const handleInputClick = () => {
         setState(State.Input);
     };
 
     return (
-        <Container
-            maxWidth="lg"
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                height: "100vh",
-            }}
-        >
-            <Title displayStar={statmentIndex >= 0} />
-            {statmentIndex < 0 && <Welcome />}
-
-            <Divider variant="middle" />
-            <DisplayContents contentIndex={statmentIndex} contents={contents} />
-
-            <Stack
-                direction="row"
-                spacing={3}
-                textAlign="center"
-                mt={4}
+        <>
+            <Toolbox />
+            <Container
+                maxWidth="lg"
                 sx={{
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
+                    flexDirection: "column",
+                    height: "100vh",
                 }}
             >
-                <Navigator
-                    onClick={handlePreviousSection}
-                    disabled={statmentIndex < 0}
-                >
-                    <NavigateBeforeIcon fontSize='large' />
-                </Navigator>
+                <Title displayStar={statmentIndex >= 0} />
+                {statmentIndex < 0 && <Welcome />}
 
-                <Navigator
-                    onClick={handleNextSection}
-                    disabled={statmentIndex === contents.length - 1}
-                >
-                    <NavigateNextIcon fontSize='large' />
-                </Navigator>
+                <Divider variant="middle" />
+                <DisplayContents contentIndex={statmentIndex} contents={contents} />
 
-                <Navigator
-                    onClick={handleInputClick}
+                <Stack
+                    direction="row"
+                    spacing={3}
+                    textAlign="center"
+                    mt={4}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
                 >
-                    <InputIcon />
-                </Navigator>
-            </Stack>
-        </Container>
+                    <Navigator
+                        onClick={handlePreviousSection}
+                        disabled={statmentIndex < 0}
+                    >
+                        <NavigateBeforeIcon fontSize='large' />
+                    </Navigator>
+
+                    <Navigator
+                        onClick={handleNextSection}
+                        disabled={statmentIndex === contents.length - 1}
+                    >
+                        <NavigateNextIcon fontSize='large' />
+                    </Navigator>
+
+                    <Navigator
+                        onClick={handleLastPageClick}
+                        disabled={statmentIndex === contents.length - 1}
+                    >
+                        <LastPageIcon fontSize='large' />
+                    </Navigator>
+
+
+                    <Navigator
+                        onClick={handleInputClick}
+                    >
+                        <InputIcon />
+                    </Navigator>
+                </Stack>
+            </Container>
+        </>
     );
 };
 
