fc750a6dd1a624fabeb8dac2d3f119474807c204
DONG ZHOU <82224165@qq.com>
2024-11-10 20:20:01 -0800

    Update index.tsx

--- src/games/two-pointers/longest-common-prefix/description/index.tsx
+++ src/games/two-pointers/longest-common-prefix/description/index.tsx
@@ -2,32 +2,45 @@ import React from 'react';
 import Title from "./Title";
 import { Container, Divider, IconButton, Stack, styled } from "@mui/material";
 import { contents, DisplayContents } from './Contents';
+import InputIcon from '@mui/icons-material/Input';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import InputIcon from '@mui/icons-material/Input';
-import { green } from '@mui/material/colors';
+import FirstPageIcon from '@mui/icons-material/FirstPage';
+import LastPageIcon from '@mui/icons-material/LastPage';
+import SentimentSatisfiedOutlinedIcon from '@mui/icons-material/SentimentSatisfiedOutlined';
 import { useAlgoContext } from '../AlgoContext';
 import { State } from '../AlgoState';
+import Toolbox from '../toolbox';
 
-const Navigator = styled(IconButton)({
+const Navigator = styled(IconButton)(({ theme }) => ({
     width: 60,
     height: 60,
-    backgroundColor: green[500],
-    color: "#fff",
+    border: "1px solid lightgray",
+    color: theme.palette.primary.main,
     '&:hover': {
-        backgroundColor: green[600],
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+        color: "#fff",
     },
     '&.Mui-disabled': {
         backgroundColor: 'lightgray',
         color: 'gray',
     },
-});
+}));
 
 const Main = () => {
 
     const { setState } = useAlgoContext();
 
     const [statmentIndex, setStatementIndex] = React.useState(-1);
+
+    React.useEffect(() => {
+        setStatementIndex(0);
+    }, []);
+
+    const handleFirstPageClick = () => {
+        setStatementIndex(-1);
+    }
 
     const handlePreviousSection = () => {
         if (statmentIndex >= 0) {
@@ -41,58 +54,84 @@ const Main = () => {
         }
     };
 
-    const handleDisplayInput = () => {
+    const handleLastPageClick = () => {
+        setStatementIndex(contents.length - 1);
+    };
+
+    const handleInputClick = () => {
         setState(State.Input);
-    }
+    };
 
     return (
-        <Container
-            maxWidth="lg"
-            sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                flexDirection: "column",
-                height: "100vh",
-            }}
-        >
-            <Title displayStar={statmentIndex >= 0} />
-
-            <Divider variant="middle" />
-            <DisplayContents contentIndex={statmentIndex} contents={contents} />
-
-            <Stack
-                direction="row"
-                spacing={3}
-                textAlign="center"
-                mt={4}
+        <>
+            <Toolbox current={State.Description} />
+            <Container
+                maxWidth="lg"
                 sx={{
                     display: "flex",
                     justifyContent: "center",
                     alignItems: "center",
+                    flexDirection: "column",
+                    height: "100vh",
                 }}
             >
-                <Navigator
-                    onClick={handlePreviousSection}
-                    disabled={statmentIndex < 0}
-                >
-                    <NavigateBeforeIcon fontSize='large' />
-                </Navigator>
+                <Title displayStar={statmentIndex >= 0} />
 
-                <Navigator
-                    onClick={handleNextSection}
-                    disabled={statmentIndex === contents.length - 1}
-                >
-                    <NavigateNextIcon fontSize='large' />
-                </Navigator>
+                <Divider variant="middle" />
+                <DisplayContents contentIndex={statmentIndex} contents={contents} />
 
-                <Navigator
-                    onClick={handleDisplayInput}
+                <Stack
+                    direction="row"
+                    spacing={3}
+                    textAlign="center"
+                    mt={4}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
                 >
-                    <InputIcon />
-                </Navigator>
-            </Stack>
-        </Container>
+                    <Navigator
+                        onClick={() => setState(State.Welcome)}
+                    >
+                        <SentimentSatisfiedOutlinedIcon fontSize='medium' />
+                    </Navigator>
+
+                    <Navigator
+                        onClick={handleFirstPageClick}
+                        disabled={statmentIndex < 0}
+                    >
+                        <FirstPageIcon fontSize='large' />
+                    </Navigator>
+                    <Navigator
+                        onClick={handlePreviousSection}
+                        disabled={statmentIndex < 0}
+                    >
+                        <NavigateBeforeIcon fontSize='large' />
+                    </Navigator>
+
+                    <Navigator
+                        onClick={handleNextSection}
+                        disabled={statmentIndex === contents.length - 1}
+                    >
+                        <NavigateNextIcon fontSize='large' />
+                    </Navigator>
+
+                    <Navigator
+                        onClick={handleLastPageClick}
+                        disabled={statmentIndex === contents.length - 1}
+                    >
+                        <LastPageIcon fontSize='large' />
+                    </Navigator>
+
+                    <Navigator
+                        onClick={handleInputClick}
+                    >
+                        <InputIcon fontSize='medium' />
+                    </Navigator>
+                </Stack>
+            </Container>
+        </>
     );
 };
 
