30744d45aa5a4d433fdf321f928712aa8a43dc6a
DONG ZHOU <82224165@qq.com>
2024-11-11 02:54:25 -0800

    Update GameInput.tsx

--- src/games/two-pointers/longest-common-prefix/input/GameInput.tsx
+++ src/games/two-pointers/longest-common-prefix/input/GameInput.tsx
@@ -1,27 +1,45 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import FormLabel from '@mui/material/FormLabel';
 import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
 import MuiCard from '@mui/material/Card';
 import { styled } from '@mui/material/styles';
 
 import AutoFixHighIcon from '@mui/icons-material/AutoFixHigh';
 
-import OutputIcon from '@mui/icons-material/Output';
-import { BottomNavigation, BottomNavigationAction, ButtonGroup, Container, Paper } from '@mui/material';
+import { BottomNavigation, BottomNavigationAction, Divider, IconButton, InputBase, List, ListItem, Paper, Stack, Typography } from '@mui/material';
 import { useAlgoContext } from '../AlgoContext';
 
 import testCases from "./test-cases.json";
-import { buildSteps, createTable, createTableStyle } from '../game/algo';
 import { State } from '../AlgoState';
 
 import LooksOneOutlinedIcon from '@mui/icons-material/LooksOneOutlined';
 import LooksTwoOutlinedIcon from '@mui/icons-material/LooksTwoOutlined';
 import Looks3OutlinedIcon from '@mui/icons-material/Looks3Outlined';
 import Looks4OutlinedIcon from '@mui/icons-material/Looks4Outlined';
-import { Solution } from "../game/solution";
+
+import OutputIcon from '@mui/icons-material/Output';
+import ClearIcon from '@mui/icons-material/Clear';
+import InputIcon from '@mui/icons-material/Input';
+import DeleteIcon from '@mui/icons-material/Delete';
+import UploadIcon from '@mui/icons-material/Upload';
+import { Solution } from '../game/solution';
+
+const StyledButton = styled(IconButton)(({ theme }) => ({
+    width: 60,
+    height: 60,
+    border: "1px solid lightgray",
+    color: theme.palette.primary.main,
+    '&:hover': {
+        backgroundColor: theme.palette.primary.main,
+        borderColor: theme.palette.primary.main,
+        color: "#fff",
+    },
+    '&.Mui-disabled': {
+        backgroundColor: 'lightgray',
+        color: 'gray',
+    },
+}));
 
 const getRandomTestCase = () => {
     const max = testCases.length;
@@ -34,158 +52,208 @@ const Card = styled(MuiCard)(({ theme }) => ({
     border: 0,
 }));
 
-export default function Main() {
+const SelectSolution: React.FC<{
+    localSolution: Solution | undefined,
+    setLocalSolution: React.Dispatch<React.SetStateAction<Solution | undefined>>,
+}> = ({
+    localSolution,
+    setLocalSolution,
+}) => {
+        const handleChange = (event: React.SyntheticEvent, newValue: any) => {
+            if (Object.values(Solution).includes(newValue as Solution)) {
+                const newSolution = newValue as Solution;
+                setLocalSolution(newSolution);
+            }
+        };
+        return (
+            <FormControl>
+                <FormLabel htmlFor="needle">Solution</FormLabel>
+                <Paper variant='outlined' sx={{ padding: "15px 0" }}>
+                    <BottomNavigation
+                        showLabels
+                        value={localSolution}
+                        onChange={handleChange}
+                    >
+                        <BottomNavigationAction
+                            label={Solution.HorizontalScanning}
+                            value={Solution.HorizontalScanning}
+                            icon={<LooksOneOutlinedIcon />}
+                        />
+                        <BottomNavigationAction
+                            label={Solution.VerticalScanning}
+                            value={Solution.VerticalScanning}
+                            icon={<LooksTwoOutlinedIcon />}
+                        />
+                        <BottomNavigationAction
+                            label={Solution.DivideAndConquer}
+                            value={Solution.DivideAndConquer}
+                            icon={<Looks3OutlinedIcon />}
+                        />
+                        <BottomNavigationAction
+                            label={Solution.BinarySearch}
+                            value={Solution.BinarySearch}
+                            icon={<Looks4OutlinedIcon />}
+                        />
+                    </BottomNavigation>
+                </Paper>
+            </FormControl>
+        );
+    }
 
-    const [localHaystack, setLocalHaystack] = React.useState("");
-    const [localNeedle, setLocalNeedle] = React.useState("");
-    const { setHaystack, setNeedle, setDisplayGame, setDisplayInput, setTable, setTableStyle, setIndex, setSteps, setState } = useAlgoContext();
+const AddString: React.FC<{ setLocalStrings: React.Dispatch<React.SetStateAction<string[]>> }> = ({
+    setLocalStrings
+}) => {
 
-    const [haystackError, setHaystackError] = React.useState(false);
-    const [haystackErrorMessage, setHaystackErrorMessage] = React.useState('');
+    const [value, setValue] = React.useState<string>("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.currentTarget.value);
+    }
 
     const handleSubmit = () => {
-        if (localHaystack.trim().length === 0) {
-            setHaystackError(true)
-            setHaystackErrorMessage("Haystack can not be empty");
-            return;
-        }
-        if (localNeedle.length > localHaystack.length) {
-            setHaystackError(true)
-            setHaystackErrorMessage("Haystack should longer than needle");
-        }
-
-        const table = createTable(localHaystack, localNeedle);
-        const tableStyle = createTableStyle(localHaystack, localNeedle);
-        const steps = buildSteps(localHaystack, localNeedle);
-
-        setTable(table);
-        setTableStyle(tableStyle);
-        setIndex(0);
-        setSteps(steps);
-        setState(State.Playing);
-
-        setHaystack(localHaystack);
-        setNeedle(localNeedle);
-
-        handleClear();
-        setDisplayInput(false);
-        setDisplayGame(true);
-    };
-
-    const handleHaystackChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (haystackError) {
-            setHaystackError(false);
-            setHaystackErrorMessage("");
-        }
-        setLocalHaystack(e.currentTarget.value);
-    };
+        setLocalStrings(current => [...current, value]);
+        setValue("");
+    }
 
     const handleAutoFill = () => {
-        handleClear();
         const { input } = getRandomTestCase();
-        const { haystack, needle } = input;
-
-        setLocalHaystack(haystack)
-        setLocalNeedle(needle);
+        setLocalStrings(input);
     }
-
-    const handleClear = () => {
-        setLocalHaystack("");
-        setHaystackError(false);
-        setHaystackErrorMessage("");
-        setLocalNeedle("");
-    }
-
-    const [value, setValue] = React.useState('recents');
-
-    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
-        setValue(newValue);
-    };
 
     return (
-        <Container>
-            <Paper elevation={4}>
-                <Card variant="outlined">
-                    <Box
-                        component="form"
-                        onSubmit={handleSubmit}
-                        noValidate
+        <Paper
+            variant="outlined"
+            sx={{
+                p: '2px 4px',
+                display: 'flex',
+                alignItems: "center",
+            }}
+        >
+            <InputIcon sx={{ color: "gray" }} />
+
+            <InputBase
+                sx={{ ml: 1, flex: 1, }}
+                placeholder='input string'
+                value={value}
+                onChange={handleChange}
+            />
+
+            <Divider sx={{ height: 28, m: 0.5, marginRight: 2 }} orientation="vertical" />
+
+            <IconButton
+                type="button"
+                sx={{ p: '10px' }}
+                aria-label="clear"
+                disabled={!value.length}
+                onClick={() => setValue("")}
+            >
+                <ClearIcon />
+            </IconButton>
+
+            <IconButton
+                sx={{ p: '10px' }}
+                aria-label="add string"
+                disabled={!value.length}
+                onClick={handleSubmit}
+            >
+                <UploadIcon />
+            </IconButton>
+
+            <Divider sx={{ height: 28, m: 0.5, marginRight: 2 }} orientation="vertical" />
+
+            <IconButton
+                onClick={handleAutoFill}
+                color='primary'
+            >
+                <AutoFixHighIcon />
+            </IconButton>
+        </Paper>
+    );
+};
+
+export default function Main() {
+
+    const [localSolution, setLocalSolution] = React.useState<Solution>();
+    const [localStrings, setLocalStrings] = React.useState<string[]>([]);
+
+    const { setState } = useAlgoContext();
+
+    const handleDeleteItem = (i: number) => {
+        localStrings.splice(i, 1);
+        setLocalStrings([...localStrings]);
+    }
+
+    const handleSubmit = () => {
+
+        setState(State.Playing);
+        handleClear();
+    };
+
+    const handleClear = () => {
+        setLocalSolution(undefined);
+        setLocalStrings([]);
+    }
+
+    return (
+        <Paper elevation={4}>
+            <Card variant="outlined">
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <FormLabel htmlFor="needle">String</FormLabel>
+                        <Paper variant='outlined' sx={{ padding: "10px" }}>
+                            <List dense={false}>
+                                {
+                                    localStrings.map((localString, i) =>
+                                        <ListItem
+                                            key={i}
+                                            secondaryAction={
+                                                <IconButton
+                                                    edge="end"
+                                                    aria-label="delete"
+                                                    onClick={() => handleDeleteItem(i)}
+                                                >
+                                                    <DeleteIcon />
+                                                </IconButton>
+                                            }
+                                        >
+                                            {localString}
+                                        </ListItem>
+                                    )
+                                }
+                            </List>
+                            <AddString setLocalStrings={setLocalStrings} />
+                        </Paper>
+                    </FormControl>
+
+                    <SelectSolution localSolution={localSolution} setLocalSolution={setLocalSolution} />
+
+                    <Stack
+                        direction="row"
+                        spacing={2}
                         sx={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            width: '100%',
-                            gap: 2,
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
                         }}
                     >
-                        <FormControl>
-                            <FormLabel htmlFor="haystack">String</FormLabel>
-                            <TextField
-                                onChange={handleHaystackChange}
-                                error={haystackError}
-                                helperText={haystackErrorMessage}
-                                type="s"
-                                name="string"
-                                placeholder="input string"
-                                value={localHaystack}
-                                autoFocus
-                                required
-                                fullWidth
-                                color={haystackError ? 'error' : 'primary'}
-                            />
-                        </FormControl>
-
-                        <FormControl>
-                            <FormLabel htmlFor="needle">Solution</FormLabel>
-                            <BottomNavigation
-                                showLabels
-                                sx={{}}
-                                value={value}
-                                onChange={handleChange}
-                            >
-                                <BottomNavigationAction
-                                    label="Horizontal Scanning"
-                                    value={Solution.HorizontalScanning}
-                                    icon={<LooksOneOutlinedIcon />}
-                                />
-                                <BottomNavigationAction
-                                    label="Vertical Scanning "
-                                    value={Solution.VerticalScanning}
-                                    icon={<LooksTwoOutlinedIcon />}
-                                />
-                                <BottomNavigationAction
-                                    label="Divide and Conquer"
-                                    value={Solution.DivideAndConquer}
-                                    icon={<Looks3OutlinedIcon />}
-                                />
-                                <BottomNavigationAction
-                                    label="Binary Search"
-                                    value={Solution.BinarySearch}
-                                    icon={<Looks4OutlinedIcon />}
-                                />
-                            </BottomNavigation>
-                        </FormControl>
-
-                        <div style={{ textAlign: "center" }}>
-                            <ButtonGroup variant='contained' size='large' color='primary'>
-                                <Button
-                                    startIcon={<AutoFixHighIcon />}
-                                    onClick={handleAutoFill}
-                                >
-                                    auto
-                                </Button>
-
-                                <Button
-                                    startIcon={<OutputIcon />}
-                                    onClick={handleSubmit}
-                                    disabled={localHaystack.length === 0 || localNeedle.length === 0}
-                                >
-                                    submit
-                                </Button>
-                            </ButtonGroup>
-                        </div>
-                    </Box>
-                </Card>
-            </Paper>
-        </Container>
+                        <StyledButton
+                            disabled={!localSolution || localStrings.length === 0}
+                        >
+                            <OutputIcon />
+                        </StyledButton>
+                    </Stack>
+                </Box>
+            </Card>
+        </Paper>
     );
 }
