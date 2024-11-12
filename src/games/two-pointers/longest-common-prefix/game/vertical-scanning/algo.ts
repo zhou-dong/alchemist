c2ace88de23ac3993490d23fd2b21438270aa27e
DONG ZHOU <82224165@qq.com>
2024-11-12 02:03:00 -0800

    Update algo.ts

--- src/games/two-pointers/longest-common-prefix/game/vertical-scanning/algo.ts
+++ src/games/two-pointers/longest-common-prefix/game/vertical-scanning/algo.ts
@@ -6,5 +6,22 @@ export const buildSteps = (input: string[]): Step[] => {
 
     const steps: Step[] = [];
 
+    function longestCommonPrefix(strs: string[]): string {
+        if (strs.length === 0) {
+            return "";
+        }
+
+        let prefix = "";
+        for (let i = 0; i < strs[0].length; i++) {
+            for (let j = 0; j < strs.length; j++) {
+                if (strs[0].charAt(i) !== strs[j].charAt(i)) {
+                    return prefix;
+                }
+            }
+            prefix += strs[0].charAt(i);
+        }
+        return prefix;
+    };
+
     return steps;
 }
