17d5e3b9fb82e3b692500b1f628d42694995cb77
DONG ZHOU <82224165@qq.com>
2024-11-23 03:21:38 -0800

    Update index.tsx

--- src/games/two-pointers/longest-common-prefix/game/vertical-scanning/index.tsx
+++ src/games/two-pointers/longest-common-prefix/game/vertical-scanning/index.tsx
@@ -1,9 +1,29 @@
+import CheckIcon from '@mui/icons-material/Check';
+import { useAlgoContext } from '../../AlgoContext';
+import MouseIcon from '@mui/icons-material/Mouse';
+import { State } from '../../AlgoState';
+import { StyledButton } from '../Component';
 
+const Main = () => {
 
-const Main = () => (
-    <>
-        vertical scanning
-    </>
-);
+    const { state, setState, verticalScanningSteps, index, setIndex } = useAlgoContext();
+
+    const handleClick = () => {
+        setIndex(i => i + 1);
+    }
+
+    return (
+        <>
+            <StyledButton
+                disabled={state !== State.Playing}
+                onClick={handleClick}
+                size='large'
+                color='primary'
+            >
+                {state === State.Finished ? <CheckIcon sx={{ color: 'green' }} /> : <MouseIcon />}
+            </StyledButton>
+        </>
+    );
+}
 
 export default Main;
