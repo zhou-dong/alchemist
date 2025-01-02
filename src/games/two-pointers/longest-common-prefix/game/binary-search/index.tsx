82d4de028d7fc492be3efa2016bbb9539450cb2c
DONG ZHOU <82224165@qq.com>
2025-01-01 18:13:07 -0800

    update of update long common prefix

--- src/games/two-pointers/longest-common-prefix/game/binary-search/index.tsx
+++ src/games/two-pointers/longest-common-prefix/game/binary-search/index.tsx
@@ -3,17 +3,31 @@ import { useAlgoContext } from '../../AlgoContext';
 import MouseIcon from '@mui/icons-material/Mouse';
 import { State } from '../../AlgoState';
 import { StyledButton } from '../Component';
+import { Chip } from '@mui/material';
+import ConstructionIcon from '@mui/icons-material/Construction';
 
 const Main = () => {
 
-    const { state, setState, horizontalScanningSteps, index, setIndex } = useAlgoContext();
+    const { state, setState, binarySearchSteps, index, setIndex } = useAlgoContext();
 
     const handleClick = () => {
+        const step = binarySearchSteps[index];
+        if (!step) {
+            setState(State.Finished);
+            return;
+        }
+
         setIndex(i => i + 1);
     }
 
     return (
         <>
+            <Chip
+                icon={<ConstructionIcon />}
+                label="Under ConstructionIcon"
+                color='warning'
+            />
+
             <StyledButton
                 disabled={state !== State.Playing}
                 onClick={handleClick}
