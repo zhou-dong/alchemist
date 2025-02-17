0ea5c065cb53537b83775f8558e8ad84bad5d691
zhou-dong <82224165@qq.com>
2025-02-16 20:47:14 -0800

    make category full screen

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,11 +1,21 @@
-
-
+import { Box, ThemeProvider } from '@mui/material';
+import theme from '../../commons/theme';
+import Header from '../commons/Header';
+import Footer from '../commons/Footer';
 
 const Main = () => {
     return (
-        <>
-            categories
-        </>
+        <ThemeProvider theme={theme}>
+            <Box
+                display="flex"
+                flexDirection="column"
+                minHeight="100vh"
+            >
+                <Header />
+                categories
+                <Footer />
+            </Box>
+        </ ThemeProvider>
     );
 }
 
--- src/pages/categories/tree/index.tsx
+++ src/pages/categories/tree/index.tsx
@@ -1,12 +1,21 @@
-
-
-
+import { Box, ThemeProvider } from '@mui/material';
+import theme from '../../../commons/theme';
+import Header from '../../commons/Header';
+import Footer from '../../commons/Footer';
 
 const Main = () => {
     return (
-        <>
-            tree
-        </>
+        <ThemeProvider theme={theme}>
+            <Box
+                display="flex"
+                flexDirection="column"
+                minHeight="100vh"
+            >
+                <Header />
+                tree
+                <Footer />
+            </Box>
+        </ ThemeProvider>
     );
 }
 
