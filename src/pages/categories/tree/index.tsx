bbb5afb7efcb4cdaed26516cb3a9b4dd1cea40c7
zhou-dong <82224165@qq.com>
2025-04-04 21:37:10 -0700

    Update index.tsx

--- src/pages/categories/tree/index.tsx
+++ src/pages/categories/tree/index.tsx
@@ -3,7 +3,7 @@ import { Box, ThemeProvider } from '@mui/material';
 import theme from '../../../commons/theme';
 import Header from '../../commons/Header';
 import Footer, { footerHeight } from '../../commons/Footer';
-import { ContentCircle, Dragger, drawArrow, drawCircle, linearResize } from '../../commons/circle';
+import { ContentCircle, doubleLineResize, Dragger, drawArrow, drawCircle, linearResize } from '../../commons/circle';
 import { steps } from './steps';
 import { resetCanvas } from '../../commons/canvas';
 
@@ -69,7 +69,8 @@ const Roadmap = () => {
             const { width, top } = container.getBoundingClientRect();
             const height = window.innerHeight - top - footerHeight;
 
-            linearResize(steps, height);
+            // linearResize(steps, height);
+            doubleLineResize(steps, height);
             drawCanvas(width, height);
 
             canvasWidth = width;
