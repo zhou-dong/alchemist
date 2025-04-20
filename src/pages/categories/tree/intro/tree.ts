96ed9c63757f8c7b36958f2f244ef5a3fc956a86
zhou-dong <82224165@qq.com>
2025-04-20 01:51:39 -0700

    Update tree.ts

--- src/pages/categories/tree/intro/tree.ts
+++ src/pages/categories/tree/intro/tree.ts
@@ -7,7 +7,7 @@ const base: Node = {
     y: 0,
     radius: 0,
     value: "",
-    text: "root",
+    text: "",
     emoji: "",
     selected: false,
 }
@@ -20,22 +20,28 @@ const tree = [
     buildNode(),
     buildNode(),
     buildNode(),
+    null,
+    buildNode(),
 ];
 
 tree.forEach((node, index) => {
-    node.emoji = index + 1 + "";
-
-    if (index % 2) {
+    if (node) {
         node.selected = true;
     }
 });
 
 export const setBasicTreePosition = (width: number, height: number) => {
-    const radius = height / 16;
+    const r1 = height / 16;
+    const r2 = width / 16;
+    const radius = Math.min(r1, r2);
+
     const adjustX: number = width / 2;
-    const adjustY: number = 5 * radius;
+    const adjustY: number = height / 2 - 4 * radius;
 
     tree.forEach((node, index) => {
+
+        if (!node) return;
+
         // reset to 0 to make sure will not over calculation.
         if (index === 0) {
             node.x = 0;
@@ -58,18 +64,23 @@ export const setBasicTreePosition = (width: number, height: number) => {
     });
 
     tree.forEach(node => {
-        node.x += adjustX;
-        node.y += adjustY;
+        if (node) {
+            node.x += adjustX;
+            node.y += adjustY;
+        }
     });
 }
 
 export const drawTreeBasics = (context: CanvasRenderingContext2D) => {
-
     tree.forEach(node => {
-        drawCircle(context, node);
+        if (node) {
+            drawCircle(context, node);
+        }
     });
 
     tree.forEach((node, index) => {
+        if (!node) return;
+
         const left = tree[2 * index + 1];
         const right = tree[2 * index + 2];
         if (left) {
