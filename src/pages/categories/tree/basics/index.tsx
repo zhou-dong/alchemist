a71bf68015fac0921a52d82af58fc5d75c13f27b
zhou-dong <82224165@qq.com>
2025-05-01 01:37:32 -0700

    Update index.tsx

--- src/pages/categories/tree/basics/index.tsx
+++ src/pages/categories/tree/basics/index.tsx
@@ -2,13 +2,39 @@ import React from "react";
 import { ThemeProvider } from "@emotion/react";
 import { Box } from "@mui/material";
 import theme from "../../../../commons/theme";
-import Footer from "../../../commons/Footer";
+import Footer, { footerHeight } from "../../../commons/Footer";
 import Header from "../../../commons/Header";
 
 const Main = () => {
 
     const containerRef = React.useRef<HTMLDivElement>(null);
     const canvasRef = React.useRef<HTMLCanvasElement>(null);
+
+    React.useEffect(() => {
+        const container = containerRef.current;
+        const canvas = canvasRef.current;
+
+        if (!container || !canvas) return;
+
+        const refreshCanvas = () => {
+            const { width, top } = container.getBoundingClientRect();
+            const height = window.innerHeight - top - footerHeight;
+
+            // setBasicTreePosition(width, height);
+            // drawCanvas(width, height);
+        }
+
+        refreshCanvas();
+
+        const resizeObserver = new ResizeObserver(() => refreshCanvas());
+
+        resizeObserver.observe(container);
+
+        return () => {
+            resizeObserver.unobserve(container);
+        };
+
+    }, [containerRef, canvasRef]);
 
     return (
         <div
