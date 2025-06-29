b2a6c8ea99ffb44585f1a50f37fa75d051a8f9f4
zhou-dong <82224165@qq.com>
2025-06-29 06:21:03 -0700

    Update animations.ts

--- packages/obelus/src/core/types/animations.ts
+++ packages/obelus/src/core/types/animations.ts
@@ -1,14 +1,28 @@
-export const animationLeafKinds = [
-    'moveTo',
-    'fadeIn',
-    'fadeOut',
-    'rotate',
-    'scale',
-    'waitUntil'
-] as const;
-export type AnimationLeafCommandKind = typeof animationLeafKinds[number];
+export const MOVE_TO_COMMAND = 'moveTo';
+export const FADE_IN_COMMAND = 'fadeIn';
+export const FADE_OUT_COMMAND = 'fadeOut';
+export const ROTATE_COMMAND = 'rotate';
+export const SCALE_COMMAND = 'scale';
+export const WAIT_UNTIL_COMMAND = 'waitUntil';
 
-export const animationGroupKinds = ['sequence', 'parallel'] as const;
+export const animationLeafKinds = [
+    MOVE_TO_COMMAND,
+    FADE_IN_COMMAND,
+    FADE_OUT_COMMAND,
+    ROTATE_COMMAND,
+    SCALE_COMMAND,
+    WAIT_UNTIL_COMMAND
+] as const;
+
+export const SEQUENCE_COMMAND = "sequence";
+export const PARALLEL_COMMAND = "parallel";
+
+export const animationGroupKinds = [
+    SEQUENCE_COMMAND,
+    PARALLEL_COMMAND
+] as const;
+
+export type AnimationLeafCommandKind = typeof animationLeafKinds[number];
 export type AnimationGroupCommandKind = typeof animationGroupKinds[number];
 
 export interface AnimationGroupCommand {
