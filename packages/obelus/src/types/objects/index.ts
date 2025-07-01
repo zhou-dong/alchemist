f2e5e87d912fb2ed7e1617ca6c6c9e56e604a3d9
zhou-dong <82224165@qq.com>
2025-06-30 18:50:29 -0700

    Update index.ts

--- packages/obelus/src/types/objects/index.ts
+++ packages/obelus/src/types/objects/index.ts
@@ -1,4 +1,6 @@
 import { CircleObject } from "./circleObject";
 import { LineObject } from "./lineObject";
 
-export type SceneObject = CircleObject | LineObject;
+export type SceneObject =
+    | CircleObject
+    | LineObject;
