1b48b34666a19f3e20b0f048f20a83de19a8c8e8
zhou-dong <82224165@qq.com>
2025-07-01 03:50:13 -0700

    Update playScene.ts

--- packages/obelus/src/engine/playScene.ts
+++ packages/obelus/src/engine/playScene.ts
@@ -2,9 +2,9 @@ import gsap from 'gsap';
 import { Object3D } from 'three';
 import { TimelineEvent } from '../types/events';
 
-export function playScene(timelineEvents: TimelineEvent[], context: Record<string, Object3D>): void {
+export function playScene(timelineEvents: TimelineEvent[], objectMap: Record<string, Object3D>): void {
     for (const { target, targetProps, animateProps, time } of timelineEvents) {
-        const targetObject = context[target];
+        const targetObject = objectMap[target];
         if (!targetObject) {
             console.warn(`Target '${target}' not found in objectMap.`);
             continue;
