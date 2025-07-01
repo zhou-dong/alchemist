3efbc6795156f8b897901828701c7da3d52760af
zhou-dong <82224165@qq.com>
2025-07-01 01:07:49 -0700

    Update events.ts

--- packages/obelus/src/types/events.ts
+++ packages/obelus/src/types/events.ts
@@ -1,22 +1,11 @@
-type BaseEvent = {
-    type: string;
-    time: number;
-}
-
 export type AnimateProps = { duration: number } & Record<string, any>;
 
-export type AnimateEvent = BaseEvent & {
+export type AnimateEvent = {
     type: 'animate';
+    time: number;
     target: string;
     targetProps: Record<string, any>;
     animateProps: AnimateProps;
 };
 
-export type WaitEvent = BaseEvent & {
-    type: 'wait';
-    duration: number;
-};
-
-export type TimelineEvent =
-    | AnimateEvent
-    | WaitEvent;
+export type TimelineEvent = AnimateEvent;
