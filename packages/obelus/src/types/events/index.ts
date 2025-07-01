b644d0cede177593ef1237c658fbc712b90724c3
zhou-dong <82224165@qq.com>
2025-06-30 22:10:08 -0700

    Update index.ts

--- packages/obelus/src/types/events/index.ts
+++ packages/obelus/src/types/events/index.ts
@@ -1,38 +1,34 @@
 import { AnimateConfig } from "./animateConfig";
 
-type EventProps = {
+type EventObject = {
+    type: string;
     time: number;
 }
 
-type EventObject = {
-    type: string;
-}
-
-export type AnimateEventProps = EventProps & {
+export type AnimateEventProps = {
     target: string;
     targetProps: Record<string, any>;
     animateConfig?: AnimateConfig;
-}
+};
 
 export type AnimateEvent = EventObject & AnimateEventProps & {
     type: 'animate';
 };
 
-export type WaitEventProps = EventProps & {
+export type WaitEventProps = {
     duration: number;
-}
+};
 
 export type WaitEvent = EventObject & WaitEventProps & {
     type: 'wait';
 };
 
-export type GroupEventProps = EventProps & {
+export type GroupEventProps = {
     events: TimelineEvent[];
-}
+};
 
 export type SequenceEvent = EventObject & GroupEventProps & {
     type: 'sequence';
-
 };
 
 export type ParallelEvent = EventObject & GroupEventProps & {
