2637176a6012cfdf41aa610c03c861b11ca4e938
zhou-dong <82224165@qq.com>
2025-07-01 03:31:45 -0700

    Update objects.ts

--- packages/obelus/src/dsl/objects.ts
+++ packages/obelus/src/dsl/objects.ts
@@ -1,11 +1,11 @@
 import { CircleObject, CircleProps, GroupObject, LineObject, LineProps } from "../types/objects";
 
-export function circle(id: string, props: CircleProps): CircleObject {
-    return { id, type: 'circle', ...props };
+export function circle(id: string, props: CircleProps, visual?: Record<string, any>): CircleObject {
+    return { id, type: 'circle', ...props, visual };
 }
 
-export function line(id: string, props: LineProps): LineObject {
-    return { id, type: 'line', ...props };
+export function line(id: string, props: LineProps, visual?: Record<string, any>): LineObject {
+    return { id, type: 'line', ...props, visual };
 }
 
 export function group(id: string, children: string[]): GroupObject {
