3cbfb37ebdf32bb4ffd8811e36208f0120258330
zhou-dong <82224165@qq.com>
2025-06-30 22:54:58 -0700

    Update events.ts

--- packages/obelus/src/dsl/events.ts
+++ packages/obelus/src/dsl/events.ts
@@ -1,26 +1,25 @@
-import { AnimateEvent, AnimateEventProps, GroupEventProps, ParallelEvent, SequenceEvent, WaitEvent, WaitEventProps } from "../types/events";
-
-function animate(time: number, props: AnimateEventProps): AnimateEvent {
-    return { ...props, type: 'animate', time };
-}
-
-function wait(time: number, props: WaitEventProps): WaitEvent {
-    return { ...props, type: 'wait', time };
-}
-
-function sequence(time: number, props: GroupEventProps): SequenceEvent {
-    return { ...props, type: 'sequence', time };
-}
-
-function parallel(time: number, props: GroupEventProps): ParallelEvent {
-    return { ...props, type: 'parallel', time };
-}
+import { AnimateEvent, TimelineEvent, WaitEvent } from "../types/events";
+import { AnimateProps } from "../types/events/animateProps";
 
 export function at(time: number) {
     return {
-        animate: (props: AnimateEventProps): AnimateEvent => animate(time, props),
-        wait: (props: WaitEventProps): WaitEvent => wait(time, props),
-        sequence: (props: GroupEventProps): SequenceEvent => sequence(time, props),
-        parallel: (props: GroupEventProps): ParallelEvent => parallel(time, props)
+        animate: (
+            target: string,
+            targetProps: Record<string, any>,
+            animateProps?: AnimateProps
+        ): AnimateEvent => ({
+            type: 'animate',
+            time,
+            target,
+            targetProps,
+            animateProps
+        }),
+        wait: (
+            duration: number
+        ): WaitEvent => ({
+            type: 'wait',
+            time,
+            duration
+        })
     }
 }
