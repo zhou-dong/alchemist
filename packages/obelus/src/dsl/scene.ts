b62e722b0b5e228e45634906485957dac76d0429
zhou-dong <82224165@qq.com>
2025-06-30 22:54:49 -0700

    Update scene.ts

--- packages/obelus/src/dsl/scene.ts
+++ packages/obelus/src/dsl/scene.ts
@@ -1,12 +1,7 @@
 import { TimelineEvent } from "../types/events";
-import { AnimateConfig } from "../types/events/animateConfig";
 import { SceneObject } from "../types/objects";
 
 export type Scene = {
     objects: SceneObject[];
     timeline: TimelineEvent[];
-    timelineOptions?: {
-        playMode?: 'auto' | 'manual';
-        animateConfig?: AnimateConfig;
-    };
 };
