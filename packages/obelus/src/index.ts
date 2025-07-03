9538b93c2d31aded5e180c7c8f325c9465404f40
zhou-dong <82224165@qq.com>
2025-07-02 20:46:17 -0700

    Create index.ts

---
+++ packages/obelus/src/index.ts
@@ -0,0 +1,18 @@
+// scene objects
+export * from './objects/dsl/objects';
+
+// Timeline Scene
+// Timeline Scene Event
+export * from './timeline/dsl/events';
+export * from './timeline/events';
+export * from './timeline/scene';
+
+// Step Scene
+// Step Scene Event
+export * from './step/dsl/events';
+export * from './step/events';
+export * from './step/scene';
+
+// Export Scene to Json
+// Import Scene from Json
+export * from './io';
