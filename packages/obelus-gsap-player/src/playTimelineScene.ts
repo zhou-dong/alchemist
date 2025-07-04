61c06cb09a9dc4b19eb9bcb8ef65c544099e2715
zhou-dong <82224165@qq.com>
2025-07-03 19:14:51 -0700

    Update playTimelineScene.ts

--- packages/obelus-gsap-player/src/playTimelineScene.ts
+++ packages/obelus-gsap-player/src/playTimelineScene.ts
@@ -1,40 +1,53 @@
 import gsap from 'gsap';
 import { TimelineEvent } from '../../obelus';
 
-type SceneContext = {
-    objectMap: Record<string, any>;
-};
-
 function animateNestedProps(
     target: any,
     targetProps: Record<string, any>,
     animateProps: Record<string, any>,
+    time: number,
     timeline: gsap.core.Timeline,
 ): void {
+
+    if (!target) return;
+    if (!targetProps) return;
+
     for (const key in targetProps) {
         const value = targetProps[key];
         if (typeof value !== 'object') {
-            // at(0).animate('ball', { opacity: 2 }, { duration: 0.5 }),
-            gsap.to(target, { [key]: value, ...animateProps });
+            // at(0).animate('ball', { opacity: 2 }, { duration: 0.5 })
+            // key = opacity
+            // value = 2
+            timeline.to(target, { [key]: value, ...animateProps }, time);
         } else {
             // Recurse into nested object (e.g., position, rotation, scale)
-            // at(0).animate('ball', { position: { y: -50 } }, { duration: 0.5 }),
-            animateNestedProps(target[key], value, animateProps, timeline);
+            // at(0).animate('ball', { position: { y: -50 } }, { duration: 0.5 })
+            // key = position
+            // value = { y: -50 }
+            animateNestedProps(target[key], value, animateProps, time, timeline);
         }
     }
 };
 
-export function playTimelineScene(events: TimelineEvent[], context: SceneContext): gsap.core.Timeline {
+export type TimelineScenePlayerProps = {
+    events: TimelineEvent[];
+    objectMap: Record<string, any>;
+    onStart: Callback;
+    onComplete: Callback;
+}
 
-    const timeline = gsap.timeline();
+type Callback = (...args: any[]) => void | null;
+export function TimelineScenePlayer({ events, objectMap, onStart, onComplete }: TimelineScenePlayerProps): gsap.core.Timeline {
+
+    const timeline = gsap.timeline({ onStart, onComplete });
 
     for (const event of events) {
-        const target = context.objectMap[event.target];
+        const target = objectMap[event.target];
         if (!target) {
             console.warn(`Target '${event.target}' not found.`);
             continue;
         }
-        animateNestedProps(target, event.targetProps, event.animateProps, timeline);
+        animateNestedProps(target, event.targetProps, event.animateProps, event.time, timeline);
     }
 
     return timeline;
