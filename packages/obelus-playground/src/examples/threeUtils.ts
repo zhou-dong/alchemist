ff8210bb0dad462eb193e337f4fa78e397d74800
zhou-dong <82224165@qq.com>
2025-07-12 18:14:05 -0700

    Create threeUtils.ts

---
+++ packages/obelus-playground/src/examples/threeUtils.ts
@@ -0,0 +1,38 @@
+import * as THREE from 'three';
+
+export function createOrthographicCamera(
+    width: number,
+    height: number,
+    {
+        near = 0.1,
+        far = 1000,
+        z = 500,
+        zoom = 1,
+    } = {}
+): THREE.OrthographicCamera {
+    const left = -width / 2;
+    const right = width / 2;
+    const top = height / 2;
+    const bottom = -height / 2;
+
+    const camera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+    camera.position.z = z;
+    camera.zoom = zoom;
+    camera.updateProjectionMatrix();
+
+    return camera;
+};
+
+export function createWebGLRenderer(
+    width: number,
+    height: number,
+    {
+        alpha = true,
+        antialias = true,
+    } = {}
+): THREE.WebGLRenderer {
+    const renderer = new THREE.WebGLRenderer({ alpha, antialias });
+    renderer.setSize(width, height);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    return renderer;
+};
