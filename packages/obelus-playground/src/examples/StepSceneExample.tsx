f6a86317f1a7f2f81613ab7f5c41928c1c550e02
zhou-dong <82224165@qq.com>
2025-07-12 18:13:59 -0700

    Update StepSceneExample.tsx

--- packages/obelus-playground/src/examples/StepSceneExample.tsx
+++ packages/obelus-playground/src/examples/StepSceneExample.tsx
@@ -1,12 +1,13 @@
-import { createOrthographicCamera, createWebGLRenderer, useThreeContainer } from '../hooks/threeUtils';
+import { createOrthographicCamera, createWebGLRenderer } from './threeUtils';
 import type { StepScene } from '../../../obelus/dist';
-import { circle, animate, line } from '../../../obelus/dist';
+import { circle, animate, line, group } from '../../../obelus/dist';
 import { renderScene } from '../../../obelus-three-render/dist';
 import { StepScenePlayer } from '../../../obelus-gsap-player/dist';
 import React from 'react';
 import * as THREE from 'three';
 import { useThreeAnimation } from '../hooks/useThreeAnimation';
 import { useThreeAutoResize } from '../hooks/useThreeAutoResize';
+import { useThreeContainer } from '../hooks/useThreeContainer';
 
 const buttonStyle = {
   bottom: '20px',    // Positions the button 20px from the bottom
@@ -20,55 +21,55 @@ const buttonStyle = {
   zIndex: '1000',    // Ensure the button is on top
 };
 
-const extra = { material: { color: "blue" } };
+const extra = { material: { color: "#fff" } };
 
-const center = { x: 0, y: 0, z: 0 };
-const radius = 20;
+const position = { x: 0, y: 0, z: 0 };
+const radius = 10;
 
 const stepScene: StepScene = {
   objects: [
     circle('circle1', {
-      center: { x: -400, y: 0, z: 0 },
+      position,
       radius,
       extra // cyan
     }),
     circle('circle2', {
-      center: { x: -300, y: 0, z: 0 },
+      position,
       radius,
       extra // green
     }),
     circle('circle3', {
-      center: { x: -200, y: 0, z: 0 },
+      position,
       radius,
       extra // lightblue
     }),
     circle('circle4', {
-      center: { x: -100, y: 0, z: 0 },
+      position,
       radius,
       extra // yellow
     }),
     circle('circle5', {
-      center: { x: 0, y: 0, z: 0 },
+      position,
       radius,
       extra // purple
     }),
     circle('circle6', {
-      center: { x: 100, y: 0, z: 0 },
+      position,
       radius,
       extra
     }),
     circle('circle7', {
-      center: { x: 200, y: 0, z: 0 },
+      position,
       radius,
       extra // red
     }),
     circle('circle8', {
-      center: { x: 300, y: 0, z: 0 },
+      position,
       radius,
       extra // orange
     }),
     circle('circle9', {
-      center: { x: 400, y: 0, z: 0 },
+      position,
       radius,
       extra
     }),
@@ -76,18 +77,28 @@ const stepScene: StepScene = {
       start: { x: 0, y: 0, z: 0 },
       end: { x: 1200, y: 0, z: 0 },
       extra
-    })
+    }),
+    group('group1', ["circle1", "circle2"])
+    // latex("latex1", {
+    //   expression: "\\frac{1}{\\sqrt{2\\pi}} e^{-x^2/2}",
+    //   position: { x: 0, y: -100, z: 0 },
+    //   extra: { color: "green", fontSize: 100 }
+    // })
   ],
   steps: [
-    animate('line1', { position: { x: -1200, y: 200 } }, { duration: 1 }),
     animate('circle1', { position: { y: 200 } }, { duration: 1 }),
     animate('circle2', { position: { x: 200, y: 200 } }, { duration: 1 }),
+    animate('group1', { position: { y: -400 } }, { duration: 1 }),
+    animate('line1', { position: { x: -200, y: 200 } }, { duration: 1 }),
+    // animate('latex1', { position: { x: 200, y: -200 } }, { duration: 1 }),
+
     animate('circle3', { position: { x: 200 } }, { duration: 1 }),
     animate('circle4', { position: { x: 200, y: -200 } }, { duration: 1 }),
     animate('circle5', { position: { y: -200 } }, { duration: 1 }),
     animate('circle6', { position: { x: -200, y: -200 } }, { duration: 1 }),
     animate('circle7', { position: { x: -200 } }, { duration: 1 }),
     animate('circle8', { position: { x: -200, y: 200 } }, { duration: 1 }),
+    animate('circle9', { position: { x: -400, y: 200 } }, { duration: 1 }),
   ]
 };
 
@@ -95,39 +106,43 @@ const width = window.innerWidth;
 const height = window.innerHeight;
 
 const renderer = createWebGLRenderer(window.innerWidth, window.innerHeight);
+const scene = new THREE.Scene();
+const camera = createOrthographicCamera(width, height);
+const objectMap = renderScene(stepScene.objects, scene);
 
 export function StepSceneExample() {
 
+  const [disabled, setDisabled] = React.useState(false);
   const [index, setIndex] = React.useState(0);
-  const scene = new THREE.Scene();
-  const camera = createOrthographicCamera(width, height);
+
   const { containerRef } = useThreeContainer(renderer);
-  const { startAnimation, stopAnimation, renderAnimationOnce } = useThreeAnimation(renderer, scene, camera);
+  const { startAnimation, stopAnimation } = useThreeAnimation(renderer, scene, camera);
   useThreeAutoResize(containerRef, renderer, scene, camera);
 
-  const objectMap = renderScene(stepScene.objects, scene);
+  const steps = React.useMemo(
+    () => StepScenePlayer({ objectMap, events: stepScene.steps, onStart: startAnimation, onComplete: stopAnimation }),
+    []
+  );
 
-  const steps = React.useMemo(() => {
-    return StepScenePlayer({ objectMap, events: stepScene.steps, onStart: startAnimation, onComplete: stopAnimation })
-  }, []);
+  const nextClick = async () => {
 
-  const nextClick = () => {
-    if (index >= steps.length) {
-      steps[index].play();
-    } else {
-      steps[index].play();
-    }
+    console.log("click....");
+
+    setDisabled(true);
+    await steps[index].play();
+    setDisabled(false);
 
     const next = (index + 1) % steps.length;
     setIndex(next);
   }
 
-  renderAnimationOnce();
+  // renderAnimationOnce();
   return (
     <>
       <button
         onClick={nextClick}
         style={{ ...buttonStyle, position: 'fixed' }}
+        disabled={disabled}
       >
         play
       </button>
