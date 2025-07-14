349d7d84f29c1c54f664e3ab64ae2ec48ed768d1
zhou-dong <82224165@qq.com>
2025-07-14 02:56:49 -0700

    Update beta-distribution.ts

--- packages/obelus-playground/src/sketches/theta/math-behind-kmv/beta-distribution.ts
+++ packages/obelus-playground/src/sketches/theta/math-behind-kmv/beta-distribution.ts
@@ -14,7 +14,12 @@ const STEP_3 = `
 
 const STEP_4 = `
 \\Rightarrow \\quad
-\\mathbb{E}[X_{(k)}] = \\frac{k}{k + (n - k + 1)} = \\frac{k}{n + 1}
+\\mathbb{E}[X_{(k)}] = \\frac{k}{k + (n - k + 1)}
+`;
+
+const STEP_5 = `
+\\Rightarrow \\quad
+\\mathbb{E}[X_{(k)}] = \\frac{k}{n + 1}
 `;
 
 type Step = {
@@ -28,5 +33,6 @@ export const betaDistributionToExpectedKthValueFormulas: Step[] = [
     { formula: STEP_1, height: 25, x: 0 },
     { formula: STEP_2, height: 25, x: 15 },
     { formula: STEP_3, height: 50, x: -33 },
-    { formula: STEP_4, height: 50, x: 15 },
+    { formula: STEP_4, height: 50, x: -30 },
+    { formula: STEP_5, height: 50, x: -71 },
 ];
