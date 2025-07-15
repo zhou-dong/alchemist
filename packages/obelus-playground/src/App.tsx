3b51b744e01c27ee5f89ef7875fa366613256584
zhou-dong <82224165@qq.com>
2025-07-15 03:59:50 -0700

    Update App.tsx

--- packages/obelus-playground/src/App.tsx
+++ packages/obelus-playground/src/App.tsx
@@ -2,19 +2,28 @@ import { StepSceneExample } from './examples/StepSceneExample';
 import { TimelineSceneExample } from './examples/TimelineSceneExample';
 import { useThree } from './hooks/useThree';
 
+const { renderer, camera } = useThree();
 const StepScene = () => {
-  const { renderer, scene, camera } = useThree();
+  const { scene } = useThree();
   return (
     <StepSceneExample renderer={renderer} scene={scene} camera={camera} />
   )
 }
 
+const TimelineScene = () => {
+  const { scene } = useThree();
+  return (
+    <TimelineSceneExample renderer={renderer} scene={scene} camera={camera} />
+  )
+}
+
+const displayScene = "timeline";
+
 function App() {
 
   return (
     <>
-      {/* <TimelineSceneExample /> */}
-      <StepScene />
+      {(displayScene !== "timeline") ? <TimelineScene /> : <StepScene />}
     </>
   )
 }
