2e9ed1b7cfbb49e3fa5e17875ad9ca220ed546ea
zhou-dong <82224165@qq.com>
2025-07-15 03:17:58 -0700

    Update utils.ts

--- packages/obelus-playground/src/sketches/theta/interfaces/utils.ts
+++ packages/obelus-playground/src/sketches/theta/interfaces/utils.ts
@@ -14,3 +14,21 @@ export const alignX = (objectMap: Record<string, THREE.Object3D>) => {
         mesh.position.setX(width / 2 - xAlign);
     });
 };
+
+export const alignY = (lineHeight: number, size: number, yAlign: number) => {
+    const totalHeight = lineHeight * size;
+    const startY = totalHeight / 2 - lineHeight / 2;
+
+    const yAxis: number[] = [];
+    for (let i = 0; i < size; i++) {
+        const y = startY - i * lineHeight; // vertical centering
+        yAxis[i] = y;
+    }
+
+    const yInit: number[] = [];
+    for (let i = 0; i < size; i++) {
+        yInit[i] = yAxis[i] + yAlign;
+    }
+
+    return { yAxis, yInit };
+};
