0d5a4319447e6147b666ec5ee56008a9fcda4fa3
zhou-dong <82224165@qq.com>
2025-07-15 03:18:00 -0700

    Update kmv-estimate-steps-scene.ts

--- packages/obelus-playground/src/sketches/theta/kmv/kmv-estimate-steps-scene.ts
+++ packages/obelus-playground/src/sketches/theta/kmv/kmv-estimate-steps-scene.ts
@@ -1,39 +1,33 @@
 import { animate, latex, type StepScene } from "../../../../../obelus/dist";
+import { alignY } from "../interfaces/utils";
 import { kmvEstimationFormulas } from "./kmv-estimate-steps-latex";
 
 const color = "blue";
 const fontSize = "28px";
 
 const lineHeight = 80;
-
-const yAxis: number[] = [];
 const yAlign = -1000;
 
-// Compute total height
-const count = kmvEstimationFormulas.length;
-const totalHeight = lineHeight * count;
-const startY = totalHeight / 2 - lineHeight / 2;
+const { yAxis, yInit } = alignY(lineHeight, kmvEstimationFormulas.length, yAlign);
+const keyPrefix = "kmvEstimate_";
 
-const objects = kmvEstimationFormulas.map(({ formula, height }, index) => {
-
-    let y = startY - index * lineHeight; // vertical centering
-    y = y + yAlign;
-    yAxis[index] = y;
-
-    return latex("betaDistributionToExpectedKthValueStep_" + index, {
+const objects = kmvEstimationFormulas.map(({ formula, height }, index) =>
+    latex(keyPrefix + index, {
         expression: formula,
-        position: { x: 0, y, z: 0 },
+        position: { x: 0, y: yInit[index], z: 0 },
         extra: {
             style: { color, fontSize },
-            height: height,
+            height,
         }
-    });
-});
+    })
+);
 
-const steps = kmvEstimationFormulas.map(({ }, index) => {
-    const y = yAxis[index] - yAlign
-    return animate("betaDistributionToExpectedKthValueStep_" + index, { position: { y } }, { duration: 1 })
-});
+const steps = kmvEstimationFormulas.map(({ }, index) =>
+    animate(
+        keyPrefix + index,
+        { position: { y: yAxis[index] } },
+        { duration: 1 }
+    )
+);
 
 export const stepScene: StepScene = { objects, steps };
-
