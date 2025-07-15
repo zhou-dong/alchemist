b73c249f1857b76573517c1083ec86dd5dee8729
zhou-dong <82224165@qq.com>
2025-07-15 02:29:39 -0700

    Update kmv-estimate-steps-scene.ts

--- packages/obelus-playground/src/sketches/theta/kmv/kmv-estimate-steps-scene.ts
+++ packages/obelus-playground/src/sketches/theta/kmv/kmv-estimate-steps-scene.ts
@@ -1,4 +1,3 @@
-import * as THREE from 'three';
 import { animate, latex, type StepScene } from "../../../../../obelus/dist";
 import { kmvEstimationFormulas } from "./kmv-estimate-steps-latex";
 
@@ -38,17 +37,3 @@ const steps = kmvEstimationFormulas.map(({ }, index) => {
 
 export const stepScene: StepScene = { objects, steps };
 
-export const alignX = (objectMap: Record<string, THREE.Object3D>) => {
-    const xAlign = Object.keys(objectMap)
-        .map(key => objectMap[key])
-        .reduce((a, b) => (a.scale.x > b.scale.x) ? a : b)
-        .scale
-        .x / 2;
-
-    // left-align
-    Object.keys(objectMap).forEach(key => {
-        const mesh = objectMap[key];
-        const width = mesh.scale.x;
-        mesh.position.setX(width / 2 - xAlign);
-    });
-};
