1f0a1d44ae485bd2d38c737f93a05e1c7eff4881
zhou-dong <82224165@qq.com>
2025-07-15 03:23:54 -0700

    Delete constants.ts

--- packages/obelus-playground/src/sketches/theta/math-behind-kmv/constants.ts
+++
@@ -1,4 +0,0 @@
-export const EXPECTED_KTH_ORDER_STATISTIC_LATEX_FORMULA = '\\mathbb{E}[X_{(k)}] \\approx \\frac{k}{n + 1}';
-export const KMV_ESTIMATION_LATEX_FORMULA = 'n \\approx \\frac{k}{\\mathbb{E}[X_{(k)}]} - 1';
-export const THETA_EXPECTATION_DEFINITION = '\\theta = \\mathbb{E}[X_{(k)}]';
-export const THETA_ESTIMATION_LATEX_FORMULA = 'n = \\frac{k}{\\theta} - 1';
