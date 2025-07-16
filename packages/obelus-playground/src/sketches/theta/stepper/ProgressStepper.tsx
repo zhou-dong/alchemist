462668b2b4210b46e06ba48ee5da3f8abbf7f459
zhou-dong <82224165@qq.com>
2025-07-16 02:37:29 -0700

    Update ProgressStepper.tsx

--- packages/obelus-playground/src/sketches/theta/stepper/ProgressStepper.tsx
+++ packages/obelus-playground/src/sketches/theta/stepper/ProgressStepper.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material/styles';
 
 const Location = styled(Box)(({ }) => ({
     position: 'fixed',
-    top: 120, // adjust depending on header height
+    top: 100, // adjust depending on header height
     left: 0,
     right: 0,
     zIndex: 1100,
