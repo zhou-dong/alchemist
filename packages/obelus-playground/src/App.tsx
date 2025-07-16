2feda4b603f3fd4b273919a261b05cc08ed2dea5
zhou-dong <82224165@qq.com>
2025-07-16 01:49:16 -0700

    Update App.tsx

--- packages/obelus-playground/src/App.tsx
+++ packages/obelus-playground/src/App.tsx
@@ -1,34 +1,18 @@
+import * as THREE from 'three';
 import { Routes, Route } from 'react-router-dom';
-import { StepSceneExample } from './examples/StepSceneExample';
-import { TimelineSceneExample } from './examples/TimelineSceneExample';
 import { useThree } from './hooks/useThree';
 import OrderStatisticsPage from './sketches/theta/order-statistics';
 import WelcomePage from './sketches/theta/welcome/WelcomePage';
 import ThemeToggleFab from './ThemeToggleFab';
 
 const { renderer, camera } = useThree();
-const StepScene = () => {
-  const { scene } = useThree();
-  return (
-    <StepSceneExample renderer={renderer} scene={scene} camera={camera} />
-  )
-}
-
-const TimelineScene = () => {
-  const { scene } = useThree();
-  return (
-    <TimelineSceneExample renderer={renderer} scene={scene} camera={camera} />
-  )
-}
-
-const displayScene = "timeline";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/sketches/theta/welcome" element={<WelcomePage />} />
-        <Route path="/sketches/theta/order-statistics" element={<OrderStatisticsPage />} />
+        <Route path="/sketches/theta/order-statistics" element={<OrderStatisticsPage renderer={renderer} scene={new THREE.Scene()} camera={camera} />} />
       </Routes>
       <ThemeToggleFab />
     </>
