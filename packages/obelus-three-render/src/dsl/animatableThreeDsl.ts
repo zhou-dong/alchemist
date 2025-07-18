0aca5c88a72963a549eba8e3f991b7c15683c458
Dong Zhou <dozhou@rubiconproject.com>
2025-07-17 17:51:53 -0700

    Update animatableThreeDsl.ts

--- packages/obelus-three-render/src/dsl/animatableThreeDsl.ts
+++ packages/obelus-three-render/src/dsl/animatableThreeDsl.ts
@@ -1,6 +1,21 @@
 import { type AnimatableObject } from "obelus";
-import { Object3D } from "three";
+import * as THREE from "three";
+import { createLatexSprite } from "../components/latexBuilder";
 
-export function animatable(id: string, target: Object3D): AnimatableObject<Object3D> {
-    return { id, target };
+export function animatable(id: string, target: THREE.Object3D): AnimatableObject<THREE.Object3D> {
+    return { id, target, type: 'object' };
+};
+
+export function circle(id: string, geometry: THREE.CircleGeometry, material: THREE.Material): AnimatableObject<THREE.Mesh> {
+    return { id, target: new THREE.Mesh(geometry, material), type: 'object' };
+};
+
+export function line(id: string, points: THREE.Vector3[], material: THREE.Material): AnimatableObject<THREE.Line> {
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+    return { id, target: new THREE.Line(geometry, material), type: 'object' };
+};
+
+export async function latex(id: string, text: string, height: number, style?: Partial<CSSStyleDeclaration>): Promise<AnimatableObject<THREE.Sprite>> {
+    const sprite = await createLatexSprite(text, style, height);
+    return { id, target: sprite, type: 'object' };
 };
