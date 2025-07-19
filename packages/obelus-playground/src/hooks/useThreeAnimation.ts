d077a12884d89fb993f1a0ef0c7563b176df15c3
Dong Zhou <dozhou@rubiconproject.com>
2025-07-19 03:24:47 -0700

    Update useThreeAnimation.ts

--- packages/obelus-playground/src/hooks/useThreeAnimation.ts
+++ packages/obelus-playground/src/hooks/useThreeAnimation.ts
@@ -1,15 +1,16 @@
+import type { DualRenderer, DualScene } from 'obelus-three-render';
 import { useRef, useCallback, useEffect } from 'react';
 import * as THREE from 'three';
 
 export function useThreeAnimation(
-    renderer: THREE.Renderer,
-    scene: THREE.Scene,
+    renderer: DualRenderer,
+    scene: DualScene,
     camera: THREE.Camera,
 ) {
     const animationFrameRef = useRef<number | null>(null);
 
     const animate = useCallback(() => {
-        renderer.render(scene, camera);
+        renderer.render(scene.threeScene, scene.css3dScene, camera as any);
         animationFrameRef.current = requestAnimationFrame(animate);
     }, [scene, camera, renderer]);
 
@@ -27,7 +28,7 @@ export function useThreeAnimation(
     }, []);
 
     const renderAnimationOnce = useCallback(() => {
-        renderer.render(scene, camera);
+        renderer.render(scene.threeScene, scene.css3dScene, camera as any);
     }, [scene, camera, renderer]);
 
     useEffect(() => stopAnimation, [stopAnimation]);
