c1783c69feaa3e3370d37d5663fcefe1696471ba
Dong Zhou <dozhou@rubiconproject.com>
2025-07-19 16:24:39 -0700

    Update animatableThreeDsl.ts

--- packages/obelus-three-render/src/dsl/animatableThreeDsl.ts
+++ packages/obelus-three-render/src/dsl/animatableThreeDsl.ts
@@ -42,20 +42,21 @@ export function scaleAxisChildren(id: string): string[] {
     return [id, id + '_min', id + '_max'];
 }
 
-export function scaleAxis(
-    id: string,
-    axisOptions: AxisOptions,
-    min: number,
-    max: number,
-    textStyle: Partial<CSSStyleDeclaration>,
-    textOffset: THREE.Vector3
-): AnimatableObject<THREE.Group | CSS3DObject>[] {
-    const { start, end } = axisOptions.position;
-    const minPosition = new THREE.Vector3(start.x + textOffset.x, start.y + textOffset.y, start.z + textOffset.z);
-    const maxPosition = new THREE.Vector3(end.x + textOffset.x, end.y + textOffset.y, end.z + textOffset.z);
+interface ScaleProps {
+    min: string;
+    max: string;
+    scaleStyle: Partial<CSSStyleDeclaration>;
+    scaleOffset: THREE.Vector3;
+}
 
+export function scaleAxis(id: string, axisOptions: AxisOptions, scaleProps: ScaleProps): AnimatableObject<THREE.Group | CSS3DObject>[] {
+    const { min, max, scaleStyle, scaleOffset } = scaleProps;
     const baseLine = axis(id, axisOptions);
-    const minText = text(id + '_min', min.toString(), minPosition, textStyle);
-    const maxText = text(id + '_max', max.toString(), maxPosition, textStyle);
+    const minText = text(id + '_min', min, createPosition(axisOptions.position.start, scaleOffset), scaleStyle);
+    const maxText = text(id + '_max', max, createPosition(axisOptions.position.end, scaleOffset), scaleStyle);
     return [baseLine, minText, maxText];
+};
+
+function createPosition(position: THREE.Vector3, textOffset: THREE.Vector3): THREE.Vector3 {
+    return new THREE.Vector3(position.x + textOffset.x, position.y + textOffset.y, position.z + textOffset.z);
 };
