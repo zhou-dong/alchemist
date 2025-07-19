997d8d05319c488cbb7f986d412a840a621b24ba
Dong Zhou <dozhou@rubiconproject.com>
2025-07-19 15:00:57 -0700

    Update animatableThreeDsl.ts

--- packages/obelus-three-render/src/dsl/animatableThreeDsl.ts
+++ packages/obelus-three-render/src/dsl/animatableThreeDsl.ts
@@ -3,7 +3,6 @@ import * as THREE from "three";
 import { createLatexSprite } from "../builders/latexBuilder";
 import { buildCylinderLine } from "../builders/cylinderLineBuilder";
 import { buildAxis, AxisOptions } from "../builders/axisBuilder";
-import { AxisScaleLabelOptions } from "../builders/axisScaleBuilder";
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
 
 export function animatable(id: string, target: THREE.Object3D): AnimatableObject<THREE.Object3D> {
