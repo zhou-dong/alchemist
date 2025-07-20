36e06dbfa6f34a68f6ef8b2accd7fd0627e3521d
Dong Zhou <dozhou@rubiconproject.com>
2025-07-19 23:19:05 -0700

    Update stepSceneThree.ts

--- packages/obelus-three-render/src/types/stepSceneThree.ts
+++ packages/obelus-three-render/src/types/stepSceneThree.ts
@@ -1,9 +1,8 @@
 import { type StepEvent } from "obelus";
 import { type Animatable } from "obelus";
 import { type Object3D } from "three";
-import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
 
 export type StepSceneThree = {
-    objects: Animatable<Object3D | CSS3DObject>[];
+    objects: Animatable<Object3D>[];
     steps: StepEvent[];
 };
