20540415aa64c12314b7eaeee8097543e43701ee
Dong Zhou <dozhou@rubiconproject.com>
2025-07-19 23:49:21 -0700

    Update render.ts

--- packages/obelus-three-render/src/render.ts
+++ packages/obelus-three-render/src/render.ts
@@ -3,9 +3,9 @@ import { type Object3D } from "three";
 import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
 import { DualScene } from "./dual";
 
-export function render(objects: Animatable<Object3D | CSS3DObject>[], scene: DualScene): Record<string, Object3D | CSS3DObject> {
+export function render(objects: Animatable<Object3D>[], scene: DualScene): Record<string, Object3D> {
 
-    const record: Record<string, Object3D | CSS3DObject> = {};
+    const record: Record<string, Object3D> = {};
 
     objects.forEach(({ id, target }) => {
         if (target instanceof CSS3DObject) {
