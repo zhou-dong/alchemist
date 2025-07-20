f15f77d40944234b9404c580ab75a4f72e8e417a
Dong Zhou <dozhou@rubiconproject.com>
2025-07-19 23:19:08 -0700

    Update timelineSceneThree.ts

--- packages/obelus-three-render/src/types/timelineSceneThree.ts
+++ packages/obelus-three-render/src/types/timelineSceneThree.ts
@@ -1,9 +1,8 @@
 import { type TimelineEvent } from "obelus";
 import { type Animatable } from "obelus";
 import { Object3D } from "three";
-import { CSS3DObject } from "three/examples/jsm/renderers/CSS3DRenderer";
 
 export type TimelineSceneThree = {
-    objects: Animatable<Object3D | CSS3DObject>[];
+    objects: Animatable<Object3D>[];
     timeline: TimelineEvent[];
 };
