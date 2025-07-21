511e6336580db8112583c53540d59a0142e370ce
Dong Zhou <dozhou@rubiconproject.com>
2025-07-21 03:19:34 -0700

    Update App.tsx

--- packages/obelus-playground/src/App.tsx
+++ packages/obelus-playground/src/App.tsx
@@ -1,19 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
-import { useDualRenderer } from './hooks/useThree';
 import OrderStatisticsPage from './sketches/theta/order-statistics';
 import WelcomePage from './sketches/theta/welcome/WelcomePage';
 import ThemeToggleFab from './ThemeToggleFab';
 import KmvPage from './sketches/theta/kmv';
-
-const { renderer, camera } = useDualRenderer();
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/sketches/theta/welcome" element={<WelcomePage />} />
-        <Route path="/sketches/theta/order-statistics" element={<OrderStatisticsPage renderer={renderer} camera={camera} />} />
-        <Route path="/sketches/theta/kmv" element={<KmvPage renderer={renderer} camera={camera} />} />
+        <Route path="/sketches/theta/order-statistics" element={<OrderStatisticsPage />} />
+        <Route path="/sketches/theta/kmv" element={<KmvPage />} />
       </Routes>
       <ThemeToggleFab />
     </>
