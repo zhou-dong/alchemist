1207a473997e126c9f77d0215f2c49e30025ee77
Dong Zhou <dozhou@rubiconproject.com>
2025-07-22 02:14:08 -0700

    Update App.tsx

--- packages/obelus-playground/src/App.tsx
+++ packages/obelus-playground/src/App.tsx
@@ -1,8 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
-import OrderStatisticsPage from './sketches/theta/steps/order-statistics';
-import WelcomePage from './sketches/theta/components/welcome/WelcomePage';
 import ThemeToggleFab from './ThemeToggleFab';
+import WelcomePage from './sketches/theta/components/welcome/WelcomePage';
+import OrderStatisticsPage from './sketches/theta/steps/order-statistics';
 import KmvPage from './sketches/theta/steps/kmv';
+import ThetaSketchPage from './sketches/theta/steps/theta-sketch';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/sketches/theta/welcome" element={<WelcomePage />} />
         <Route path="/sketches/theta/order-statistics" element={<OrderStatisticsPage />} />
         <Route path="/sketches/theta/kmv" element={<KmvPage />} />
+        <Route path="/sketches/theta/theta-sketch" element={<ThetaSketchPage />} />
       </Routes>
       <ThemeToggleFab />
     </>
