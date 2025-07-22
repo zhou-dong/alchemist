5602e203faab8cfe79050c9773398080ffa4b003
Dong Zhou <dozhou@rubiconproject.com>
2025-07-22 03:48:45 -0700

    Update hotpink.ts

--- packages/obelus-three-render/src/themes/hotpink.ts
+++ packages/obelus-three-render/src/themes/hotpink.ts
@@ -1,21 +1,21 @@
 import * as THREE from "three";
 import { AxisProps } from "../dsl/animatable";
 
-export const lineStyle: THREE.LineBasicMaterial = new THREE.LineBasicMaterial({ color: 'hotpink' });
+export const lineStyle: THREE.LineBasicMaterial = new THREE.LineBasicMaterial({ color: '#FFC107' });
 
 export const axisStyle: AxisProps = {
     dotCount: 3,
     lineWidth: 2,
     lineMaterial: lineStyle,
-    dotMaterial: new THREE.MeshBasicMaterial({ color: 'cyan' }),
+    dotMaterial: new THREE.MeshBasicMaterial({ color: '#00BCD4' }),
     dotRadius: 4
 }
 
 export const textStyle: Partial<CSSStyleDeclaration> = {
-    color: 'cyan',
+    color: '#00BCD4',
     fontSize: '16px',
     fontWeight: 'normal',
     fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, sans-serif',
 }
 
-export const ringStyle: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 'cyan' });
+export const ringStyle: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#FFC107' });
