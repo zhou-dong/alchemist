ed14efc89bd9526f1cc71c2d703d307452a5dc23
Dong Zhou <dozhou@rubiconproject.com>
2025-07-23 02:18:59 -0700

    Update WelcomePage.tsx

--- packages/obelus-playground/src/sketches/theta/components/welcome/WelcomePage.tsx
+++ packages/obelus-playground/src/sketches/theta/components/welcome/WelcomePage.tsx
@@ -42,7 +42,7 @@ export default function WelcomePage() {
                     sx={{ display: 'flex', alignItems: 'center', gap: 1 }}
                 >
                     <PsychologyIcon fontSize="inherit" sx={{ color: 'secondary.main' }} />
-                    The Road to Theta Sketch: A Journey Through Order Statistics, K-th Smallest Estimation, KMV, and Beyond
+                    The Road to Theta Sketch: A Journey Through Order Statistics, K-th Smallest Estimation, Theta Sketch, and Beyond
                 </Typography>
 
                 <Button
