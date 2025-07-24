61fffb89c42010c9e85fc595dd3002587e83a59b
Dong Zhou <dozhou@rubiconproject.com>
2025-07-24 01:28:46 -0700

    Update index.tsx

--- packages/obelus-playground/src/sketches/theta/steps/kmv/index.tsx
+++ packages/obelus-playground/src/sketches/theta/steps/kmv/index.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import gsap from 'gsap';
 import { useNavigate } from 'react-router-dom';
 import { at } from 'obelus';
 import { createDualRenderer, createOrthographicCamera } from '../../../../utils/threeUtils';
 import { WrapperProvider } from '../../components/wrapper/WrapperProvider';
-import { buildAnimateSteps, type PlayableStep } from 'obelus-gsap-player';
+import { buildAnimateTimeline, type PlayableStep } from 'obelus-gsap-player';
 import { useThreeContainer } from '../../../../hooks/useThreeContainer';
 import { useThreeAutoResize } from '../../../../hooks/useThreeAutoResize';
 import { DualScene, textStyle, latex, type TimelineSceneThree, render, axisStyle, axis, text } from 'obelus-three-render';
 import { AnimationController } from '../../../../utils/animation-controller';
 import KseToKmv from './KseToKmv';
 import PlayButton from '../../components/PlayButton';
+import TimelinePlayer from '../../components/TimelinePlayer';
+import { Container, Button, Box, Tooltip, Fab } from '@mui/material';
+
+import * as PlayArrow from '@mui/icons-material/PlayArrow';
+import * as Settings from '@mui/icons-material/Settings';
+import KmvConfigDialog from './KmvConfigDialog';
+
+const SettingsIcon = Settings.default as unknown as React.ElementType;
+const PlayArrowIcon = PlayArrow.default as unknown as React.ElementType;
 
 const axisWidth = window.innerWidth / 2;
 
@@ -37,7 +47,7 @@ const scene = new DualScene();
 const animationController = new AnimationController(renderer, scene, camera);
 
 const record = render(stepScene.objects, scene as any);
-let steps: PlayableStep[] = buildAnimateSteps(
+let timeline: gsap.core.Timeline = buildAnimateTimeline(
     [], //stepScene.steps,
     record,
     animationController.startAnimation,
@@ -54,6 +64,10 @@ function ThetaSketchPageContent({
     const navigate = useNavigate();
     const [disabled, setDisabled] = React.useState(false);
     const [displayIntroduction, setDisplayIntroduction] = React.useState(false);
+
+
+    const [openKmvConfigDialog, setOpenKmvConfigDialog] = React.useState(true);
+    const [showTimelinePlayer, setShowTimelinePlayer] = React.useState(true);
 
     const { containerRef } = useThreeContainer(renderer);
     useThreeAutoResize(containerRef, renderer, scene, camera);
@@ -78,22 +92,71 @@ function ThetaSketchPageContent({
             return;
         }
 
-        if (index === steps.length) {
-            navigate('/sketches/theta/set-operations');
-            return;
-        }
+        // if (index === steps.length) {
+        navigate('/sketches/theta/set-operations');
+        // return;
+        // }
 
         setDisabled(true);
-        await steps[index].play();
+        // await steps[index].play();
 
         index = index + 1;
         setDisabled(false);
     };
 
+    const KmvSettings = () => (
+        <Tooltip title={openKmvConfigDialog ? 'Close Config' : 'KMV Config'} placement="left">
+            <Fab
+                onClick={() => setOpenKmvConfigDialog(!openKmvConfigDialog)}
+                sx={{
+                    position: 'fixed',
+                    bottom: 168,
+                    right: 24,
+                    zIndex: 1000
+                }}
+            >
+                <SettingsIcon />
+            </Fab>
+        </Tooltip>
+    );
+
+    const TimelineToggle = () => (
+        <Tooltip title={showTimelinePlayer ? 'Hide Timeline' : 'Show Timeline'} placement="left">
+            <Fab
+                onClick={() => setShowTimelinePlayer(!showTimelinePlayer)}
+                sx={{
+                    position: 'fixed',
+                    bottom: 240,
+                    right: 24,
+                    zIndex: 1000
+                }}
+            >
+                < PlayArrowIcon sx={{ fontSize: 28 }} />
+            </Fab>
+        </Tooltip>
+    );
+
+    const TimelinePlayerContainer = () => (
+        <Container
+            maxWidth="sm"
+            sx={{ position: 'fixed', bottom: 100, left: 0, right: 0, }}
+        >
+            <TimelinePlayer timeline={timeline} />
+        </Container>
+    );
+
     return (
         <>
             {displayIntroduction && <KseToKmv setDisabled={setDisabled} setDisplayIntroduction={setDisplayIntroduction} />}
-            <PlayButton index={index} steps={steps} disabled={disabled} nextPage="Set Operations" onClick={onClick} />
+
+            <KmvSettings />
+            <TimelineToggle />
+
+            <Container maxWidth="xs">
+                <KmvConfigDialog open={openKmvConfigDialog} onClose={() => { setOpenKmvConfigDialog(false) }} onStart={() => { }} />
+            </Container>
+
+            {showTimelinePlayer && <TimelinePlayerContainer />}
             <div ref={containerRef} style={{ width: '100vw', height: '100vh', }} />
         </>
     );
