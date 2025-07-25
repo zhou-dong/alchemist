296931dbb7d2ab2a3e3dd7b552d57f9d67254bab
Dong Zhou <dozhou@rubiconproject.com>
2025-07-25 03:41:58 -0700

    Update index.tsx

--- packages/obelus-playground/src/sketches/theta/steps/set-operations/index.tsx
+++ packages/obelus-playground/src/sketches/theta/steps/set-operations/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
 import { WrapperProvider } from '../../components/wrapper/WrapperProvider';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+import * as RocketLaunch from '@mui/icons-material/RocketLaunch';
+import { createDualRenderer, createOrthographicCamera } from '../../../../utils/threeUtils';
+import { DualScene } from 'obelus-three-render';
+import { AnimationController } from '../../../../utils/animation-controller';
+import { useThreeContainer } from '../../../../hooks/useThreeContainer';
+import { useThreeAutoResize } from '../../../../hooks/useThreeAutoResize';
+
+const RocketLaunchIcon = RocketLaunch.default as unknown as React.ElementType;
+
+const renderer = createDualRenderer();
+const camera = createOrthographicCamera();
+const scene = new DualScene();
+const animationController = new AnimationController(renderer, scene, camera);
 
 let componentLevelShowStepper: boolean = true;
 
@@ -13,6 +27,8 @@ function SetOperationsPageContent({
 }) {
     const navigate = useNavigate();
 
+    const { containerRef } = useThreeContainer(renderer);
+    useThreeAutoResize(containerRef, renderer, scene, camera);
 
     React.useEffect(() => {
         setShowStepper(componentLevelShowStepper);
@@ -21,7 +37,34 @@ function SetOperationsPageContent({
         };
     }, []);
 
-    return <div>Set Operations</div>;
+    const StartButton = () => (
+        <Button
+            variant='contained'
+            size="large"
+            sx={{
+                position: 'fixed',
+                bottom: 100,
+                left: '50%',
+                transform: 'translateX(-50%)',
+                zIndex: 1300,
+            }}
+            startIcon={<RocketLaunchIcon />}
+            onClick={() => {
+                setShowStepper(false);
+                componentLevelShowStepper = false;
+            }}
+        >
+            Start
+        </Button>
+    );
+
+
+    return (
+        <>
+            {showStepper && <StartButton />}
+            <div ref={containerRef} style={{ width: '100vw', height: '100vh', }} />
+        </>
+    );
 };
 
 export default function SetOperationsPage() {
