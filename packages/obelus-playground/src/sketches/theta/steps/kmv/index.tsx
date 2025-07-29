4daad8cc5709718e42d503846370e9b7b9b0b262
Dong Zhou <dozhou@rubiconproject.com>
2025-07-29 13:55:25 -0700

    Update index.tsx

--- packages/obelus-playground/src/sketches/theta/steps/kmv/index.tsx
+++ packages/obelus-playground/src/sketches/theta/steps/kmv/index.tsx
@@ -152,7 +152,7 @@ function ThetaSketchPageContent({
     showStepper: boolean;
     setShowStepper: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
-    const defaultK = 10;
+    const defaultK = 15;
     const defaultStreamSize = 50;
     const [k, setK] = React.useState(defaultK);
     const [streamSize, setStreamSize] = React.useState(defaultStreamSize);
