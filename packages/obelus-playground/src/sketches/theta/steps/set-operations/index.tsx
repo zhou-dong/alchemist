8f58ef111d83b8a4e20f092dd37f911ae75ecd62
Dong Zhou <dozhou@rubiconproject.com>
2025-07-31 23:43:01 -0700

    Update index.tsx

--- packages/obelus-playground/src/sketches/theta/steps/set-operations/index.tsx
+++ packages/obelus-playground/src/sketches/theta/steps/set-operations/index.tsx
@@ -28,34 +28,48 @@ const animationController = new AnimationController(renderer, scene, camera);
 let componentLevelShowStepper: boolean = true;
 let componentLevelShowNextPageButton: boolean = false;
 
-const unionFormula = `
-\\text{Union} \\\\
+const unionFormula = (k: number, theta: number) => {
 
-\\text{sort}(A \\cup B)[:k]
+    const estimated = (k / theta - 1).toFixed(2);
+
+    return `
+\\begin{align*} 
+1. &\\quad H_{\\text{union}} = \\text{sort}(A \\cup B)[:k] \\\\ 
+2. &\\quad \\theta = \\max(H_{\\text{union}}) \\\\ 
+3. &\\quad \\hat{N} = \\frac{k}{\\theta} - 1 =  \\frac{${k}}{${theta}} - 1 = ${estimated}
+\\end{align*}
 `;
-
-const intersectionFormula = `
-\\text{Intersection} \\\\
-
+};
+const intersectionFormula = (intersectionSize: number, theta: number) => {
+    const result = (intersectionSize / theta).toFixed(2);
+    return `
 \\begin{align*}
-
 1. & \\quad \\theta = \\min(\\theta_A, \\theta_B) \\\\ 
 2. & \\quad h_1 = \\{v \\in A \\mid v < \\theta\\} \\\\ 
 3. & \\quad h_2 = \\{v \\in B \\mid v < \\theta\\} \\\\ 
-4. & \\quad \\text{result} = \\frac{|h_1 \\cap h_2|}{\\theta} 
+4. & \\quad \\hat{N} = \\frac{|h_1 \\cap h_2|}{\\theta}  = \\frac{${intersectionSize}}{${theta}} = ${result}
 \\end{align*}
 `;
+};
 
-const differenceFormula = `
-\\text{Difference} \\\\
-
-\\begin{align*}
+const differenceFormula = (differenceSize: number, theta: number) => {
+    const result = (differenceSize / theta).toFixed(2);
+    return `
+\\begin{align*}     
 1. &\\quad \\theta = \\min(\\theta_A, \\theta_B) \\\\ 
 2. &\\quad h_1 = \\{v \\in A \\mid v < \\theta\\} \\\\ 
 3. &\\quad h_2 = \\{v \\in B \\mid v < \\theta\\} \\\\ 
-4. &\\quad \\text{result} = \\frac{|h_1 \\setminus h_2|}{\\theta} 
+4. &\\quad \\hat{N} = \\frac{|h_1 \\setminus h_2|}{\\theta}  = \\frac{${differenceSize}}{${theta}} = ${result}
 \\end{align*}
 `;
+};
+
+interface AxisFormula {
+    title: string;
+    expected: number;
+    k: number;
+    theta: number;
+}
 
 const buildAxis = (id: string, y: number, width: number) => {
     const leftX = -width / 2;
@@ -64,6 +78,28 @@ const buildAxis = (id: string, y: number, width: number) => {
         axis(`${id}_axis`, { x: leftX, y }, { x: rightX, y }, { ...axisStyle, dotCount: 2 }),
         text(`${id}_start`, "0", { x: leftX, y: y - 15 }, textStyle),
         text(`${id}_end`, "1", { x: rightX, y: y - 15 }, textStyle),
+    ];
+};
+
+const buildBaseAxis = (id: string, y: number, width: number, { title, expected, k, theta }: AxisFormula) => {
+    const leftX = -width / 2;
+    const rightX = width / 2;
+
+    const estimated = (k / theta - 1).toFixed(2);
+
+    const formula = `
+    \\begin{align*}
+    \\text{Expected} &= ${expected} \\\\
+    \\text{Estimated} &= \\frac{k}{\\theta} - 1 = \\frac{${k}}{${theta}} - 1 = ${estimated}
+    \\end{align*}
+    `;
+
+    return [
+        axis(`${id}_axis`, { x: leftX, y }, { x: rightX, y }, { ...axisStyle, dotCount: 2 }),
+        text(`${id}_start`, "0", { x: leftX, y: y - 15 }, textStyle),
+        text(`${id}_end`, "1", { x: rightX, y: y - 15 }, textStyle),
+        text(`${id}_title`, title, { x: -window.innerWidth / 8 * 3, y }, textStyle),
+        latex(`${id}_formula`, formula, { x: window.innerWidth / 8 * 3, y }, textStyle),
     ];
 };
 
@@ -154,7 +190,7 @@ function SetOperationsPageContent({
         clearScene(scene);
 
         const axisWidth = window.innerWidth / 2;
-        const height = window.innerHeight / 8;
+        const height = window.innerHeight / 6;
 
         const hashesA = buildHashes(streamASize, axisWidth, -axisWidth / 2);
         const hashesB = buildHashes(streamBSize, axisWidth, -axisWidth / 2);
@@ -223,132 +259,107 @@ function SetOperationsPageContent({
             return [parallel(moveAHashes), parallel(moveBHashes)];
         }
 
+
+        const hashesAWithSmallerKth = hashesA.filter((hash) => hash.value <= smallKth.value);
+        const hashesBWithSmallerKth = hashesB.filter((hash) => hash.value <= smallKth.value);
+
+        const intersectionsSet = new Set(hashesAWithSmallerKth.map((hash) => hash.value));
+        const intersections = hashesBWithSmallerKth.filter(item => intersectionsSet.has(item.value));
+
         const intersectionCircles = () => {
             const result: any[] = [];
 
-            hashesA
-                .filter((hash) => hash.value <= smallKth.value)
+            hashesAWithSmallerKth
                 .map((hash, index) => {
                     const { location } = hash;
                     const style = new THREE.MeshBasicMaterial({ color: green });
                     result.push(circle(`intersection_a_circle_${index}`, radius, { x: location, y: height * 2, z: 1 }, style as any));
                 });
 
-            hashesB
-                .filter((hash) => hash.value <= smallKth.value)
+            hashesBWithSmallerKth
                 .map((hash, index) => {
                     const { location } = hash;
                     const style = new THREE.MeshBasicMaterial({ color: blue });
                     result.push(circle(`intersection_b_circle_${index}`, radius, { x: location, y: height, z: 1 }, style as any));
                 });
 
-            const intersections = () => {
-                const a = hashesA.filter((hash) => hash.value <= smallKth.value);
-                const b = hashesB.filter((hash) => hash.value <= smallKth.value);
-
-                const set = new Set(a.map((hash) => hash.value));
-                const intersection = b.filter(item => set.has(item.value));
-
-                intersection.map((hash, index) => {
-                    const { location } = hash;
-                    const style = new THREE.MeshBasicMaterial({ color: yellow });
-                    result.push(circle(`intersection_theta_${index}`, radius, { x: location, y: -height - window.innerHeight, z: 1 }, style as any));
-                });
-            }
-
-            intersections();
+            intersections.map((hash, index) => {
+                const { location } = hash;
+                const style = new THREE.MeshBasicMaterial({ color: yellow });
+                result.push(circle(`intersection_theta_${index}`, radius, { x: location, y: -height - window.innerHeight, z: 1 }, style as any));
+            });
 
             return result;
         }
 
         const moveIntersectionCircles = () => {
-            const moveAHashes = hashesA
-                .filter((hash) => hash.value <= smallKth.value)
+            const moveAHashes = hashesAWithSmallerKth
                 .map((_, index) => animate(`intersection_a_circle_${index}`, { position: { y: `-=${height * 3 - 10}` } }, { duration: 1 }));
 
-            const moveBHashes = hashesB
-                .filter((hash) => hash.value <= smallKth.value)
+            const moveBHashes = hashesBWithSmallerKth
                 .map((_, index) => animate(`intersection_b_circle_${index}`, { position: { y: `-=${height * 2 + 10}` } }, { duration: 1 }));
 
-            const a = hashesA.filter((hash) => hash.value <= smallKth.value);
-            const b = hashesB.filter((hash) => hash.value <= smallKth.value);
-
-            const set = new Set(a.map((hash) => hash.value));
-            const intersection = b.filter(item => set.has(item.value));
-
-            const moveIntersectionThetas = intersection.map((_, index) => {
+            const moveIntersectionThetas = intersections.map((_, index) => {
                 return animate(`intersection_theta_${index}`, { position: { y: `+=${window.innerHeight}` } }, { duration: 1 });
             });
 
             return [parallel(moveAHashes), parallel(moveBHashes), parallel(moveIntersectionThetas)];
         }
 
+        const differencesSet = new Set(hashesBWithSmallerKth.map((hash) => hash.value));
+        const differences = hashesAWithSmallerKth.filter(item => !differencesSet.has(item.value));
+
         const differenceCircles = () => {
             const result: any[] = [];
 
-            hashesA
-                .filter((hash) => hash.value <= smallKth.value)
+            hashesAWithSmallerKth
                 .map((hash, index) => {
                     const { location } = hash;
                     const style = new THREE.MeshBasicMaterial({ color: green });
                     result.push(circle(`difference_a_circle_${index}`, radius, { x: location, y: height * 2, z: 1 }, style as any));
                 });
 
-            hashesB
-                .filter((hash) => hash.value <= smallKth.value)
+            hashesBWithSmallerKth
                 .map((hash, index) => {
                     const { location } = hash;
                     const style = new THREE.MeshBasicMaterial({ color: blue });
                     result.push(circle(`difference_b_circle_${index}`, radius, { x: location, y: height, z: 1 }, style as any));
                 });
 
-            const differences = () => {
-                const a = hashesA.filter((hash) => hash.value <= smallKth.value);
-                const b = hashesB.filter((hash) => hash.value <= smallKth.value);
-                const set = new Set(b.map((hash) => hash.value));
-                const difference = a.filter(item => !set.has(item.value));
-
-                difference.map((hash, index) => {
-                    const { location } = hash;
-                    const style = new THREE.MeshBasicMaterial({ color: yellow });
-                    result.push(circle(`difference_theta_${index}`, radius, { x: location, y: -height * 2 - window.innerHeight, z: 1 }, style as any));
-                });
-            }
-
-            differences();
+            differences.map((hash, index) => {
+                const { location } = hash;
+                const style = new THREE.MeshBasicMaterial({ color: yellow });
+                result.push(circle(`difference_theta_${index}`, radius, { x: location, y: -height * 2 - window.innerHeight, z: 1 }, style as any));
+            });
 
             return result;
         }
 
         const moveDifferenceCircles = () => {
-            const moveAHashes = hashesA
-                .filter((hash) => hash.value <= smallKth.value)
+            const moveAHashes = hashesAWithSmallerKth
                 .map((_, index) => animate(`difference_a_circle_${index}`, { position: { y: `-=${height * 4 - 10}` } }, { duration: 1 }));
 
-            const moveBHashes = hashesB
-                .filter((hash) => hash.value <= smallKth.value)
+            const moveBHashes = hashesBWithSmallerKth
                 .map((_, index) => animate(`difference_b_circle_${index}`, { position: { y: `-=${height * 3 + 10}` } }, { duration: 1 }));
 
-            const a = hashesA.filter((hash) => hash.value <= smallKth.value);
-            const b = hashesB.filter((hash) => hash.value <= smallKth.value);
-            const set = new Set(b.map((hash) => hash.value));
-            const difference = a.filter(item => !set.has(item.value));
-
-            const moveDifferenceThetas = difference.map((_, index) => {
+            const moveDifferenceThetas = differences.map((_, index) => {
                 return animate(`difference_theta_${index}`, { position: { y: `+=${window.innerHeight}` } }, { duration: 1 })
             });
 
             return [parallel(moveAHashes), parallel(moveBHashes), parallel(moveDifferenceThetas)];
         }
 
-        const unionFormulaLatex = latex("union_formula", unionFormula, { x: -window.innerWidth / 8 * 3, y: 0 - window.innerHeight }, textStyle);
-        const intersectionFormulaLatex = latex("intersection_formula", intersectionFormula, { x: window.innerWidth / 8 * 3, y: -height - window.innerHeight }, textStyle);
-        const differenceFormulaLatex = latex("difference_formula", differenceFormula, { x: -window.innerWidth / 8 * 3, y: -height * 2 - window.innerHeight }, textStyle);
+        const unionTitle = text("union_title", "Union", { x: -window.innerWidth / 8 * 3, y: 0 - window.innerHeight }, textStyle);
+        const intersectionTitle = text("intersection_title", "Intersection", { x: -window.innerWidth / 8 * 3, y: -height - window.innerHeight }, textStyle);
+        const differenceTitle = text("difference_title", "Difference", { x: -window.innerWidth / 8 * 3, y: -height * 2 - window.innerHeight }, textStyle);
+        const unionFormulaLatex = latex("union_formula", unionFormula(k, kthUnion.value), { x: window.innerWidth / 8 * 3, y: 0 - window.innerHeight }, textStyle);
+        const intersectionFormulaLatex = latex("intersection_formula", intersectionFormula(intersections.length, smallKth.value), { x: window.innerWidth / 8 * 3, y: -height - window.innerHeight }, textStyle);
+        const differenceFormulaLatex = latex("difference_formula", differenceFormula(differences.length, smallKth.value), { x: window.innerWidth / 8 * 3, y: -height * 2 - window.innerHeight }, textStyle);
 
         return {
             objects: [
-                ...buildAxis("sketch_a", height * 2, axisWidth),
-                ...buildAxis("sketch_b", height, axisWidth),
+                ...buildBaseAxis("sketch_a", height * 2, axisWidth, { title: "Sketch A", expected: streamASize, k, theta: kthHashA.value }),
+                ...buildBaseAxis("sketch_b", height, axisWidth, { title: "Sketch B", expected: streamBSize, k, theta: kthHashB.value }),
                 ...buildAxis("union", 0 - window.innerHeight, axisWidth),
                 ...buildAxis("intersection", -height - window.innerHeight, axisWidth),
                 ...buildAxis("difference", -height * 2 - window.innerHeight, axisWidth),
@@ -356,26 +367,38 @@ function SetOperationsPageContent({
                 ...buildThetaMarker("hashes_b", kthHashB.location, height, kthHashB.value),
                 ...hashesACircles,
                 ...hashesBCircles,
+                unionTitle,
                 unionFormulaLatex,
                 ...unionCircles(),
                 ...buildThetaMarker("union_theta", kthUnion.location, 0 - window.innerHeight, kthUnion.value),
+                intersectionTitle,
                 intersectionFormulaLatex,
                 ...intersectionCircles(),
                 ...buildThetaMarker("intersection_theta", smallKth.location, 0 - window.innerHeight - height, smallKth.value),
+                differenceTitle,
                 differenceFormulaLatex,
                 ...differenceCircles(),
                 ...buildThetaMarker("difference_theta", smallKth.location, 0 - window.innerHeight - height * 2, smallKth.value),
             ],
             steps: [
-                animate("union_formula", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                parallel([
+                    animate("union_title", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                    animate("union_formula", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                ]),
                 moveAxis("union"),
                 ...moveUnionCircles(),
                 moveThetaMarkers("union_theta"),
-                animate("intersection_formula", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                parallel([
+                    animate("intersection_title", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                    animate("intersection_formula", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                ]),
                 moveAxis("intersection"),
                 moveThetaMarkers("intersection_theta"),
                 ...moveIntersectionCircles(),
-                animate("difference_formula", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                parallel([
+                    animate("difference_title", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                    animate("difference_formula", { position: { y: `+=${window.innerHeight}` } }, { duration: 1 }),
+                ]),
                 moveAxis("difference"),
                 moveThetaMarkers("difference_theta"),
                 ...moveDifferenceCircles(),
