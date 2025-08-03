5b6903946c4b7eb80c25f87d22de45475b73ebdc
Dong Zhou <dozhou@rubiconproject.com>
2025-08-03 02:30:50 -0700

    Update default.ts

--- packages/obelus-three-render/src/themes/default.ts
+++ packages/obelus-three-render/src/themes/default.ts
@@ -1,26 +1,26 @@
 import * as THREE from "three";
 import { AxisProps } from "../dsl/animatable";
 
-const lineStyle: THREE.LineBasicMaterial = new THREE.LineBasicMaterial({ color: '#00BCD4' });
+const lineStyle: THREE.LineBasicMaterial = new THREE.LineBasicMaterial({ color: '#9C27B0' });
 
 const axisStyle: AxisProps = {
     dotCount: 3,
     lineWidth: 1.5,
     lineMaterial: new THREE.LineBasicMaterial({ color: '#9C27B0' }),
-    dotMaterial: new THREE.MeshBasicMaterial({ color: '#00BCD4' }),
+    dotMaterial: new THREE.MeshBasicMaterial({ color: '#4285F4' }),
     dotRadius: 4
 }
 
 const textStyle: Partial<CSSStyleDeclaration> = {
-    color: '#00BCD4',
+    color: '#4285F4',
     fontSize: '16px',
     fontWeight: 'normal',
     fontFamily: 'Inter, -apple-system, BlinkMacSystemFont, sans-serif',
 }
 
-const ringStyle: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#9C27B0' });
+const ringStyle: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#4285F4' });
 
-const circleStyle: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#9C27B0' });
+const circleStyle: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: '#4285F4' });
 
 export const theme = {
     lineStyle,
